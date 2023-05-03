138726cb3ac181f8ab107eabf3eb40dac1a201df
olsohee <108605017+olsohee@users.noreply.github.com>
2023-05-04 00:34:17 +0900

    [Bronze III] Title: 최댓값, Time: 132 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,15 +1,18 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int[] arr = new int[9];
         int max = 0;
         int index = 0;
 
         for(int i = 0; i < 9; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(br.readLine());
             if(max < arr[i]) {
                 max = arr[i];
                 index = i + 1;
