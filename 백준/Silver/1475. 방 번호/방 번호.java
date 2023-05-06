a4cc021a7a02df121bf6073ad2ff1d8648192443
olsohee <108605017+olsohee@users.noreply.github.com>
2023-05-06 12:54:33 +0900

    [Silver V] Title: 방 번호, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,23 +10,23 @@ public class Main {
         String input = br.readLine();
         int inputLen = input.length();
 
-        int[] arr = new int[9];
+        int[] arr = new int[9]; //0~8의 갯수를 나타내는 배열 (6과 9는 동일하게 취급하여 arr[6]에 담을 예정)
 
         for(int i = 0; i < inputLen; i++) {
             if(input.charAt(i) - '0' == 9) {
                 arr[6]++;
-            } else {
-                arr[input.charAt(i) - '0']++;
+                continue;
             }
+            arr[input.charAt(i) - '0']++;
         }
 
         int max = 0;
         for(int i = 0; i < arr.length; i++) {
             if(i == 6) {
-                    max = Math.max((arr[6] + 1) / 2, max);
-            } else {
-                max = Math.max(arr[i], max);
-            }
+                    max = Math.max((arr[6] + 1) / 2, max); 
+                    continue;
+            } 
+            max = Math.max(arr[i], max);
         }
 
         System.out.println(max);
