78d2a2719d522bb1a918336213d260b9c1195627
olsohee <108605017+olsohee@users.noreply.github.com>
2023-05-08 15:08:45 +0900

    [Silver II] Title: 키로거, Time: 1204 ms, Memory: 210004 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,43 +1,45 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        Stack<Character> stack1 = new Stack<>();
-        Stack<Character> stack2 = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
+            Stack<Character> stack2 = new Stack<>();
+            String input = br.readLine();
+            for(int j = 0; j < input.length(); j++) {
+                char cmd = input.charAt(j);
 
-        for(int i = 0; i < num; i++) {
-            String str = br.readLine();
-            for(int j = 0; j < str.length(); j++) {
-                if(str.charAt(j) == '<') {
-                    if(!stack1.isEmpty()) {
-                        stack2.push(stack1.pop());
+                if(cmd == '<') {
+                    if(!stack.isEmpty()) {
+                        stack2.push(stack.pop());
                     }
-                } else if(str.charAt(j) == '>') {
+                } else if(cmd == '>') {
                     if(!stack2.isEmpty()) {
-                        stack1.push(stack2.pop());
+                        stack.push(stack2.pop());
                     }
-                } else if(str.charAt(j) == '-') {
-                    if (!stack1.isEmpty()) {
-                        stack1.pop();
+                } else if(cmd == '-') {
+                    if(!stack.isEmpty()) {
+                        stack.pop();
                     }
                 } else {
-                    stack1.push(str.charAt(j));
+                    stack.push(cmd);
                 }
             }
-            while(!stack2.isEmpty())
-                stack1.push(stack2.pop());
-
+            while(!stack.isEmpty()) {
+                stack2.push(stack.pop());
+            }
             StringBuffer sb = new StringBuffer();
-            while (!stack1.isEmpty())
-                sb.append(stack1.pop());
-
-           
-            System.out.println(sb.reverse());
+            while(!stack2.isEmpty()) {
+                sb.append(stack2.pop());
+            }
+            System.out.println(sb);
         }
     }
 }
