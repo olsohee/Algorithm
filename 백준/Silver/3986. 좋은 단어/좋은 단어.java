3a2787efaf5bab01e5435d04d3d91b248bad2d6a
olsohee <108605017+olsohee@users.noreply.github.com>
2023-05-10 17:07:38 +0900

    [Silver IV] Title: 좋은 단어, Time: 336 ms, Memory: 22092 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
+            String s = br.readLine();
+
+            for(int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+
+                if(stack.isEmpty() || stack.peek() != c) {
+                    stack.push(c);
+                } else {
+                    stack.pop();
+                }
+            }
+
+            if(stack.isEmpty()) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
