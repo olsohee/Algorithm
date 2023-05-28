c30f613f4ad0b2a3e5c4cf4922e9ebca3140c2a1
olsohee <108605017+olsohee@users.noreply.github.com>
2023-05-28 17:47:08 +0900

    [Silver III] Title: N과 M (8), Time: 168 ms, Memory: 16412 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[] savedNum;
+    static int[] outputNum;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        savedNum = new int[n];
+        outputNum = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            savedNum[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(savedNum);
+        func(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void func(int k, int startIdx) {
+
+        if(k == m) {
+            for(int i = 0; i < m; i++) {
+                sb.append(outputNum[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = startIdx; i < n; i++) {
+            outputNum[k] = savedNum[i];
+            func(k + 1, i );
+        }
+    }
+}
