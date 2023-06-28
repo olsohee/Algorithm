6a0d371172621e11409ea90878fa0ed3988275ba
olsohee <108605017+olsohee@users.noreply.github.com>
2023-06-28 12:04:35 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -10,18 +10,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < N; i++) {
+        String str = br.readLine();
+        int numLen = str.length();
+        
+        int N = Integer.parseInt(str);
+        
+        for(int i = (N - numLen*9); i < N; i++) {
             int number = i;
             int sum = 0;
-
+            
             while(number != 0) {
                 sum += number % 10;
                 number = number / 10;
             }
-
-            if(i + sum == N) {
+            
+            if(sum + i == N) {
                 System.out.println(i);
                 return;
             }
