2495f135b33d9be5b926a3941efe505656698c5e
olsohee <108605017+olsohee@users.noreply.github.com>
2023-07-05 10:11:04 +0900

    [Bronze I] Title: 캠핑, Time: 200 ms, Memory: 16348 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int cnt = 0;
+
+        while(true) {
+            cnt++;
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            if(L == 0 && P == 0 && V == 0) {
+                break;
+            }
+
+            int result = V / P * L + Math.min(L, V % P);
+
+            System.out.println("Case " + cnt + ": " + result);
+        }
+
+    }
+}
