86954edfeb547eabf84ab384068654347a8adfeb
olsohee <108605017+olsohee@users.noreply.github.com>
2023-08-26 11:35:10 +0900

    [Silver IV] Title: 제로, Time: 228 ms, Memory: 24464 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,26 +2,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < num; i++) {
-            int n = Integer.parseInt(br.readLine());
-            if (n == 0) {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        LinkedList<Integer> stack = new LinkedList<>();
+        for(int i = 0; i < cnt; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(!stack.isEmpty() && num == 0) {
+                stack.pop();
             } else {
-                stack.push(n);
+                stack.push(num);
             }
         }
+
+        int size = stack.size();
         int result = 0;
-        while(!stack.isEmpty()) {
+        for(int i = 0; i < size; i++) {
             result += stack.pop();
         }
         System.out.println(result);
