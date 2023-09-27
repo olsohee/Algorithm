d50821b3a3a6f2280e1c910354510bff9d644dda
olsohee <108605017+olsohee@users.noreply.github.com>
2023-09-27 18:05:27 +0900

    [Silver I] Title: 그림, Time: 512 ms, Memory: 50644 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,43 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int n, m;
-    static int[][] map;
-    static boolean[][] visited;
-    static Queue<Pair> que = new LinkedList<>();
-    static int count;
-    static int max = 0;
-
     public static void main(String[] args) throws IOException {
 
-
-
+        int n, m;
+        int[][] arr;
+        boolean[][] visit;
+        int[] dx;
+        int[] dy;
+        Queue<Pair> que;
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        visited = new boolean[n][m];
 
+        arr = new int[n][m];
+        visit = new boolean[n][m];
+        que = new LinkedList<>();
+        
+        dx = new int[]{1, 0, -1, 0};
+        dy = new int[]{0, 1, 0, -1};
+        
+        //배열값 세팅
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
+        
+        //시작
+        int count = 0;
+        int area = 0;
+        int max = 0;
+        
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(map[i][j] == 1 && !visited[i][j]) {
-                    bfs(i, j);
+                //0이거나 방문한 적이 있으면 생략
+                if(arr[i][j] == 0 || visit[i][j]) {
+                    continue;
+                }
+                
+                //시작점 시작
+                count++; 
+                que.offer(new Pair(i, j));
+                visit[i][j] = true;
+                area = 0;
+                
+                while(!que.isEmpty()) {
+                    Pair p = que.poll();
+                    area++;
+                    for(int k = 0; k < 4; k++) {
+                        int nx = p.x + dx[k];
+                        int ny = p.y + dy[k];
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                            continue;
+                        }
+                        if(arr[nx][ny] == 1 && !visit[nx][ny]) {
+                            que.offer(new Pair(nx, ny));
+                            visit[nx][ny] = true;
+                        }
+                    }
+                }
+                
+                if(area > max) {
+                    max = area;
                 }
             }
         }
@@ -46,40 +77,17 @@ public class Main {
         System.out.println(max);
     }
     
-    static void bfs(int y, int x) {
-
-        count++;
-        que.offer(new Pair(x, y));
-        visited[y][x] = true;
-        
-        int area = 0;
-        
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        
-        while(!que.isEmpty()) {
-            Pair p = que.poll();
-            area++;
-            for(int i = 0; i < 4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                
-                if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-                if(map[ny][nx] != 1 || visited[ny][nx]) continue;
-                
-                que.offer(new Pair(nx, ny));
-                visited[ny][nx] = true;
-            }
-        }
-
-        max = Math.max(max, area);
-    }
-    
-    static class Pair {
+    public static class Pair {
         int x, y;
+
         public Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
+
+        public int getX() {return x;}
+        public int getY() {return y;}
+        public void setX(int x) {this.x = x;}
+        public void setY(int y) {this.y = y;}
     }
 }
