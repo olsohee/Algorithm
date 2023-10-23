b9cbf57224d5b82f6e2feecaf4f11add3f94efe9
olsohee <108605017+olsohee@users.noreply.github.com>
2023-10-23 11:06:32 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-// 1463번 1로 만들기
+import java.io.*;
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
 
-        int x = Integer.parseInt(br.readLine());
-
-        int dp[] = new int[x + 1];
-
-        dp[0] = dp[1] = 0;
-
-        for (int i = 2; i <= x; i++) {
-            dp[i] = dp[i - 1] + 1; // 먼저 1을 빼준다 
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 1을 뺀 값과 2로 나눈 값 중 최솟값을 dp[i]에 저장한다 
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 1을 뺀 값과 2로 나눈 값 중 최소값인 dp[i] 와 3으로 나눈 값 중 최솟값을 dp[i]에 저장한다 
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if(i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if(i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
         }
 
-        System.out.println(dp[x]);
+        System.out.println(dp[n]);
     }
 }
