ec58ef37a21998b59f20216ea63c3095e0d31e3a
olsohee <108605017+olsohee@users.noreply.github.com>
2023-11-16 17:54:06 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,44 @@
-import java.io.*;
-import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-    static int[][] map;
+    static int n;
+    static int m;
+    static boolean[][] graph;
     static boolean[] visited;
-    static int N;
-    static int M;
     static int answer;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        graph = new boolean[n+1][n+1];
+        visited = new boolean[n+1];
 
-        map = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
-
-        for(int i = 0; i < M; i++) {
+        for(int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
-
-            map[n1][n2] = map[n2][n1] = 1;
+            graph[n1][n2] = graph[n2][n1] = true;
         }
 
-        // 1번 노드에서 dfs 시작
         dfs(1);
 
         System.out.println(answer - 1);
     }
 
-    private static void dfs(int idx) {
+    static void dfs(int idx) {
 
-        answer++;
         visited[idx] = true;
+        answer++;
 
-        for(int i = 1; i < N + 1; i++) {
-            if(map[idx][i] == 1 && !visited[i]) {
+        for(int i = 1; i < n + 1; i++) {
+            if(graph[idx][i] && !visited[i]) {
                 dfs(i);
             }
         }
