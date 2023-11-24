c111491e31dd1cceeb271b6a4dc29568df4d708b
olsohee <108605017+olsohee@users.noreply.github.com>
2023-11-24 12:04:12 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 48.23 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,50 +1,47 @@
+
+
 import java.util.*;
 
 class Solution {
-     public int solution(int[] queue1, int[] queue2) {
-        int answer = 0;
-		int length = queue1.length;
-		long queue1Sum = 0; // queue1의 합계만을 사용할 것
-		long totalSum = 0;
-		long targetSum = 0;
-		
-		Queue<Integer> q1 = new LinkedList<>();
-		Queue<Integer> q2 = new LinkedList<>();
-        
-		queue1Sum = Arrays.stream(queue1).sum(); // 6
-        
-		totalSum = queue1Sum + Arrays.stream(queue2).sum(); // 20
+    public int solution(int[] queue1, int[] queue2) {
 
-		if (totalSum % 2 == 1) return -1; // 똑같이 나눌 수 없다면 -1 리턴
-        
-		targetSum = totalSum / 2; // 10 // 하나의 큐만 절반 값을 맞춘다면 okay, 절반 값을 이용
-        
-		for (int i = 0; i < length; i++) {
-			q1.offer(queue1[i]); // 1, 2, 1, 2
-			q2.offer(queue2[i]); // 1, 10, 1, 2
-		}
-        
-		int tempNum = 0;
-        
-		while (queue1Sum != targetSum) {
-        	
-			if (queue1Sum < targetSum) {
-				tempNum = q2.poll();
-				q1.offer(tempNum);
-				queue1Sum += tempNum;
-			} else {
-				tempNum = q1.poll();
-				q2.offer(tempNum);
-				queue1Sum -= tempNum;
-			}
-        	
-			answer++;
-        	
-			// queue1, queue2의 모든 원소가 자리바꿈하여 다시 원래의 위치로 오기 위한 횟수 (queue1.length + queue2.length) * 2 = 16
-			// 즉 16이 된다는 것은 다시 처음의 경우와 같아졌음을 의미하고, 더이상 반복할 필요가 없음을 뜻한다.
-			if (answer > length * 4 - 1) return -1;
-		}
-		
-		return answer;
+        Queue<Integer> que1 = new LinkedList<>();
+        Queue<Integer> que2 = new LinkedList<>();
+        long totalSum = 0;
+        long que1Sum = 0;
+
+        for (int i = 0; i < queue1.length; i++) {
+            que1.add(queue1[i]);
+            que2.add(queue2[i]);
+            que1Sum += queue1[i];
+            totalSum += (queue1[i] + queue2[i]);
+        }
+
+        if(totalSum % 2 != 0) {
+            return -1;
+        }
+
+        int cnt = 0;
+        long goal = totalSum / 2;
+
+        while(que1Sum != goal) {
+            if(cnt == queue1.length * 4) {
+                return -1;
+            }
+
+            if(que1Sum > goal) {
+                Integer value = que1.poll();
+                que1Sum -= value;
+                que2.add(value);
+            } else {
+                Integer value = que2.poll();
+                que1Sum += value;
+                que1.add(value);
+            }
+            cnt++;
+        }
+
+        return cnt;
     }
 }
+
