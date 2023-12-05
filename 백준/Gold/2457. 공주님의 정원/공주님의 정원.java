3bd4facfddaf17c11a771add3fa62b9e6329019f
olsohee <108605017+olsohee@users.noreply.github.com>
2023-12-05 23:15:19 +0900

    [Gold III] Title: 공주님의 정원, Time: 588 ms, Memory: 53604 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -20,41 +20,38 @@ public class Main {
             flowers.add(new Flower(start, end));
         }
 
-        // 시작일을 기준으로 오름차순 정렬 (만약 시작일이 같다면, 종료일이 늦은 순서로 정렬)
+        // 꽃이 피는 닐짜가 빠른 순으로 정렬 (만약 피는 날짜가 같으면 늦게 지는 순으로 정렬)
         Collections.sort(flowers);
 
-        int point = 301;
+        int maxEndDay = 301;
         int start = 301;
         int idx = 0;
-        int answer = 0;
+        int flowerCount = 0;
 
-        while(start <= 1130) {
+        while (start <= 1130) { // 탈출 조건: 1130이어도 안되고 1130보다 커야 함
             boolean flag = false;
-            for(int i = idx; i < n; i++) {
-                if(flowers.get(i).start > start) {
+            for (int i = idx; i < n; i++) {
+                if (flowers.get(i).start > start) {
                     break;
                 }
-
-                if(flowers.get(i).end > point) {
-                    point = flowers.get(i).end;
-                    idx = i + 1;
+                if (flowers.get(i).end > maxEndDay) {
+                    maxEndDay = flowers.get(i).end;
+                    idx = i + 1; // 다음 꽃을 찾을 때 굳이 i와 그 이전 인덱스의 꽃을 탐색할 필요가 없음
                     flag = true;
                 }
             }
-
-            if(flag) {
-                start = point;
-                answer++;
+            if (flag) {
+                start = maxEndDay;
+                flowerCount++;
             } else {
-                // 아예 해당하는 꽃이 없는 경우
-                break;
+                break; // 해당하는 꽃이 한 개도 없는 경우
             }
         }
 
-        if (point <= 1130) {
+        if (maxEndDay <= 1130) {
             System.out.println(0);
         } else {
-            System.out.println(answer);
+            System.out.println(flowerCount);
         }
     }
 
