9fcd3b92aac840bb5e89e4d00ff65ae293331fb1
olsohee <108605017+olsohee@users.noreply.github.com>
2023-12-26 14:01:05 +0900

    [Silver I] Title: 그림, Time: 436 ms, Memory: 44060 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,93 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 
-import java.io.*;
-import java.util.*;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+// 시간복잡도: O(N) = 300
+// dp[i] = i까지 올라오는 데 얻을 수 있는 점수의 최댓값
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int n; // 세로
+    static int m; // 가로
+    static int[][] map;
+    static boolean[][] visited;
+    static int count = 0; // 그림의 개수
+    static int maxArea = 0; // 그림 중 가장 큰 것의 넓이
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-        int n, m;
-        int[][] arr;
-        boolean[][] visit;
-        int[] dx;
-        int[] dy;
-        Queue<Pair> que;
-        
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        visited = new boolean[n][m];
 
-        arr = new int[n][m];
-        visit = new boolean[n][m];
-        que = new LinkedList<>();
-        
-        dx = new int[]{1, 0, -1, 0};
-        dy = new int[]{0, 1, 0, -1};
-        
-        //배열값 세팅
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        //시작
-        int count = 0;
-        int area = 0;
-        int max = 0;
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                //0이거나 방문한 적이 있으면 생략
-                if(arr[i][j] == 0 || visit[i][j]) {
-                    continue;
-                }
-                
-                //시작점 시작
-                count++; 
-                que.offer(new Pair(i, j));
-                visit[i][j] = true;
-                area = 0;
-                
-                while(!que.isEmpty()) {
-                    Pair p = que.poll();
-                    area++;
-                    for(int k = 0; k < 4; k++) {
-                        int nx = p.x + dx[k];
-                        int ny = p.y + dy[k];
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                            continue;
-                        }
-                        if(arr[nx][ny] == 1 && !visit[nx][ny]) {
-                            que.offer(new Pair(nx, ny));
-                            visit[nx][ny] = true;
-                        }
-                    }
-                }
-                
-                if(area > max) {
-                    max = area;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // 그림이면서 아직 방문하지 않은 부분 -> 탐색 시작하기
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    count++;
+                    visited[i][j] = true;
+                    bfs(new Pair(j, i));
                 }
             }
         }
 
         System.out.println(count);
-        System.out.println(max);
+        System.out.println(maxArea);
     }
-    
-    public static class Pair {
-        int x, y;
+
+    private static void bfs(Pair pair) {
+        int area = 0;
+        Queue<Pair> que = new LinkedList<>();
+        que.offer(pair);
+        area++;
+
+        while (!que.isEmpty()) {
+            Pair p = que.poll();
+
+            // 사방 탐색
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n || visited[ny][nx]) {
+                    continue;
+                }
+
+                if (map[ny][nx] == 1) {
+                    que.offer(new Pair(nx, ny));
+                    area++;
+                    visited[ny][nx] = true;
+                }
+            }
+        }
+        maxArea = Math.max(maxArea, area);
+    }
+
+    static class Pair {
+        int x;
+        int y;
 
         public Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
-
-        public int getX() {return x;}
-        public int getY() {return y;}
-        public void setX(int x) {this.x = x;}
-        public void setY(int y) {this.y = y;}
     }
 }
