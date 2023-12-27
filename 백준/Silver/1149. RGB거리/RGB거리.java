2bd6ba91326ac711b1d7ada3bf8e6e40a2eb939f
olsohee <108605017+olsohee@users.noreply.github.com>
2023-12-27 17:25:23 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,40 +1,42 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 시간복잡도: 이차원 배열 = 3 * N = 3,000
 public class Main {
 
-    static int[][] cost; // 각 집의 색칠 가격
-    static int[][] dp; // dp[i]: i번째 집을 칠하는 최소 가격
+    static int n;
+    static int[][] cost;
+    static int[][] totalMinCost;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        cost = new int[n + 1][3];
-        dp = new int[n + 1][3];
-        for (int i = 1; i <= n; i++) {
+        n = Integer.parseInt(br.readLine());
+        cost = new int[n][3];
+        totalMinCost = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.parseInt(st.nextToken());
-            cost[i][1] = Integer.parseInt(st.nextToken());
-            cost[i][2] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        // 초기값
-        dp[1][0] = cost[1][0];
-        dp[1][1] = cost[1][1];
-        dp[1][2] = cost[1][2];
+        totalMinCost[0][0] = cost[0][0];
+        totalMinCost[0][1] = cost[0][1];
+        totalMinCost[0][2] = cost[0][2];
 
-        // dp(Bottom-Up)
-        for (int i = 2; i <= n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+        for (int i = 1; i < n; i++) {
+            // i번째 집을 빨간색으로 칠하는 경우(i-1번째 집은 초록, 파랑 중 총 비용이 더 적은 색으로)
+            totalMinCost[i][0] = Math.min(totalMinCost[i - 1][1], totalMinCost[i - 1][2]) + cost[i][0];
+            // i번째 집을 초록색으로 칠하는 경우(i-1번째 집은 빨강, 파랑 중 총 비용이 더 적은 색으로)
+            totalMinCost[i][1] = Math.min(totalMinCost[i - 1][0], totalMinCost[i - 1][2]) + cost[i][1];
+            // i번째 집을 파란색으로 칠하는 경우(i-1번째 집은 빨강, 초록 중 총 비용이 더 적은 색으로)
+            totalMinCost[i][2] = Math.min(totalMinCost[i - 1][0], totalMinCost[i - 1][1]) + cost[i][2];
         }
 
-        // n번째 집을 색칠하는데 드는 최소 비용 구하기
-        System.out.println(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
+        System.out.println(Math.min(totalMinCost[n - 1][0], Math.min(totalMinCost[n - 1][1], totalMinCost[n - 1][2])));
     }
 }
