bcb9e9383bba1eddcb749e4e74480070fee4a6c6
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-01 21:16:39 +0900

    [Gold II] Title: 2048 (Easy), Time: 184 ms, Memory: 19888 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -1,140 +1,163 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 시간복잡도: 4^5 + O(N^2) = 4^5 + 400
 public class Main {
-    
-    static int n, answer, map[][];
-    
-    public static void main(String[] args) throws Exception {
+
+    static int n; 
+    static int[][] map;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        answer = 0;
         map = new int[n][n];
-        StringTokenizer stz;
-        for(int i = 0; i < n; i++) {
-            stz = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++)
-                map[i][j] = Integer.parseInt(stz.nextToken());
-        }
         
-        game(0);
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+//        int[][] newMap = up(map);
+//        for (int[] ints : newMap) {
+//            System.out.println();
+//            for (int anInt : ints) {
+//                System.out.print(anInt + " ");
+//            }
+//        }
+
+        dfs(0, copyMap(map));
+
         System.out.println(answer);
     }
-    
-    public static void game(int count) {
-        if(count == 5) {
-            findMax();
+
+    private static void dfs(int count, int[][] map) {
+        if (count == 5) {
+            answer = Math.max(getMax(map), answer);
             return;
         }
-        int copy[][] = new int[n][n];
-        for(int i = 0; i < n; i++)
-            copy[i] = map[i].clone();
-        
-        for(int i = 0; i < 4; i++) {
-            move(i);
-            game(count+1);
-            for(int a = 0; a < n; a++)
-                map[a] = copy[a].clone();
-        }
+
+        // 위
+        dfs(count + 1, up(copyMap(map)));
+
+        // 아래
+        dfs(count + 1, down(copyMap(map)));
+
+        // 오른쪽
+        dfs(count + 1, right(copyMap(map)));
+
+        // 왼쪽
+        dfs(count + 1, left(copyMap(map)));
     }
-    
-    public static void move(int dir) {
-        switch(dir) {
-            //위로 몰기
-            case 0:
-                for(int i = 0; i < n; i++) {
-                    int index = 0;
-                    int block = 0;
-                    for(int j = 0; j < n; j++) {
-                        if(map[j][i] != 0) {
-                            if(block == map[j][i]) {
-                                map[index - 1][i] = block * 2;
-                                block = 0;
-                                map[j][i] = 0;
-                            }
-                            else {
-                                block = map[j][i];
-                                map[j][i] = 0;
-                                map[index][i] = block;
-                                index++;
-                            }
-                        }
-                    }
+
+    private static int[][] up(int[][] map) {
+        int[][] newMap = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            int lastIdx = 0;
+            int checkIdx = 0;
+            for (int j = 0; j < n; j++) {
+                int num = map[j][i];
+                if (num == 0) continue;
+                if (newMap[checkIdx][i] == num) {
+                    newMap[checkIdx][i] = num * 2;
+                    checkIdx++;
+                } else {
+                    newMap[lastIdx][i] = num;
+                    lastIdx++;
+                    checkIdx = lastIdx - 1;
                 }
-                break;
-            //왼쪽으로 몰기
-            case 1:
-                for(int i = 0; i < n; i++) {
-                    int index = n - 1;
-                    int block = 0;
-                    for(int j = n - 1; j >= 0; j--) {
-                        if(map[j][i] != 0) {
-                            if(block == map[j][i]) {
-                                map[index + 1][i] = block * 2;
-                                block = 0;
-                                map[j][i] = 0;
-                            }
-                            else {
-                                block = map[j][i];
-                                map[j][i] = 0;
-                                map[index][i] = block;
-                                index--;
-                            }
-                        }
-                    }
-                }
-                break;
-            //왼쪽으로 몰기
-            case 2:
-                for(int i = 0; i < n; i++) {
-                    int index = 0;
-                    int block = 0;
-                    for(int j = 0; j < n; j++) {
-                        if(map[i][j] != 0) {
-                            if(block == map[i][j]) {
-                                map[i][index - 1] = block * 2;
-                                block = 0;
-                                map[i][j] = 0;
-                            }
-                            else {
-                                block = map[i][j];
-                                map[i][j] = 0;
-                                map[i][index] = block;
-                                index++;
-                            }
-                        }
-                    }
-                }
-                break;
-            //오른쪽으로 몰기
-            case 3:
-                for(int i = 0; i < n; i++) {
-                    int index = n - 1;
-                    int block = 0;
-                    for(int j = n - 1; j >= 0; j--) {
-                        if(map[i][j] != 0) {
-                            if(block == map[i][j]) {
-                                map[i][index + 1] = block * 2;
-                                block = 0;
-                                map[i][j] = 0;
-                            }
-                            else {
-                                block = map[i][j];
-                                map[i][j] = 0;
-                                map[i][index] = block;
-                                index--;
-                            }
-                        }
-                    }
-                }
-                break;
+            }
         }
+
+        return newMap;
     }
-    
-    public static void findMax() {
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                answer = Math.max(answer, map[i][j]);
+
+    private static int[][] down(int[][] map) {
+        int[][] newMap = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            int lastIdx = n - 1;
+            int checkIdx = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                int num = map[j][i];
+                if (num == 0) continue;
+                if (newMap[checkIdx][i] == num) {
+                    newMap[checkIdx][i] = num * 2;
+                    checkIdx--;
+                } else {
+                    newMap[lastIdx][i] = num;
+                    lastIdx--;
+                    checkIdx = lastIdx + 1;
+                }
+            }
+        }
+
+        return newMap;
+    }
+
+    private static int[][] right(int[][] map) {
+        int[][] newMap = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            int lastIdx = n - 1;
+            int checkIdx = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                int num = map[i][j];
+                if (num == 0) continue;
+                if (newMap[i][checkIdx] == num) {
+                    newMap[i][checkIdx] = num * 2;
+                    checkIdx--;
+                } else {
+                    newMap[i][lastIdx] = num;
+                    lastIdx--;
+                    checkIdx = lastIdx + 1;
+                }
+            }
+        }
+        return newMap;
+    }
+
+    private static int[][] left(int[][] map) {
+        int[][] newMap = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            int lastIdx = 0;
+            int checkIdx = 0;
+            for (int j = 0; j < n; j++) {
+                int num = map[i][j];
+                if (num == 0) continue;
+                if (newMap[i][checkIdx] == num) {
+                    newMap[i][checkIdx] = num * 2;
+                    checkIdx++;
+                } else {
+                    newMap[i][lastIdx] = num;
+                    lastIdx++;
+                    checkIdx = lastIdx - 1;
+                }
+            }
+        }
+
+        return newMap;
+    }
+
+    private static int getMax(int[][] map) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, map[i][j]);
+            }
+        }
+        return max;
+    }
+
+    private static int[][] copyMap(int[][] map) {
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            copy[i] = map[i];
+        }
+        return copy;
     }
 }
