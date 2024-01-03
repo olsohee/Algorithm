86f875017a8eba70a93880985a2991a59e13c2a5
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-03 12:14:32 +0900

    [Gold V] Title: 적록색약, Time: 144 ms, Memory: 16304 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,83 +1,157 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static char[][] arr1;
-    static char[][] arr2;
-    static boolean[][] visited1;
-    static boolean[][] visited2;
+
+    /**
+     * 1. 아이디어
+     * - BFS
+     * - 이중 for문 돌면서 bfs의 시작점부터 bfs 시작(큐에 넣고 bfs 시작)
+     * - 이때 적록색약인 사람은 R, G을 같은 취급
+     * - bfs의 시작점을 카운트하기
+     *
+     * 2. 시간 복잡도
+     * - BFS: O(V+E)
+     *
+     * 3. 자료구조
+     * - 그래프 char[][]
+     * - 큐, 방문 기록
+     * - 카운드 int count
+     */
+
+    static int N;
+    static char[][] map;
+    static boolean[][] visited;
+    static Queue<Pair> que = new LinkedList<>();
     static int count1;
     static int count2;
-    static Queue<Pair> que;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
 
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr1 = new char[n][n];
-        arr2 = new char[n][n];
-        visited1 = new boolean[n][n];
-        visited2 = new boolean[n][n];
-        que = new LinkedList<>();
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        visited = new boolean[N][N];
 
-        //적록색약이 아닌 경우(arr1) 적록색약인 경우(arr2) 배열 초기화
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < n; j++) {
-
-                arr1[i][j] = s.charAt(j);
-
-                if (arr1[i][j] == 'G') {
-                    arr2[i][j] = 'R';
-                } else {
-                    arr2[i][j] = arr1[i][j];
-                }
+        for(int  i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j);
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                //적록색약이 아닌 경우
-                if(!visited1[i][j]) { //방문하지 않은 경우
-                    que.offer(new Pair(j, i));
-                    visited1[i][j] = true; //방문 표시
-                    count1++; //각 영역의 첫 스타트 때 count값 증가
-                    bfs(arr1, visited1);
-                }
-
-                //적록색약인 경우
-                if(!visited2[i][j]) {
-                    que.offer(new Pair(j, i));
-                    visited2[i][j] = true;
-                    count2++;
-                    bfs(arr2, visited2);
+        // bfs(적록색약 X)
+        for(int  i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!visited[i][j]) {
+                    bfs1(i, j);
                 }
             }
         }
-        System.out.println(count1 + " " + count2);
+        System.out.println(count1);
+
+        // 방문기록 초기화
+        for(int  i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                visited[i][j] = false;
+            }
+        }
+
+        // bfs(적록색약 O)
+        for(int  i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!visited[i][j]) {
+                    bfs2(i, j);
+                }
+            }
+        }
+        System.out.println(count2);
     }
 
-    public static void bfs(char[][] arr, boolean[][] visited) {
+    static void bfs1(int y, int x) {
+
+        char color = map[y][x];
+        que.offer(new Pair(x, y));
+        visited[y][x] = true;
+        count1++;
+
         while(!que.isEmpty()) {
             Pair p = que.poll();
-            for(int i = 0; i < 4; i++) {
-                int x = p.x + dx[i];
-                int y = p.y + dy[i];
 
-                if(x < 0 || x >= n || y < 0 || y >= n || visited[y][x] || arr[p.y][p.x] != arr[y][x]) {
+            // 사방면 탐색
+            int[] dx = {1, -1, 0, 0};
+            int[] dy = {0, 0, -1, 1};
+
+            for(int i = 0; i < 4; i++) {
+                int nx = dx[i] + p.x;
+                int ny = dy[i] + p.y;
+
+                // 그래프 밖인 경우 pass
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N) {
                     continue;
                 }
 
-                que.offer(new Pair(x, y));
-                visited[y][x] = true;
+                // 이미 방문했거나 다른 색일 경우 경우 pass
+                if(visited[ny][nx] || map[ny][nx] != color) {
+                    continue;
+                }
+
+                que.offer(new Pair(nx, ny));
+                visited[ny][nx] = true;
             }
         }
     }
 
-    public static class Pair {
+    static void bfs2(int y, int x) {
+
+        char color = map[y][x];
+        que.offer(new Pair(x, y));
+        visited[y][x] = true;
+        count2++;
+
+        while(!que.isEmpty()) {
+            Pair p = que.poll();
+
+            // 사방면 탐색
+            int[] dx = {1, -1, 0, 0};
+            int[] dy = {0, 0, -1, 1};
+
+            for(int i = 0; i < 4; i++) {
+                int nx = dx[i] + p.x;
+                int ny = dy[i] + p.y;
+
+                // 그래프 밖인 경우 pass
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
+                }
+
+                // 이미 방문했거나 다른 색이면 pass
+                if(visited[ny][nx]) {
+                    continue;
+                }
+
+                if(color == 'G' || color == 'R') {
+                    if(map[ny][nx] == 'B') {
+                        continue;
+                    }
+                } else {
+                    if(map[ny][nx] != color) {
+                        continue;
+                    }
+                }
+
+                que.offer(new Pair(nx, ny));
+                visited[ny][nx] = true;
+            }
+        }
+    }
+
+    static class Pair {
+
         int x, y;
         public Pair(int x, int y) {
             this.x = x;
