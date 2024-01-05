9e2444453cdcd309fd96882d68db1aef410de32a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-05 11:09:10 +0900

    [Silver III] Title: N과 M (6), Time: 144 ms, Memory: 16288 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,53 +1,48 @@
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.io.InputStreamReader;
 import java.util.*;
 
+// 시간복잡도: O(N^2) = 64
 public class Main {
 
     static int n;
     static int m;
-    static int[] savedNum;
-    static int[] outputNum;
-
-    static boolean[] visited;
+    static int[] numbers;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        savedNum = new int[n];
-        visited = new boolean[n];
-        outputNum = new int[m];
+        numbers = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            savedNum[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(numbers);
 
-        Arrays.sort(savedNum);
-        func(0, 0);
+        dfs(0, new int[m], 0);
+        System.out.println(sb);
     }
 
-    public static void func(int k, int startIdx) {
-
-        if(k == m) {
-            for(int i = 0; i < m; i++) {
-                System.out.print(outputNum[i] + " ");
+    private static void dfs(int cnt, int[] arr, int startIdx) {
+        if (cnt == m) {
+            for (int i : arr) {
+                sb.append(i + " ");
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
-        for(int i = startIdx; i < n; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                outputNum[k] = savedNum[i];
-                func(k + 1, i + 1);
-                visited[i] = false;
-            }
+        for (int i = startIdx; i < n; i++) {
+            int[] cloneArr = arr.clone();
+            cloneArr[cnt] = numbers[i];
+            dfs(cnt + 1, cloneArr, i + 1);
         }
     }
 }
