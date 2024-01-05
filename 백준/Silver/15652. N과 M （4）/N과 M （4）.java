21ac19168f2ce014dead37bc9ede7a04bfd8272f
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-05 10:57:36 +0900

    [Silver III] Title: N과 M (4), Time: 216 ms, Memory: 19680 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,58 +1,39 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
+
 import java.io.InputStreamReader;
 import java.util.*;
 
+// 시간복잡도: O(N^2) = 64
 public class Main {
 
-    /**
-     * 1. 아이디어
-     * - 백트래킹
-     * - 중복 허용
-     * - 재귀 함수 호출 시, 반복 시작 값 전달하기
-     *
-     * 2. 시간 복잡도
-     * - 중복 허용: N^N
-     *
-     * 3. 자료구조
-     * - 결과를 저장할 int[]
-     */
-
-    static int N;
-    static int M;
-    static StringTokenizer st;
-    static BufferedReader br;
-    static int[] arr;
+    static int n;
+    static int m;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        // 입력
-        br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-
-        // 백트래킹
-        func(0, 1);
+        dfs(0, new int[m], 1);
+        System.out.println(sb);
     }
 
-    public static void func(int m, int start) {
-
-        // 종료 조건
-        if(m == M) {
-            for(int i = 0; i < m; i++) {
-                System.out.print(arr[i] + " ");
+    private static void dfs(int cnt, int[] arr, int startNum) {
+        if (cnt == m) {
+            for (int i : arr) {
+                sb.append(i + " ");
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
-        // 재귀 호출
-        for(int i = start; i <= N; i++) {
-            arr[m] = i;
-            func(m + 1, i);
+        for (int i = startNum; i <= n; i++) {
+            int[] cloneArr = arr.clone();
+            cloneArr[cnt] = i;
+            dfs(cnt + 1, cloneArr, i);
         }
     }
 }
