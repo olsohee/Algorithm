197ba9abe370d75c4ae406a333a6e9fe3c446845
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-09 11:39:50 +0900

    [level 2] Title: 타겟 넘버, Time: 0.46 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,25 @@
+// 시간복잡도: O(2^20) = 1,048,576
 class Solution {
-        int answer = 0;
-        int[] numbers;
-        int target = 0;
 
-        public int solution(int[] numbers, int target) {
-
-            this.numbers = numbers;
-            this.target = target;
-
-            dfs(0, 0);
-            return answer;
-        }
-
-        private void dfs(int cnt, int sum) {
-
-            if(cnt == numbers.length) {
-                if(sum == target) {
-                    answer++;
-                }
-                return;
-            }
-
-            dfs(cnt + 1, sum + numbers[cnt]);
-            dfs(cnt + 1, sum - numbers[cnt]);
-        }
+    int answer = 0;
+    int[] numbers;
+    int target;
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        dfs(0, 0);
+        return answer;
     }
+
+    private void dfs(int depth, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+
+        dfs(depth + 1, sum + numbers[depth]);
+        dfs(depth + 1, sum - numbers[depth]);
+    }
+}
