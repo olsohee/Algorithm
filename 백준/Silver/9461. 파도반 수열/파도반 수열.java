6e3bf018ddfe43fcce63b1e95ce5be1729afd0a0
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-10 10:53:27 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -3,12 +3,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
+// 시간복잡도: O(N) = 100
 public class Main {
 
-    static long[] dp;
     static int t;
+    static int n;
+    static long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,19 +19,13 @@ public class Main {
         dp[1] = 1;
         dp[2] = 1;
         dp[3] = 1;
-        dp[4] = 2;
-        dp[5] = 2;
-        long max = 2;
-
-        for(int i = 6; i <= 100; i++) {
-            dp[i] = max + dp[i - 5];
-            max = dp[i];
+        for (int j = 4; j <= 100; j++) {
+            dp[j] = dp[j - 2] + dp[j - 3];
         }
 
         for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine());
             System.out.println(dp[n]);
         }
     }
 }
-
