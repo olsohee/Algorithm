2e8d7d01eac755b0afe4127b8a8c96caa6cb3ca6
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-12 10:52:02 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 24188 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,36 +1,35 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
+
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 시간 복잡도: O(N) = 100,000
 public class Main {
 
-    static int N;
-    static int[] num;
-    static int[] dp;
-    static int max;
+    static int n;
+    static int[] input;
+    static long[] dp;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        num = new int[N];
-        dp = new int[N];
-
+        n = Integer.parseInt(br.readLine());
+        input = new int[n + 1];
+        dp = new long[n + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0] = num[0];
-        max = dp[0];
-
-        for(int i = 1; i < N; i++) {
-            dp[i] = Math.max(num[i], (dp[i-1] + num[i]));
-            max = Math.max(max, dp[i]);
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Math.max(input[i], dp[i - 1] + input[i]);
         }
 
-        System.out.println(max);
+        long answer = dp[1];
+        for (int i = 2; i <= n; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+        System.out.println(answer);
     }
 }
