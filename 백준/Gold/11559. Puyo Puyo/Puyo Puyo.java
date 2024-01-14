3c224f4dca4517c1106b5abe60336834af84d85f
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-14 12:36:26 +0900

    [Gold IV] Title: Puyo Puyo, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -1,113 +1,118 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
-import java.io.*;
-import java.util.*;
-
-
+// 시간 복잡도:
 public class Main {
 
-
-    static int result;
     static char[][] map = new char[12][6];
-    static boolean isContinue = false;
+    static int pop = 0;
+    static boolean isPop = false;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 그래프 초기화
         for (int i = 0; i < 12; i++) {
-            String s = br.readLine();
-            map[i] = s.toCharArray();
+            char[] input = br.readLine().toCharArray();
+            for (int j = 0; j < 6; j++) {
+                map[i][j] = input[j];
+            }
         }
 
+        // bfs
         while (true) {
-
-            // bfs로 터트릴 뿌요 체크 후 .로 변경
-            isContinue = false;
+            isPop = false; // 초기화
             bfs();
 
-            if(isContinue == false) {
-                System.out.println(result);
+            // 터트리지 않았으면 끝내기
+            if (!isPop) {
                 break;
             }
 
-            // 아래로 밀기
-            map = down();
-
-            // 뿌요 한 번 터트릴 때마다 값 증가시키기
-            result++;
+            // 터트렸으면 카운트하고 아래로 내리기
+            pop++;
+            onFloor();
         }
+
+        System.out.println(pop);
     }
 
-    private static char[][] down() {
-
-        char[][] newMap = new char[12][6];
-        for(int i = 0; i < 6; i++) {
-            int index = 11;
-            for(int j = 11; j >= 0; j--) {
-                newMap[j][i] = '.';
-                if(map[j][i] != '.') {
-                    newMap[index][i] = map[j][i];
-                    index--;
-                } else {
-                    newMap[j][i] = '.';
-                }
-            }
-        }
-        return newMap;
-    }
-
-    public static void bfs() {
-
+    private static void bfs() {
         boolean[][] visited = new boolean[12][6];
 
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 6; j++) {
-                if(map[i][j] != '.' && !visited[i][j]) {
-                    // 한가지 색에 대한 bfs 시작
+                if (!visited[i][j] && map[i][j] != '.') {
                     Queue<Point> que = new LinkedList<>();
                     List<Point> list = new ArrayList<>();
-
-                    que.offer(new Point(j, i));
+                    // bfs
+                    que.add(new Point(i, j));
                     visited[i][j] = true;
-                    list.add(new Point(j, i));
-
-                    int[] dx = {1, -1, 0, 0};
-                    int[] dy = {0, 0, 1, -1};
+                    list.add(new Point(i, j));
 
                     while (!que.isEmpty()) {
                         Point p = que.poll();
                         for (int k = 0; k < 4; k++) {
                             int nx = dx[k] + p.x;
                             int ny = dy[k] + p.y;
-
-                            if (nx < 0 || nx >= 6 || ny < 0 || ny >= 12 || map[ny][nx] != map[p.y][p.x] || visited[ny][nx]) {
+                            if (nx < 0 || nx >= 6 || ny < 0 || ny >= 12) {
                                 continue;
                             }
-
-                            que.offer(new Point(nx, ny));
-                            list.add(new Point(nx, ny));
-                            visited[ny][nx] = true;
+                            if (map[ny][nx] == map[i][j] && !visited[ny][nx]) {
+                                que.add(new Point(ny, nx));
+                                visited[ny][nx] = true;
+                                list.add(new Point(ny, nx));
+                            }
                         }
                     }
 
-                    // 해당 색에 대한 bfs 끝나면 뿌요 터트리기
-                    if(list.size() >= 4) {
-                        isContinue = true;
-                        for(int k = 0; k < list.size(); k++) {
+                    // 인접한 같은 색이 4개 이상이면 터트리기(. 표시)
+                    if (list.size() >= 4) {
+                        for (int k = 0; k < list.size(); k++) {
                             Point p = list.get(k);
                             map[p.y][p.x] = '.';
                         }
+                        isPop = true;
                     }
                 }
             }
         }
     }
 
+    private static void onFloor() {
+
+        for (int i = 0; i < 6; i++) {
+            Queue<Character> que = new LinkedList<>();
+            for (int j = 11; j >= 0; j--) {
+                if (map[j][i] != '.') {
+                    que.add(map[j][i]);
+                }
+            }
+
+            for (int j = 11; j >= 0; j--) {
+                if (!que.isEmpty()) {
+                    map[j][i] = que.poll();
+                } else {
+                    map[j][i] = '.';
+                }
+            }
+        }
+    }
+
     static class Point {
-        int x, y;
-        public Point(int x, int y) {
-            this.x = x;
+        int y, x;
+
+        public Point(int y, int x) {
             this.y = y;
+            this.x = x;
         }
     }
 }
