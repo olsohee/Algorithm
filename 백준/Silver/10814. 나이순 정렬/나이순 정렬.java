05ece44d5b4ba3b3423f04e00bc997b237710682
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-15 11:13:09 +0900

    [Silver V] Title: 나이순 정렬, Time: 1536 ms, Memory: 54280 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,47 +2,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 // 시간 복잡도:
 public class Main {
 
     static int n;
-    static List<Person> personList = new ArrayList<>();
-    static boolean[] arr = new boolean[2000002]; // -1,000,000 ~ 1,000,0000의 범위
-
+    static Person[] arr;
+    
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        arr = new Person[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            personList.add(new Person(age, name));
+            arr[i] = new Person(age, name);
         }
 
-        Collections.sort(personList);
-        for (Person person : personList) {
+        Arrays.sort(arr, new Comparator<Person>() {
+            // 나이순 정렬
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age - o2.age;
+            }
+        });
+        
+        for (Person person : arr) {
             System.out.println(person.age + " " + person.name);
         }
     }
 
-    static class Person implements Comparable<Person> {
+    static class Person {
         int age;
         String name;
 
         public Person(int age, String name) {
             this.age = age;
             this.name = name;
-        }
-
-        @Override
-        public int compareTo(Person o) {
-            return this.age - o.age;
         }
     }
 }
