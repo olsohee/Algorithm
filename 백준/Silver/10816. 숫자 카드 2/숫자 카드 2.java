e2ebe3d79dc4b84697f7a54a100800d9ee8ab878
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-17 16:17:50 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1156 ms, Memory: 182096 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,33 +1,42 @@
 
 import java.io.BufferedReader;
 import java.io.IOException;
-
 import java.io.InputStreamReader;
 import java.util.*;
 
+// 시간 복잡도: 500,000 * log500,000
 public class Main {
 
     static int n;
     static int m;
-    static Map<Integer, Integer> map = new HashMap();
-    static StringBuilder sb = new StringBuilder();
+    static Map<Integer, Integer> cardsMap = new HashMap<>();
+    static List<Integer> sortedCard;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            map.put(key, map.getOrDefault(key, 0) + 1);
+            int card = Integer.parseInt(st.nextToken());
+            cardsMap.put(card, cardsMap.getOrDefault(card, 0) + 1);
         }
 
         m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            sb.append(map.getOrDefault(key, 0) + " ");
+            int targetNumber = Integer.parseInt(st.nextToken());
+            sb.append(cardsMap.getOrDefault(targetNumber, 0) + " ");
         }
+
+//            boolean isContain = binarySearch(targetNumber);
+//            if (isContain) {
+//                sb.append(cardsMap.get(targetNumber) + " ");
+//            } else {
+//                sb.append(0 + " ");
+//            }
 
         System.out.println(sb);
     }
