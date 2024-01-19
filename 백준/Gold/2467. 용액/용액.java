c295f13ab6bf36a3bcdc14cac3036d83754e6786
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-19 10:40:44 +0900

    [Gold V] Title: 용액, Time: 396 ms, Memory: 28352 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,67 +1,46 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 시간 복잡도: O(N * logN)
+// 시간 복잡도: O(N)
 public class Main {
 
     static int n;
     static int[] arr;
     static long answer = Long.MAX_VALUE;
-    static int[] answerArr;
+    static int[] answerNumberArr = new int[2];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
-        answerArr = new int[2];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
-        for (int i = 0; i < n; i++) {
-            calculate(arr[i], i);
-        }
-
-//        System.out.println("answer = " + answer);
-        Arrays.sort(answerArr);
-        for (int i : answerArr) {
-            System.out.print(i + " ");
-        }
-    }
-
-    private static void calculate(int targetNum, int targetIdx) {
-        int minIdx = targetIdx + 1;
-        int maxIdx = n - 1;
-        int midIdx = 0;
-
-        long result = Long.MAX_VALUE;
-        int[] resultArr = new int[2];
-        resultArr[0] = targetNum;
-
-        while (minIdx <= maxIdx) {
-            midIdx = (minIdx + maxIdx) / 2;
-            long sum = targetNum + arr[midIdx];
-            if (result > Math.abs(sum - 0)) {
-                result = Math.abs(sum - 0);
-                resultArr[1] = arr[midIdx];
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (answer > Math.abs(sum)) {
+                answer = Math.abs(sum);
+                answerNumberArr[0] = arr[left];
+                answerNumberArr[1] = arr[right];
             }
-            if (sum <= 0) {
-                minIdx = midIdx + 1;
+
+            if (sum >= 0) {
+                right--;
             } else {
-                maxIdx = midIdx - 1;
+                left++;
             }
         }
 
-        if (answer > result) {
-            answer = result;
-            answerArr[0] = resultArr[0];
-            answerArr[1] = resultArr[1];
+        for (int i : answerNumberArr) {
+            System.out.print(i + " ");
         }
     }
 }
