b0c73c76299227ece539d0a1fe301eb99c12e346
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-24 23:39:16 +0900

    [Gold II] Title: 플로이드 2, Time: 700 ms, Memory: 57444 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.java
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.java
@@ -3,22 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 시간 복잡도: O(N^3) = 1,000,000
+// 시간 복잡도: O(N ^ 3) = 1,000,000
 public class Main {
 
     static int n;
     static int m;
-    static int[][] dist;
+    static long[][] dist;
     static List<Integer>[][] route;
-    static final int INF = 10000001;
-    static StringTokenizer st;
+    static final long INF = 10000000001L;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
-        dist = new int[n + 1][n + 1];
+        dist = new long[n + 1][n + 1];
         route = new ArrayList[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
@@ -30,29 +29,28 @@ public class Main {
         }
 
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
             dist[start][end] = Math.min(dist[start][end], cost);
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) continue;
-                for (int k = 1; k <= n; k++) {
-                    if (i == k || j == k) continue;
-                    // 갱신되는 경우
-                    if (dist[j][k] >= dist[j][i] + dist[i][k]) {
-                        dist[j][k] = dist[j][i] + dist[i][k];
-                        findRoute(j, k, i); // j에서 k로 가는데 i를 경유
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                if (k == i) continue;
+                for (int j = 1; j <= n; j++) {
+                    if (j == k || j == i) continue;
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        findRoute(k, i, j);
                     }
                 }
             }
         }
 
-        // 출력
         StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (dist[i][j] >= INF) {
@@ -66,29 +64,29 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == j || dist[i][j] >= INF) {
+                if (dist[i][j] == 0 || dist[i][j] >= INF) {
                     sb.append(0).append('\n');
-                    continue;
+                } else {
+                    sb.append(route[i][j].size() + 2 + " " + i + " ");
+                    for (Integer num : route[i][j]) {
+                        sb.append(num + " ");
+                    }
+                    sb.append(j).append('\n');
                 }
-                sb.append(route[i][j].size() + 2 + " " + i + " ");
-                for (Integer num : route[i][j]) {
-                    sb.append(num + " ");
-                }
-                sb.append(j).append('\n');
             }
         }
 
         System.out.println(sb);
     }
 
-    private static void findRoute(int start, int end, int mid) {
-        route[start][end].clear();
-        for (Integer i : route[start][mid]) {
-            route[start][end].add(i);
+    private static void findRoute(int k, int i, int j) {
+        route[i][j].clear();
+        for (Integer node : route[i][k]) {
+            route[i][j].add(node);
         }
-        route[start][end].add(mid);
-        for (Integer i : route[mid][end]) {
-            route[start][end].add(i);
+        route[i][j].add(k);
+        for (Integer node : route[k][j]) {
+            route[i][j].add(node);
         }
     }
 }
