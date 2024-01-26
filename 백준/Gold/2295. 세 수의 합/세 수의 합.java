3e92cc33380b42673fd04297514256374500c5f4
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-26 09:43:36 +0900

    [Gold IV] Title: 세 수의 합, Time: 724 ms, Memory: 43704 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 시간 복잡도 = O(N^2 * logN)
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+    static int[] arr;
+    static List<Integer> list = new ArrayList<>();
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        // a + b = x - c
+
+        // 이중 반복문으로 a + b의 집합 만들기
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                list.add(arr[i] + arr[j]);
+            }
+        }
+        Collections.sort(list);
+
+        // 이분 탐색으로 x - c를 찾고, a + b 집합에 있는지 확인하기
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (Collections.binarySearch(list, arr[j] - arr[i]) >= 0) {
+                    answer = Math.max(answer, arr[j]);
+                }
+//                binarySearch(arr[j], arr[i]);
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+    private static void binarySearch(int max, int min) {
+        int diff = max - min;
+        int start = 0;
+        int end = list.size() - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // 있는 경우
+            if (mid == diff) {
+                answer = Math.max(answer, max);
+                return;
+            }
+            if (mid > diff) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+    }
+}
