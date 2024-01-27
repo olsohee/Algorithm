56bdfefa2efb45d149606ca6a82672fc802955a9
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-27 11:29:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -4,52 +4,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 시간 복잡도: log(2^31 - 1)
+// 시간 복잡도 = O(N * logN)
 public class Main {
 
-    static int n;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int k;
-    static int[] rope;
+    static int n;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         k = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        rope = new int[k];
-        long maxLen = 0;
+        arr = new int[k];
+        long end = 0;
         for (int i = 0; i < k; i++) {
-            rope[i] = Integer.parseInt(br.readLine());
-            maxLen = Math.max(maxLen, rope[i]);
+            arr[i] = Integer.parseInt(br.readLine());
+            end = Math.max(end, arr[i]);
         }
-        
-        maxLen++;
-        long minLen = 0;
-        long midLen = 0;
-        
-        while (minLen < maxLen) {
-            midLen = (minLen + maxLen) / 2;
-            int count = getCount(midLen); // midLen 길이로 자르면 생기는 밧줄 갯수
 
-            // 길이 줄이기
+        // 이분 탐색
+        long start = 0;
+        end++;
+
+        while (start < end) { // start와 end가 같아질 때까지
+            long mid = (start + end) / 2;
+            long count = 0;
+            for (int i = 0; i < k; i++) {
+                count += arr[i] / mid;
+            }
+
+            // 더 짧게 자르기
             if (count < n) {
-                maxLen = midLen;
-            }
-            // 길이 늘리기
-            else {
-                minLen = midLen + 1;
+                end = mid;
+            } else {
+                // 더 길게 자르기 (같은 경우에도 더 길게)
+                start = mid + 1;
             }
         }
-        // while 문이 끝난 후에는 minLen == maxLen
-        System.out.println(minLen - 1);
-    }
-
-    private static int getCount(long midLen) {
-        int count = 0;
-        for (int i = 0; i < rope.length; i++) {
-            count += (rope[i] / midLen);
-        }
-        return count;
+        System.out.println(start - 1); // upper bound를 구했으므로
     }
 }
