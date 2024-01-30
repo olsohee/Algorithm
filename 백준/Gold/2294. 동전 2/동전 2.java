477096d6c01cc99da08b8dc0dcd9efd46730f668
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-30 10:31:44 +0900

    [Gold V] Title: 동전 2, Time: 136 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,42 +1,41 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
+// 시간 복잡도 = O(k * n) = 1,000,000
 public class Main {
 
-    static int N;
-    static int K;
-    static int[] coins;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int k;
     static int[] dp;
+    static int[] coin;
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        coin = new int[n];
+        dp = new int[k + 1];
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        coins = new int[N + 1];
-        dp = new int[K + 1];
-
-        for(int i = 1; i <= N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i = 1; i <= K; i++) {
-            dp[i] = 100001;
-        }
-
-        for(int i = 1; i <= N; i++) {
-            for(int j = coins[i]; j <= K; j++) {
-                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+        Arrays.fill(dp, 10001);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin[i]] + 1);
             }
         }
 
-        if(dp[K] == 100001) {
+        if (dp[k] >= 10001) {
             System.out.println(-1);
         } else {
-            System.out.println(dp[K]);
+            System.out.println(dp[k]);
         }
     }
 }
