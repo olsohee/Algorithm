321a1bffbba1f35cf0079f7f26d4b0788466f335
olsohee <108605017+olsohee@users.noreply.github.com>
2024-01-30 09:51:41 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 176 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,112 +1,114 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-
+// 시간 복잡도
 public class Main {
 
-    static int[][] map;
-    static int[] dice = new int[6];
-    static int N;
-    static int M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int m;
     static int x;
     static int y;
-    static int K;
+    static int k;
+    static int[][] map;
+    static int[] dice = new int[6];
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 세로 길이
-        M = Integer.parseInt(st.nextToken()); // 가로 길이
-        map = new int[N][M];
-        y = Integer.parseInt(st.nextToken()); // 세로 좌표
-        x = Integer.parseInt(st.nextToken()); // 가로 좌표
-        K = Integer.parseInt(st.nextToken());
-
-
-        // 지도 초기화
-        for(int i = 0; i < N; i++) {
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++) {
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 명령
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < K; i++) {
-            int direction = Integer.parseInt(st.nextToken());
-            move(direction);
+        for (int i = 0; i < k; i++) {
+            int command = Integer.parseInt(st.nextToken());
+            if (command == 1) {
+                if (y + 1 >= m) continue;
+                moveRight();
+                check();
+            }
+            if (command == 2) {
+                if (y - 1 < 0) continue;
+                moveLeft();
+                check();
+            }
+            if (command == 3) {
+                if (x - 1 < 0) continue;
+                moveUp();
+                check();
+            }
+
+            if (command == 4) {
+                if (x + 1 >= n) continue;
+                moveDown();
+                check();
+            }
+            System.out.println(dice[0]);
         }
     }
 
-    private static void move(int direction) {
-
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        // 이동 후 주사위의 위치
-        int nx = x + dx[direction - 1];
-        int ny = y + dy[direction - 1];
-
-        if(nx < 0 || nx >= M || ny < 0 || ny >= N) {
-            return;
-        }
-
-        x = nx;
-        y = ny;
-        roll(direction);
-
-    }
-
-    private static void roll(int direction) {
-
-        switch (direction) {
-            case 1:
-                // 오른쪽으로 이동
-                int temp = dice[0];
-                dice[0] = dice[4];
-                dice[4] = dice[1];
-                dice[1] = dice[5];
-                dice[5] = temp;
-                break;
-
-            case 2:
-                // 왼쪽으로 이동
-                temp = dice[0];
-                dice[0] = dice[5];
-                dice[5] = dice[1];
-                dice[1] = dice[4];
-                dice[4] = temp;
-                break;
-
-            case 3:
-                // 위으로 이동
-                temp = dice[0];
-                dice[0] = dice[3];
-                dice[3] = dice[1];
-                dice[1] = dice[2];
-                dice[2] = temp;
-                break;
-
-            case 4:
-                // 아래로 이동
-                temp = dice[0];
-                dice[0] = dice[2];
-                dice[2] = dice[1];
-                dice[1] = dice[3];
-                dice[3] = temp;
-                break;
-        }
-
-        if(map[y][x] == 0)  {
-            map[y][x] = dice[0];
+    private static void check() {
+        if (map[x][y] == 0) {
+            map[x][y] = dice[2]; // 바닥면 -> map으로 복사
         } else {
-            dice[0] = map[y][x];
-            map[y][x] = 0;
+            dice[2] = map[x][y]; // map -> 바닥면으로 복사
+            map[x][y] = 0;
         }
+    }
 
-        System.out.println(dice[1]);
+    private static void moveRight() {
+        y++; // 주사위 위치 이동
+
+        // 주사위 값 이동
+        int temp = dice[0];
+        dice[0] = dice[4];
+        dice[4] = dice[2];
+        dice[2] = dice[5];
+        dice[5] = temp;
+    }
+
+    private static void moveLeft() {
+        y--; // 주사위 위치 이동
+
+        // 주사위 값 이동
+        int temp = dice[0];
+        dice[0] = dice[5];
+        dice[5] = dice[2];
+        dice[2] = dice[4];
+        dice[4] = temp;
+    }
+
+    private static void moveUp() {
+        x--; // 주사위 위치 이동
+
+        // 주사위 값 이동
+        int temp = dice[0];
+        dice[0] = dice[1];
+        dice[1] = dice[2];
+        dice[2] = dice[3];
+        dice[3] = temp;
+    }
+
+    private static void moveDown() {
+        x++; // 주사위 위치 이동
+
+        // 주사위 값 이동
+        int temp = dice[0];
+        dice[0] = dice[3];
+        dice[3] = dice[2];
+        dice[2] = dice[1];
+        dice[1] = temp;
     }
 }
