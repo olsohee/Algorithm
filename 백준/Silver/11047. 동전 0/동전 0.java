ffdf416fe59fe10fa13eaf86edaf6affcf5eb61a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-02-05 10:52:12 +0900

    [Silver IV] Title: 동전 0, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,36 +1,31 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
-
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
+// 시간 복잡도: O(N)
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int n;
     static int k;
     static int[] coins;
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        coins = new int[n];
-        for(int i = 0; i < n; i++) {
+        coins = new int[1001];
+        for (int i = 0; i < n; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
         int answer = 0;
-        for(int i = n - 1; i >= 0; i--) {
-            if(k == 0) {
-                break;
-            }
-            int coin = coins[i];
-            if(k >= coin) {
-                answer += k / coin;
-                k %= coin;
+        for (int i = n - 1; i >= 0; i--) {
+            if (coins[i] <= k) {
+                answer += k / coins[i];
+                k %= coins[i];
             }
         }
 
