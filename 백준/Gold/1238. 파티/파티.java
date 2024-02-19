b118e6a49756a65a55f64a7dfabefb87eb10b202
olsohee <108605017+olsohee@users.noreply.github.com>
2024-02-19 12:32:17 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,65 +3,63 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 시간 복잡도: ElogV = 10,000 * log1,000
 public class Main {
 
-    static int n, m, x;
-    static List<List<Node>> list = new ArrayList();
-    static List<List<Node>> reverseList = new ArrayList();
-    static int[] dist;
-    static int[] reverseDist;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+    static int m;
+    static int x;
+    static List<List<Node>> list = new ArrayList<>();
+    static List<List<Node>> reversList = new ArrayList<>();
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         x = Integer.parseInt(st.nextToken());
-        dist = new int[n + 1];
-        reverseDist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Arrays.fill(reverseDist, Integer.MAX_VALUE);
-
         for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
-            reverseList.add(new ArrayList<>());
+            reversList.add(new ArrayList<>());
         }
 
-        // 그래프 초기화
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-
             list.get(start).add(new Node(end, cost));
-            reverseList.get(end).add(new Node(start, cost));
+            reversList.get(end).add(new Node(start, cost));
         }
 
-        dijkstra(x, list, dist);
-        dijkstra(x, reverseList, reverseDist);
+        // 다익스트라
+        int[] dist = dijkstra(x, list); // x -> i
+        int[] reverseDist = dijkstra(x, reversList); // i -> x
 
-        int maxSum = -1;
+        // 결과 계산
         for (int i = 1; i <= n; i++) {
-            maxSum = Math.max(maxSum, dist[i] + reverseDist[i]);
+            answer = Math.max(answer, dist[i] + reverseDist[i]);
         }
-        System.out.println(maxSum);
+
+        System.out.println(answer);
     }
 
-    private static void dijkstra(int start, List<List<Node>> list, int[] dist) {
-        PriorityQueue<Node> que = new PriorityQueue<>();
+    private static int[] dijkstra(int start, List<List<Node>> list) {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Queue<Node> que = new PriorityQueue<>();
         boolean[] visited = new boolean[n + 1];
+
+        que.add(new Node(start, 0)); // start까지의 비용 = 0
         dist[start] = 0;
-        que.add(new Node(start, 0));
 
         while (!que.isEmpty()) {
             int now = que.poll().end;
-            if (visited[now]) {
-                continue;
-            }
+
+            if (visited[now]) continue;
             visited[now] = true;
+
             for (Node node : list.get(now)) {
                 if (dist[node.end] > dist[now] + node.cost) {
                     dist[node.end] = dist[now] + node.cost;
@@ -69,8 +67,9 @@ public class Main {
                 }
             }
         }
-    }
 
+        return dist;
+    }
 
     static class Node implements Comparable<Node> {
         int end, cost;
@@ -82,7 +81,7 @@ public class Main {
 
         @Override
         public int compareTo(Node o) {
-            return this.cost - o.cost;
+            return cost - o.cost;
         }
     }
 }
