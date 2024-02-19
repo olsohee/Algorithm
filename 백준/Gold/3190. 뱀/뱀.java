c3f588938a06a3eb5b5c9014e7773064e6921745
olsohee <108605017+olsohee@users.noreply.github.com>
2024-02-19 10:59:38 +0900

    [Gold IV] Title: 뱀, Time: 152 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -12,11 +12,8 @@ public class Main {
     static int l;
     static int[][] map;
     static int time;
-    static int[] times;
-    static String[] dirs;
+    static Map<Integer, String> turnMap = new HashMap<>();
     static List<Position> positions = new ArrayList<>();
-    static int startY;
-    static int startX;
     static int[] dx = {1, 0, -1, 0}; // dir: 0(오), 1(아래), 2(왼), 3(위)
     static int[] dy = {0, 1, 0, -1};
 
@@ -31,67 +28,66 @@ public class Main {
         }
 
         l = Integer.parseInt(br.readLine());
-        times = new int[l];
-        dirs = new String[l];
         for (int i = 0; i < l; i++) {
             st = new StringTokenizer(br.readLine());
-            times[i] = Integer.parseInt(st.nextToken());
-            dirs[i] = st.nextToken();
+            turnMap.put(Integer.parseInt(st.nextToken()), st.nextToken());
         }
 
-        startX = 1;
-        startY = 1;
-        positions.add(new Position(startY, startX));
+        positions.add(new Position(1, 1));
         time = 0;
         int dir = 0;
         while (true) {
             int nx = positions.get(0).x + dx[dir];
             int ny = positions.get(0).y + dy[dir];
 
-            // 그래프 범위 확인
-            if (nx <= 0 || nx > n || ny <= 0 || ny > n) break;
-
-            if (map[ny][nx] == -1) {
+            // 이동 가능한지 검증
+            if (isFinish(ny, nx)) {
                 break;
             }
+
             // 이동
             if (map[ny][nx] == 1) {
                 // 사과가 있는 경우
-                map[ny][nx] = -1;
                 positions.add(0, new Position(ny, nx)); // 앞머리 추가
+                map[ny][nx] = 0;
             } else {
                 // 사과가 없는 경우
-
-                map[ny][nx] = -1;
-                map[positions.get(positions.size() - 1).y][positions.get(positions.size() - 1).x] = 0;
                 positions.remove(positions.size() - 1); // 꼬리 제거
                 positions.add(0, new Position(ny, nx)); // 앞머리 추가
             }
+
+            // 시간 추가
             time++;
 
-            // 방향 회전 판단
-            for (int i = 0; i < l; i++) {
-                if (time == times[i]) {
-                    if (dirs[i].equals("L")) {
-                        if (dir == 0) {
-                            dir = 3;
-                        } else {
-                            dir -= 1;
-                        }
-                    }
-                    if (dirs[i].equals("D")) {
-                        if (dir == 3) {
-                            dir = 0;
-                        } else {
-                            dir += 1;
-                        }
-                    }
-                    break;
+            // 방향 회전 O/X
+            if (turnMap.containsKey(time)) {
+                if (turnMap.get(time).equals("L")) {
+                    dir -= 1;
+                    if (dir == -1) dir = 3;
+                } else {
+                    dir += 1;
+                    if (dir == 4) dir = 0;
                 }
             }
         }
 
         System.out.println(time + 1);
+    }
+
+    private static boolean isFinish(int ny, int nx) {
+        // 그래프 범위 확인
+        if (nx <= 0 || nx > n || ny <= 0 || ny > n) {
+            return true;
+        }
+
+        for (int i = 0; i < positions.size(); i++) {
+            Position position = positions.get(i);
+            if (position.y == ny && position.x == nx) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     static class Position {
