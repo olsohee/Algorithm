8c889d83a774ae6581bddc48e3ceea769bbf2fa3
olsohee <108605017+olsohee@users.noreply.github.com>
2024-02-27 11:44:27 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.66 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,47 +1,43 @@
 import java.util.*;
 
-/*
-- 파기해야 할 개인정보의 번호를 "오름차순"으로
- */
+    // 파기해야 할 개인정보 번호를 오름차순으로 
+    class Solution {
+       
+        public int[] solution(String today, String[] terms, String[] privacies) {
 
-class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
+            String[] arr = today.split("\\.");
+            int todayNum = Integer.parseInt(arr[0]) * 12 * 28 + Integer.parseInt(arr[1]) * 28 + Integer.parseInt(arr[2]);
 
-        Map<String, Integer> termsMap = new HashMap<>();
-        List<Integer> answerList = new ArrayList<>();
-
-        for (int i = 0; i < terms.length; i++) {
-            String term = terms[i].split(" ")[0];
-            Integer date = Integer.parseInt(terms[i].split(" ")[1]);
-            termsMap.put(term, date);
-        }
-
-        // 파기 날짜 계산 + 오늘 날짜와 비교
-        int todayDate = 0;
-        todayDate += Integer.parseInt(today.split("\\.")[0]) * 12 * 28;
-        todayDate += Integer.parseInt(today.split("\\.")[1]) * 28;
-        todayDate += Integer.parseInt(today.split("\\.")[2]);
-
-        for (int i = 0; i < privacies.length; i++) {
-            String createDate = privacies[i].split(" ")[0];
-            String term = privacies[i].split(" ")[1];
-
-            int createDateInt = 0;
-            createDateInt += Integer.parseInt(createDate.split("\\.")[0]) * 12 * 28;
-            createDateInt += Integer.parseInt(createDate.split("\\.")[1]) * 28;
-            createDateInt += Integer.parseInt(createDate.split("\\.")[2]);
-
-            int deleteDate = createDateInt + termsMap.get(term) * 28;
-
-            if (deleteDate <= todayDate) {
-                answerList.add(i + 1);
+            Map<String, Integer> map = new HashMap<>();
+            for (String term : terms) {
+                String[] termArr = term.split(" ");
+                map.put(termArr[0], Integer.parseInt(termArr[1]));
             }
-        }
 
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
-        return answer;
-    }
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < privacies.length; i++) {
+                String[] privacy = privacies[i].split(" ");
+
+                String[] dayArr = privacy[0].split("\\.");
+                int year = Integer.parseInt(dayArr[0]);
+                int month = Integer.parseInt(dayArr[1]);
+                int day = Integer.parseInt(dayArr[2]);
+
+                int num = year * 12 * 28 + month * 28 + day;
+                num += map.get(privacy[1]) * 28;
+
+                // 유효기간이 지났는지 확인
+                if (todayNum >= num) {
+                    list.add(i + 1);
+                }
+
+            }
+            Collections.sort(list);
+            int[] answer = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                answer[i] = list.get(i);
+            }
+            return answer;
+        } 
+    
 }
