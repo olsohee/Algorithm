7cc68e2df246110db2ce51aa4cd7caba63331fd5
olsohee <108605017+olsohee@users.noreply.github.com>
2024-02-27 11:52:53 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.57 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,42 +2,45 @@ import java.util.*;
 
     // 파기해야 할 개인정보 번호를 오름차순으로 
     class Solution {
-       
+ 
+        
         public int[] solution(String today, String[] terms, String[] privacies) {
-
-            String[] arr = today.split("\\.");
-            int todayNum = Integer.parseInt(arr[0]) * 12 * 28 + Integer.parseInt(arr[1]) * 28 + Integer.parseInt(arr[2]);
 
             Map<String, Integer> map = new HashMap<>();
             for (String term : terms) {
-                String[] termArr = term.split(" ");
-                map.put(termArr[0], Integer.parseInt(termArr[1]));
+                String name = term.split(" ")[0];
+                Integer date = Integer.parseInt(term.split(" ")[1]);
+                map.put(name, date);
             }
+
+            int year = Integer.parseInt(today.split("\\.")[0]);
+            int month = Integer.parseInt(today.split("\\.")[1]);
+            int day = Integer.parseInt(today.split("\\.")[2]);
+            int todayNum = year * 12 * 28 + month * 28 + day;
+
 
             List<Integer> list = new ArrayList<>();
             for (int i = 0; i < privacies.length; i++) {
                 String[] privacy = privacies[i].split(" ");
 
-                String[] dayArr = privacy[0].split("\\.");
-                int year = Integer.parseInt(dayArr[0]);
-                int month = Integer.parseInt(dayArr[1]);
-                int day = Integer.parseInt(dayArr[2]);
+                int y = Integer.parseInt(privacy[0].split("\\.")[0]);
+                int m = Integer.parseInt(privacy[0].split("\\.")[1]);
+                int d = Integer.parseInt(privacy[0].split("\\.")[2]);
 
-                int num = year * 12 * 28 + month * 28 + day;
-                num += map.get(privacy[1]) * 28;
+                int n = y * 12 * 28 + m * 28 + d + map.get(privacy[1]) * 28;
 
                 // 유효기간이 지났는지 확인
-                if (todayNum >= num) {
+                if (todayNum >= n) {
                     list.add(i + 1);
                 }
 
             }
-            Collections.sort(list);
+            Collections.sort(list); // 오름차순 정렬
             int[] answer = new int[list.size()];
             for (int i = 0; i < list.size(); i++) {
                 answer[i] = list.get(i);
             }
             return answer;
-        } 
+        }
     
 }
