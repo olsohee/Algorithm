b85775821ca30331d06da6cb3019ab7011aaf901
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-01 12:12:47 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,37 +1,23 @@
 import java.util.*;
 class Solution {
      public int[] solution(int[] progresses, int[] speeds) {
-            // 각 작업이 완성되는데 걸리는 시간 계산
-            int[] days = new int[progresses.length];
+
+            Queue<Integer> q = new LinkedList<>();
+            List<Integer> answerList = new ArrayList<>();
+
             for (int i = 0; i < progresses.length; i++) {
-                int day = (100 - progresses[i]) / speeds[i];
-                if ((100 - progresses[i]) % speeds[i] != 0) {
-                    day++;
+                // 각 작업이 완성되는데 걸리는 시간 계산
+                int day = (int)Math.ceil((100 - progresses[i]) / (double)speeds[i]);
+
+                // 배포 날짜 계산
+                if (!q.isEmpty() && q.peek() < day) {
+                    answerList.add(q.size());
+                    q.clear();
                 }
-                days[i] = day;
+                q.add(day);
             }
 
-            // 배포 날짜 계산
-            Stack<Integer> stack = new Stack<>();
-            List<Integer> answerList = new ArrayList<>();
-            for (int i = 0; i < days.length; i++) {
-                // 스택이 비어있으면 배포 시작
-                if (stack.isEmpty()) {
-                    stack.add(days[i]);
-                    answerList.add(1);
-                    continue;
-                }
-                // peek한 값이 현재값보다 크거나 같으면 함께 배포
-                if (stack.peek() >= days[i]) {
-                    Integer num = answerList.get(answerList.size() - 1);
-                    answerList.remove(answerList.size() - 1);
-                    answerList.add(num + 1);
-                } else {
-                    // peek한 값이 현재값보다 작으면 새로 배포 시작
-                    stack.add(days[i]);
-                    answerList.add(1);
-                }
-            }
+            answerList.add(q.size());
 
             int[] answer = new int[answerList.size()];
             for (int i = 0; i < answerList.size(); i++) {
