7241730acbac107e478af6375edc90a7a296fe5e
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-03 18:26:42 +0900

    [level 2] Title: 프로세스, Time: 0.73 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,21 +1,18 @@
 import java.util.*;
 
 class Solution {
-       public int solution(int[] priorities, int location) {
+        public int solution(int[] priorities, int location) {
             Queue<Info> que = new LinkedList<>();
             for (int i = 0; i < priorities.length; i++) {
                 que.add(new Info(priorities[i], i));
             }
-            List<Integer> list = new ArrayList<>();
-            for (int i : priorities) {
-                list.add(i);
-            }
-            Collections.sort(list, Collections.reverseOrder());
-
-            int idx = 0;
-            int maxPri = list.get(idx);
+            Arrays.sort(priorities);
+          
+            int idx = priorities.length - 1;
+            int maxPri; 
             int answer = 0;
             while (!que.isEmpty()) {
+                maxPri = priorities[idx];
                 Info info = que.poll();
                 // 우선순위의 숫자가 아니면 다시 큐에 넣기
                 if (info.priority != maxPri) {
@@ -27,12 +24,12 @@ class Solution {
                 if (info.location == location) {
                     break;
                 }
-                maxPri = list.get(++idx);
+                idx--;
             }
-            
+
             return answer;
         }
-        
+
         class Info {
             int priority;
             int location;
