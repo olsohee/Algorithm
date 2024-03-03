3fed6179dca18dc1643e7ce33a2bdfad47a9a08e
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-03 19:02:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.97 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,49 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
+            public String solution(String[] participant, String[] completion) {
 
-        String answer = "";
-
-        // map에 이름과 몇명인지 저장
-        Map<String, Integer> map = new HashMap<>();
-        for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
-        }
-
-        // 완주한 사람들 map에서 제거
-        for (String name : completion) {
-            map.put(name, map.get(name) - 1);
-        }
-
-        // 완주하지 못한 사람 출력
-        for(String key : map.keySet()) {
-            if(map.get(key) >= 1) {
-                answer += key;
+            // Map에 참여자 저장
+            Map<String, Integer> map = new HashMap();
+            for (String s : participant) {
+                map.put(s, map.getOrDefault(s, 0) + 1);
             }
+            
+            // 완주한 선수 제거
+            for (String s : completion) {
+                map.put(s, map.get(s) - 1);
+            }
+            
+            // 남은 선수는 완주하지 못한 선수
+            for (String key : map.keySet()) {
+                if (map.get(key) > 0) {
+                    return key;
+                }
+            }
+            
+            return "";
         }
-        return answer;
-    }
+//     public String solution(String[] participant, String[] completion) {
+
+//         String answer = "";
+
+//         // map에 이름과 몇명인지 저장
+//         Map<String, Integer> map = new HashMap<>();
+//         for (String name : participant) {
+//             map.put(name, map.getOrDefault(name, 0) + 1);
+//         }
+
+//         // 완주한 사람들 map에서 제거
+//         for (String name : completion) {
+//             map.put(name, map.get(name) - 1);
+//         }
+
+//         // 완주하지 못한 사람 출력
+//         for(String key : map.keySet()) {
+//             if(map.get(key) >= 1) {
+//                 answer += key;
+//             }
+//         }
+//         return answer;
+//     }
 }
