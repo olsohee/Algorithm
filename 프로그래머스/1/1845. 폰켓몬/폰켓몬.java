9761868e689e44b02504b5e178a1b365ece092fd
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-06 10:30:12 +0900

    [level 1] Title: 폰켓몬, Time: 4.98 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            int typeCnt = set.size();
+            if (typeCnt >= nums.length / 2) {
+                return nums.length / 2;
+            } else {
+                return typeCnt;
+            }
+        }
+//     public int solution(int[] nums) {
+//          HashSet<Integer> hashSet = new HashSet<>();
+
+//         // 중복을 제거하여 저장
+//         for(int i = 0; i < nums.length; i++) {
+//             hashSet.add(nums[i]);
+//         }
+
+//         if(hashSet.size() >= nums.length / 2) {
+//             return nums.length / 2;
+//         } else {
+//             return hashSet.size();
+//         }
+//     }
+}
