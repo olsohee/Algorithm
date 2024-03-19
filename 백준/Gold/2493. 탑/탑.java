573655ed85aa9b9610622da6b3b9d2ddfe186946
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-19 14:15:46 +0900

    [Gold V] Title: 탑, Time: 3040 ms, Memory: 130112 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -15,52 +15,49 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         answer = new int[n + 1];
         arr = new int[n + 1];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = Integer.parseInt(st.nextToken());
         }
+        Stack<Tower> stack = new Stack<>();
 
-        // 1. 스택이 비어있으면, 답은 0 + 현재 값 push
-        // 2. 스택이 비어있지 않으면, peek한 값과 현재 값 비교
-        // 2-1. peek한 값이 더 크면, 답은 peek한 값의 인덱스 + 현재 값 push
-        // 2-2. 그렇지 않으면, peek한 값을 pop하고 2번으로
-
-        Stack<Node> stack = new Stack<>();
-        for (int i = 1; i <= n; i++) {
-            int now = arr[i];
+        for (int i = 0; i <= n; i++) {
             if (stack.isEmpty()) {
-                answer[i] = 0;
-                stack.push(new Node(now, i));
-            } else {
-                while (true) {
-                    if (stack.isEmpty()) {
-                        answer[i] = 0;
-                        stack.push(new Node(now, i));
-                        break;
-                    }
-                    if (stack.peek().num > now) {
-                        answer[i] = stack.peek().idx;
-                        stack.push(new Node(now, i));
-                        break;
-                    } else {
+                stack.push(new Tower(arr[i], i));
+                continue;
+            }
+
+            // peek한 값이 더 크면 peek한 값에 신호가 닿고 스택에 넣기
+            if (stack.peek().num > arr[i]) {
+                answer[i] = stack.peek().idx;
+                stack.push(new Tower(arr[i], i));
+            }
+
+            // peek한 값이 더 작거나 같으면 신호가 닿을 때까지 pop
+            else {
+                while (!stack.isEmpty()) {
+                    if (stack.peek().num <= arr[i]) {
                         stack.pop();
+                    } else {
+                        answer[i] = stack.peek().idx;
+                        break;
                     }
                 }
+                stack.push(new Tower(arr[i], i));
             }
         }
 
-        for (int i = 1; i < answer.length; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.print(answer[i] + " ");
         }
     }
 
-    static class Node {
-        int num, idx;
-
-        public Node(int num, int idx) {
+    public static class Tower {
+        int num;
+        int idx;
+        public Tower (int num, int idx) {
             this.num = num;
             this.idx = idx;
         }
