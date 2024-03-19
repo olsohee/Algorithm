4e83941a7e20d679f6dd43fa36ee4108a042ae83
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-19 11:41:48 +0900

    [level 1] Title: 폰켓몬, Time: 10.81 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,29 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-            Set<Integer> set = new HashSet<>();
-            for (int num : nums) {
-                set.add(num);
-            }
-            int typeCnt = set.size();
-            if (typeCnt >= nums.length / 2) {
-                return nums.length / 2;
-            } else {
-                return typeCnt;
+        
+        // Map에 종류 번호, 갯수 저장
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        // Map 돌면서 1개씩 채우기 (개수가 1 이상인 경우만)
+        int sum = 0;
+        Set<Integer> choiceNum = new HashSet<>();
+        outer: while (true) {
+            for (int num : map.keySet()) {
+                
+                if (sum >= nums.length / 2) {
+                    break outer;
+                }
+            
+                int cnt = map.get(num);
+                if (cnt >= 1) {
+                    sum++;
+                    map.put(num, cnt - 1);
+                    choiceNum.add(num);
+                }
             }
         }
-//     public int solution(int[] nums) {
-//          HashSet<Integer> hashSet = new HashSet<>();
-
-//         // 중복을 제거하여 저장
-//         for(int i = 0; i < nums.length; i++) {
-//             hashSet.add(nums[i]);
-//         }
-
-//         if(hashSet.size() >= nums.length / 2) {
-//             return nums.length / 2;
-//         } else {
-//             return hashSet.size();
-//         }
-//     }
+        
+        return choiceNum.size();
+    }
 }
