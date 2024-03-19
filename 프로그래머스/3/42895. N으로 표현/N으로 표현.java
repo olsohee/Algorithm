5eb8403c70cc3b645a287dec9a5f2239307c2092
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-19 13:30:35 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,41 +2,44 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-            
-        List<Set<Integer>> dp = new ArrayList();
-        for (int i = 0; i <= 8; i++) {
-            dp.add(new HashSet<Integer>());
+        
+        if (N == number) {
+            return 1;
         }
         
-        dp.get(1).add(N);
-        if (number == N) return 1;
+        List<Set<Integer>> dp = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) {
+            dp.add(new HashSet<>());
+        }
         
-        for (int i = 2; i <= 8; i++) {
+        for (int i = 1; i <= 8; i++) {
+            // i개로 만들 수 있는 숫자들의 set
             Set<Integer> set = dp.get(i);
-            // dp.get(i)의 set에 연산 결과 담기
+            
+            String str = "";
+            for (int j = 0; j < i; j++) {
+                str += N;
+            }
+            set.add(Integer.parseInt(str));
+            
+            // j개로 만드는 수 X k개로 만드는 수를 set에 넣기
             for (int j = 1; j < i; j++) {
                 int k = i - j;
                 for (int num1 : dp.get(j)) {
                     for (int num2 : dp.get(k)) {
-                        dp.get(i).add(num1 + num2);
-                        dp.get(i).add(num1 - num2);
-                        dp.get(i).add(num1 * num2);
-                        if (num2 != 0) {
-                            dp.get(i).add(num1 / num2);
-                        }
+                        set.add(num1 + num2);
+                        set.add(num1 - num2);
+                        set.add(num1 * num2);
+                        if (num2 != 0) set.add(num1 / num2);
                     }
                 }
             }
-            
-            String s = "";
-            for (int j = 0; j < i; j++) {
-                s += N;
-            }
-            dp.get(i).add(Integer.parseInt(s));
-            
-            // dp.get(i)의 set에 number가 있는지 확인
-            if (dp.get(i).contains(number)) {
-                return i;
+                     
+            // set에 number이 있는지 확인
+            for (int num : set) {
+                if (num == number) {
+                    return i;
+                }
             }
         }
         
