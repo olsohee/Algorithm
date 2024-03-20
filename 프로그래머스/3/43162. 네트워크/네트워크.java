f0f67f7fd623434c51e7d60547a2c9f4bb9e445d
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-20 14:51:19 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,39 @@
 import java.util.*;
 
 class Solution {
-        int answer;
-        int n;
-        int[][] computers;
-        boolean[] visited;
-
-        public int solution(int n, int[][] computers) {
-            this.n = n;
-            this.computers = computers;
-            visited = new boolean[n];
-
-            for (int i = 0; i < n; i++) {
-                if (!visited[i]) {
-                    dfs(i);
+    
+    int n;
+    int[][] computers;
+    boolean[] visited;
+    int answer = 0;
+    
+    public int solution(int n, int[][] computers) {
+        
+        this.n = n;
+        this.computers = computers;
+        this.visited = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (computers[i][j] == 1 && !visited[j]) {
+                    dfs(j);
                     answer++;
                 }
             }
-
-            return answer;
         }
-
-        private void dfs(int start) {
-            visited[start] = true;
-
-            for (int i = 0; i < n; i++) {
-                if (computers[start][i] == 1 && !visited[i]) {
-                    dfs(i);
-                }
+        
+        // 네트워크 개수
+        return answer;
+    }
+    
+    public void dfs(int start) {
+        
+        visited[start] = true;
+        
+        for (int i = 0; i < n; i++) {
+            if (computers[start][i] == 1 && !visited[i]) {
+                dfs(i);
             }
         }
+    }
 }
