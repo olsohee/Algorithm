05e08eae1b25916c60a7b0dffe7003e15301a313
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-20 19:23:00 +0900

    [level 3] Title: 정수 삼각형, Time: 9.45 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,38 +1,35 @@
 import java.util.*;
 
 class Solution {
- 
     public int solution(int[][] triangle) {
-            int n = triangle.length;
-            int[][] arr = new int[n][n];
-            int[][] dp = new int[n][n];
-            for (int i = 0; i < triangle.length; i++) {
-                arr[i] = triangle[i];
+        
+        int y = triangle.length;
+        int x = triangle[triangle.length - 1].length;
+        
+        int[][] arr = new int[y + 1][x + 1];
+        int[][] dp = new int[y + 1][x + 1];
+        
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                arr[i + 1][j + 1] = triangle[i][j];
             }
-//            for (int[] ints : arr) {
-//                for (int anInt : ints) {
-//                    System.out.print(anInt + " ");
-//                }
-//                System.out.println();
-//            }
-
-            // dp 초기화
-            dp[0][0] = arr[0][0];
-            for (int i = 1; i < n; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0) {
-                        dp[i][j] = arr[i][j] + dp[i - 1][j];
-                        continue;
-                    }
-                    dp[i][j] = arr[i][j] + Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
-                }
-            }
-
-            // 최댓값 찾기
-            int answer = 0;
-            for (int i = 0; i < n; i++) {
-                answer = Math.max(answer, dp[n - 1][i]);
-            }
-            return answer;
         }
+        
+        dp[1][1] = arr[1][1];
+        for (int i = 2; i < y + 1; i++) {
+            for (int j = 1; j < x + 1; j++) {
+                // (위에 dp OR 왼쪽 위 dp) + 현재 자리 arr
+                int max = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + arr[i][j];
+                dp[i][j] = Math.max(dp[i][j], max);
+            }
+        }
+        
+        // 마지막 줄에서 최댓값 찾기
+        int answer = 0;
+        for (int i = 1; i < x; i++) {
+            answer = Math.max(answer, dp[y][i]);
+        }
+        
+        return answer;
+    }
 }
