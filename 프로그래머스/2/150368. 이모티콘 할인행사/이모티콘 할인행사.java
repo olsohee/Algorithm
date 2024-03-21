8e3512c57b6c3950640d3465854a2146bb340385
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-21 15:14:45 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.52 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,88 +1,98 @@
 import java.util.*;
 
 class Solution {
-
-    List<int[]> combination = new ArrayList<>();
+    
     int[][] users;
     int[] emoticons;
-
+    int n;
+    int m;
+    List<int[]> sales = new ArrayList<>();
+    
     public int[] solution(int[][] users, int[] emoticons) {
-
         this.users = users;
         this.emoticons = emoticons;
-
-        // 재귀로 조합 만들기
-        dfs(1, new int[emoticons.length + 1]);
-
-        // 조합별로 구매 + 가입 계산
-        List<Result> results = new ArrayList<>();
-        for (int[] combi : combination) {
-            results.add(purchase(combi));
-        }
-
-        // 가입자수 내림차순 정렬, 총금액 내림차순 정렬
-        Collections.sort(results, new Result(1, 1));
-
-        Result result = results.get(0);
-        return new int[]{result.join, result.totalPrice};
-    }
-
-    private Result purchase(int[] combi) {
-
-        int join = 0; // 가입자 수
-        int totalPrice = 0; //구매금액
-
-        for (int[] user : users) {
-            int price = 0;
-
-            for(int i = 1; i <= emoticons.length; i++) {
-                int percent = combi[i];
-
-                if(user[0] <= percent) {
-                    int purchasePrice = emoticons[i - 1] * (100 - percent) / 100;
-                    price += purchasePrice;
+        
+        n = users.length;
+        m = emoticons.length;
+        
+        // 1. 각 이모티콘의 할인율의 경우의 수 (O(4^m)) (할인율: 10, 20, 30, 40%)
+        dfs(0, new int[m]);
+        
+        // 2. 해당 경우의 수에서 사용자 별로 플러스 가입 OR 이모티콘 구매 판단 (O(100))
+        List<Result> result = new ArrayList<>();
+        
+        // for (int[] arr : sales) {
+        //     for (int n : arr) {
+        //         System.out.print(n + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        // 세일 경우마다 Result(총 플러스 구매 수, 판매액)를 리스트에 저장
+        for (int[] sale : sales) {
+            int plus = 0;
+            int purchase = 0;
+            
+            // 사용자마다 판단
+            for (int[] user : users) {
+                int sum = 0;
+                for (int i = 0; i < m; i++) {
+                    int percent = sale[i];
+                    if (percent >= user[0]) {
+                        sum += emoticons[i] * (100 - percent) / 100;
+                    }
+                }
+                // 플러스 가입
+                if (sum >= user[1]) {
+                    plus++;
+                } 
+                // 이모티콘 구매
+                else {
+                    purchase += sum;
                 }
             }
-
-            if(price >= user[1]) {
-                join++;
-            } else {
-                totalPrice += price;
-            }
+            
+            result.add(new Result(plus, purchase));
         }
-
-        return new Result(join, totalPrice);
+        
+        // 플러스 가입 수, 매출액 반환 (1순위: 플러스 가입 수, 2순위: 매출액)
+        Collections.sort(result);
+        int[] answer = new int[2];
+        answer[0] = result.get(0).plus;
+        answer[1] = result.get(0).purchase;
+        return answer;
     }
-
+    
     public void dfs(int cnt, int[] arr) {
-        if(cnt == emoticons.length + 1) {
-            combination.add(arr);
+        
+        // 이모티콘의 할인률 모두 결정되면(m개) 끝내기
+        if (cnt == m) {
+            sales.add(arr);
             return;
         }
-
-        for(int i = 1; i <= 4; i++) {
+        
+        // cnt번째 이모티콘의 할인률 결정
+        for (int i = 1; i <= 4; i++) {
             int[] cloneArr = arr.clone();
             cloneArr[cnt] = i * 10;
             dfs(cnt + 1, cloneArr);
         }
     }
-
-    class Result implements Comparator<Result> {
-        int join;
-        int totalPrice;
-
-        public Result(int join, int totalPrice) {
-            this.join = join;
-            this.totalPrice = totalPrice;
+    
+    public class Result implements Comparable<Result> {
+        int plus;
+        int purchase;
+        public Result (int plus, int purchase) {
+            this.plus = plus;
+            this.purchase = purchase;
         }
-
+        
         @Override
-        public int compare(Result o1, Result o2) {
-            if(o1.join == o2.join) {
-                return o2.totalPrice - o1.totalPrice;
+        public int compareTo(Result o) {
+            if (o.plus == this.plus) {
+                return o.purchase - this.purchase;
             }
-
-            return o2.join - o1.join;
+            return o.plus - this.plus;
         }
     }
 }
