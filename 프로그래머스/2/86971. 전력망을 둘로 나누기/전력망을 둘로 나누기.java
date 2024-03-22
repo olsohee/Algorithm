4b2447ef51f29cac02682514803e194b904eecce
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-22 21:13:03 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.46 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,51 +2,52 @@ import java.util.*;
 
 class Solution {
     
+    int answer = Integer.MAX_VALUE;
     boolean[][] map;
     boolean[] visited;
     int n;
-    int answer = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
         
-        map = new boolean[n][n];
-        visited = new boolean[n];
         this.n = n;
         
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < wires.length; i++) {
             // 초기화
-            for (boolean[] arr : map) {
-                Arrays.fill(arr, false);
-            }
-            Arrays.fill(visited, false);
+            map = new boolean[n + 1][n + 1];
+            visited = new boolean[n + 1];
             
-            // map 채우기
-            for (int j = 0; j < n - 1; j++) {
-                if (i == j) continue;
-                int num1 = wires[j][0] - 1;
-                int num2 = wires[j][1] - 1;
-                map[num1][num2] = true;
-                map[num2][num1] = true;
+            for (int j = 0; j < wires.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int n1 = wires[j][0];
+                int n2 = wires[j][1];
+                map[n1][n2] = true;
+                map[n2][n1] = true;
             }
             
-            // map을 기반으로 dfs
-            int cnt = dfs(0);
-            answer = Math.min(answer, Math.abs(cnt - (n - cnt)));
+            // dfs
+            int result1 = dfs(1);
+            int result2 = n - result1;
+            
+            answer = Math.min(answer, Math.abs(result1 - result2));
         }
+        
+        // 개수 차이의 절대값
         return answer;
     }
     
-    public int dfs(int start) {
+    // 연결된 한 덩어리의 노드 개수 반환
+    public int dfs (int start) {
         
         visited[start] = true;
         int sum = 1;
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (map[start][i] && !visited[i]) {
                 sum += dfs(i);
             }
         }
-        
         return sum;
     }
 }
