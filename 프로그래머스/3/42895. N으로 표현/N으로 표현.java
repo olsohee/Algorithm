ec25707af0132bb1b6d5d170d8ef9d3df23c1c3c
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-22 20:31:32 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,41 +1,47 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int N, int number) {
         
-        if (N == number) {
-            return 1;
-        }
+        if (N == number) return 1;
         
-        List<Set<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) {
-            dp.add(new HashSet<>());
-        }
+        // i개로 만들 수 있는 수를 list에 저장
+        List<Set<Integer>> list = new ArrayList<>();
         
-        for (int i = 1; i <= 8; i++) {
-            // i개로 만들 수 있는 숫자들의 set
-            Set<Integer> set = dp.get(i);
+        // 1. dp 초기화
+        list.add(new HashSet<>()); // idx: 0
+        
+        Set<Integer> set = new HashSet<>();
+        set.add(N);
+        list.add(set); // idx: 1
+        
+        for (int i = 2; i <= 8; i++) {
+            // 2. dp에 저장
+            set = new HashSet<>();
             
-            String str = "";
+            String s = "";
             for (int j = 0; j < i; j++) {
-                str += N;
+                s += N;
             }
-            set.add(Integer.parseInt(str));
+            set.add(Integer.parseInt(s));
             
-            // j개로 만드는 수 X k개로 만드는 수를 set에 넣기
             for (int j = 1; j < i; j++) {
                 int k = i - j;
-                for (int num1 : dp.get(j)) {
-                    for (int num2 : dp.get(k)) {
+                for (int num1 : list.get(j)) {
+                    for (int num2 : list.get(k)) {
                         set.add(num1 + num2);
                         set.add(num1 - num2);
                         set.add(num1 * num2);
-                        if (num2 != 0) set.add(num1 / num2);
+                        if (num2 != 0) {
+                            set.add(num1 / num2);
+                        }
                     }
                 }
             }
-                     
-            // set에 number이 있는지 확인
+            list.add(set);
+            
+            // 3. number가 있는지 확인
             for (int num : set) {
                 if (num == number) {
                     return i;
