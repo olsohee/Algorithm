e0eda7b8a65770b6c1d9e6bb2ae341b31286ef0e
olsohee <108605017+olsohee@users.noreply.github.com>
2024-03-22 18:06:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.09 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,14 +4,15 @@ class Solution {
     
     int[] dx = {1, -1, 0, 0};
     int[] dy = {0, 0, 1, -1};
-        
+    
     public int solution(int[][] maps) {
         
-        // bfs로 이동 (0, 0에서 -> n, m으로)
-        int n = maps.length;
-        int m = maps[0].length;
+        int n = maps.length; // 세로
+        int m = maps[0].length; // 가로
+        
         int[][] visited = new int[n][m];
         
+        // (0, 0)에서 bfs 시작
         Queue<Node> que = new LinkedList<>();
         que.add(new Node(0, 0));
         visited[0][0] = 1;
@@ -19,31 +20,31 @@ class Solution {
         while (!que.isEmpty()) {
             Node node = que.poll();
             for (int i = 0; i < 4; i++) {
-                int ny = node.y + dy[i];
-                int nx = node.x + dx[i];
+                int nx = dx[i] + node.x;
+                int ny = dy[i] + node.y;
                 
                 if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
                     continue;
                 }
                 if (visited[ny][nx] != 0 || maps[ny][nx] == 0) {
                     continue;
-                }
+                } 
                 
-                visited[ny][nx] = visited[node.y][node.x] + 1;
                 que.add(new Node(ny, nx));
+                visited[ny][nx] = visited[node.y][node.x] + 1;
             }
         }
         
-        // 지나가는 최소 칸수 (도착 불가하면 -1)
+        // 지나는 칸의 최솟값 (불가능하면 -1)
         if (visited[n - 1][m - 1] == 0) {
             return -1;
-        }
+        } 
         return visited[n - 1][m - 1];
     }
     
     public class Node {
         int y, x;
-        public Node(int y, int x) {
+        public Node (int y, int x) {
             this.y = y;
             this.x = x;
         }
