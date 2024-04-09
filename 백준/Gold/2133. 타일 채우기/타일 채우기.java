cb8aff212b3442f6d8800b51c7e7a2b8bee4d42e
olsohee <108605017+olsohee@users.noreply.github.com>
2024-04-09 12:10:32 +0900

    [Gold IV] Title: 타일 채우기, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        
+        for (int i = 2; i <= n; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
