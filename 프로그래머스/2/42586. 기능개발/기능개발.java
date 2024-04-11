b760f887344164f33ecf1df10cad74d4e6a6fcbc
olsohee <108605017+olsohee@users.noreply.github.com>
2024-04-11 17:13:06 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,10 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int n = progresses.length;
-        int[] days = new int[n];
         
-        for (int i = 0; i < n; i++) {
+        List<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        int[] days = new int[progresses.length];
+        
+        // 1. 몇일 뒤에 배포되는지 계산
+        for (int i = 0; i < progresses.length; i++) {
             int day = (100 - progresses[i]) / speeds[i];
             if ((100 - progresses[i]) % speeds[i] != 0) {
                 day++;
@@ -13,34 +16,36 @@ class Solution {
             days[i] = day;
         }
         
-        List<Integer> list = new ArrayList<>();
-        
-        Stack<Integer> stack = new Stack<>();
-        stack.push(days[0]);
-        int max = days[0];
-        
-        for (int i = 1; i < n; i++) {
-            if (days[i] <= max) {
+        // 2. 배포 날짜를 스택에 반영
+        int maxDay = 0;
+        for (int i = 0; i < days.length; i++) {
+            if (stack.isEmpty()) {
                 stack.push(days[i]);
-            } else {
-                int cnt = 0;
-                while (!stack.isEmpty()) {
-                    stack.pop();
-                    cnt++;
-                }
-                list.add(cnt);
+                maxDay = days[i];
+                continue;
+            }
+            // peek한 값보다 작거나 같으면 스택에 넣기
+            if (maxDay >= days[i]) {
                 stack.push(days[i]);
-                max = days[i];
+            } 
+            
+            // peek한 값보다 크면 스택에 있는 것들 다 빼서 배포 + 현재 값 스택에 넣기
+            else {
+                list.add(stack.size());
+                stack.clear();
+                
+                stack.push(days[i]);
+                maxDay = days[i];
             }
         }
-        
+       
+        // 마지막에 스택에 남은 것들 다 빼서 배포
         list.add(stack.size());
         
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
-         
         return answer;
     }
 }
