d921874f507d46dca7d6ac8ae90091dbaf933386
olsohee <108605017+olsohee@users.noreply.github.com>
2024-04-12 13:39:28 +0900

    [level 1] Title: 폰켓몬, Time: 2.83 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,32 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        
-        // Map에 종류 번호, 갯수 저장
-        Map<Integer, Integer> map = new HashMap<>();
+        int goal = nums.length / 2;
+        Set<Integer> set = new HashSet<>(); // 포켓몬 종류를 담는 set
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            set.add(num);
         }
         
-        // Map 돌면서 1개씩 채우기 (개수가 1 이상인 경우만)
-        int sum = 0;
-        Set<Integer> choiceNum = new HashSet<>();
-        outer: while (true) {
-            for (int num : map.keySet()) {
-                
-                if (sum >= nums.length / 2) {
-                    break outer;
-                }
-            
-                int cnt = map.get(num);
-                if (cnt >= 1) {
-                    sum++;
-                    map.put(num, cnt - 1);
-                    choiceNum.add(num);
-                }
-            }
-        }
-        
-        return choiceNum.size();
+        if (goal <= set.size()) return goal;
+        else return set.size();
     }
 }
