9a4e264fa08145a4111bde6f6151f53b4ea0c94e
olsohee <108605017+olsohee@users.noreply.github.com>
2024-04-21 15:46:43 +0900

    [Gold III] Title: 허들 넘기, Time: 728 ms, Memory: 33556 KB -BaekjoonHub

---
+++ 백준/Gold/23286. 허들 넘기/허들 넘기.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i != j) {
+                    dist[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            dist[start][end] = cost;
+        }
+
+        // 플로이드
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] == Integer.MAX_VALUE || dist[k][j] == Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    if (dist[i][j] > Math.max(dist[i][k], dist[k][j])) {
+                        dist[i][j] = Math.max(dist[i][k], dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        // 출력
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            if (dist[start][end] == Integer.MAX_VALUE) {
+                System.out.println(-1);
+            } else {
+                System.out.println(dist[start][end]);
+            }
+        }
+    }
+}
