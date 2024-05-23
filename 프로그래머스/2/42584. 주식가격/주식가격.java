e8558eeb761a531abd14d8217925ee43caa9e4fe
olsohee <108605017+olsohee@users.noreply.github.com>
2024-05-23 12:33:39 +0900

    [level 2] Title: 주식가격, Time: 23.59 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,37 +3,29 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         
+        Stack<Integer> stack = new Stack<>();
         int[] answer = new int[prices.length];
         
-        Stack<Info> st = new Stack<>();
         for (int i = 0; i < prices.length; i++) {
-            if (st.isEmpty()) {
-                st.push(new Info(prices[i], i));
+            if (stack.isEmpty()) {
+                stack.push(i);    
                 continue;
             }
             
-            while (!st.isEmpty() && st.peek().num > prices[i]) {
-                Info p = st.pop();
-                answer[p.idx] = i - p.idx;
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
-            st.push(new Info(prices[i], i));
-       
+            
+            stack.push(i);
         }
         
-        while (!st.isEmpty()) {
-            Info p = st.pop();
-            answer[p.idx] = prices.length - 1 - p.idx;
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
         }
         
+        // 가격이 떨어지지 않은 시간
         return answer;
-    }
-    
-    public class Info {
-        int num;
-        int idx;
-        public Info(int num, int idx) {
-            this.num = num;
-            this.idx = idx;
-        }
     }
 }
