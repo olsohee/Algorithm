f0954b6beb28fbe9a721ca926634b7bfb6fde1cc
olsohee <108605017+olsohee@users.noreply.github.com>
2024-05-24 12:00:00 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.15 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,66 +1,44 @@
 import java.util.*;
 
 class Solution {
-    
-    int answer = 0;
-    
     public int solution(int[][] jobs) {
         
-        // que1 (요청순서대로 정렬)
-        Queue<Job> que1 = new PriorityQueue<>(new Comparator<>() {
-            @Override
-            public int compare(Job o1, Job o2) {
-                if (o1.request == o2.request) {
-                    return o1.duration - o2.duration;
-                }
-                return o1.request - o2.request;
-            }
-        });
+        PriorityQueue<Job> que = new PriorityQueue<>((o1, o2) -> o1.start - o2.start);
+        PriorityQueue<Job> ready = new PriorityQueue<>((o1, o2) -> o1.duration - o2.duration);
         
-        // que2 (작업시간대로 정렬)
-        Queue<Job> que2 = new PriorityQueue<>(new Comparator<>() {
-            @Override
-            public int compare(Job o1, Job o2) {
-                return o1.duration - o2.duration;
-            }
-        });
-        
-        // que1에 값 채우기
         for (int[] job : jobs) {
-            que1.add(new Job(job[0], job[1]));
+            que.add(new Job(job[0], job[1]));
         }
         
-        int now = 0;
-        while (!que1.isEmpty() || !que2.isEmpty()) {
-            // 1. 현재 시간을 기준으로 들어온 요청을 que1에서 que2로 옮기기  
-            while (!que1.isEmpty()) {
-                if (que1.peek().request <= now) {
-                    que2.add(que1.poll());
-                } else {
-                    break;
-                }
+        int cur = 0;
+        int sum = 0;
+        
+        while (!(que.isEmpty() && ready.isEmpty())) {
+            // 현재 시간까지 들어온 요청들 ready에 담기
+            while (!que.isEmpty() && que.peek().start <= cur) {
+                ready.add(que.poll());
             }
-
-            if (que2.isEmpty()) {
-                // 2. que2가 비어있다면, 아직 현재시간까지 들어온 요청이 없으므로 que1에서 peek한 요청 시간까지 현재 시간 보내기        
-                now = que1.peek().request;
+            
+            if (ready.isEmpty()) {
+                cur = que.peek().start;
             } else {
-                // 3. que2에서 작업시간이 가장 적은 1개 처리하기
-                Job job = que2.poll();
-                now += job.duration;
-                answer += (now - job.request);                
+                 Job job = ready.poll();
+                cur += job.duration;
+                sum += cur - job.start;
             }
+           
         }
-
-        return answer / jobs.length;
+        
+        return sum / jobs.length;
     }
     
     public class Job {
-        int request;
+        
+        int start;
         int duration;
         
-        public Job(int request, int duration) {
-            this.request = request;
+        public Job(int start, int duration) {
+            this.start = start;
             this.duration = duration;
         }
     }
