a5cf313a20cc626519c1b6e837360d3fd88391b1
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-03 21:02:18 +0900

    [level 3] Title: 네트워크, Time: 0.25 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,34 +2,30 @@ import java.util.*;
 
 class Solution {
     
+    int answer = 0;
     int n;
     int[][] computers;
     boolean[] visited;
-    int answer = 0;
     
     public int solution(int n, int[][] computers) {
-        
         this.n = n;
         this.computers = computers;
         this.visited = new boolean[n];
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (computers[i][j] == 1 && !visited[j]) {
-                    dfs(j);
+                if (computers[i][j] == 1 && !visited[i]) {
+                    dfs(i);
                     answer++;
                 }
             }
         }
         
-        // 네트워크 개수
         return answer;
     }
     
     public void dfs(int start) {
-        
         visited[start] = true;
-        
         for (int i = 0; i < n; i++) {
             if (computers[start][i] == 1 && !visited[i]) {
                 dfs(i);
