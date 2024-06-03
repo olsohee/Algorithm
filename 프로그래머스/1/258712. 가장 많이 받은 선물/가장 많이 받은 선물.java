813441ead590506efbe73b13f640be1d83038fca
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-03 21:26:45 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 4.49 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,72 +1,56 @@
 import java.util.*;
 
 class Solution {
-    
-    String[] friends;
-    
     public int solution(String[] friends, String[] gifts) {
+        int n = friends.length;
+        int[] giftScore = new int[n];
+        int[][] record = new int[n][n];
+        Map<String, Integer> idxMap = new HashMap<>();
         
-        this.friends = friends;
+        for (int i = 0; i < n; i++) {
+            idxMap.put(friends[i], i);
+        }
         
-        // map 초기화
-        int[][] map = new int[friends.length][friends.length];
+        // 주고받은 개수 계산
         for (String str : gifts) {
-            String name1 = str.split(" ")[0];
-            String name2 = str.split(" ")[1];
-            map[getIdx(name1)][getIdx(name2)]++;
+            String giver = str.split(" ")[0];
+            String receiver = str.split(" ")[1];
+            record[idxMap.get(giver)][idxMap.get(receiver)]++;
         }
         
         // 선물지수 계산
-        int[] giftScore = new int[friends.length];
-        for (int i = 0; i < friends.length; i++) {
+        for (int i = 0; i < n; i++) {
             int giveCnt = 0;
-            int getCnt = 0;
-            for (int j = 0; j < friends.length; j++) {
-                if (i == j) continue;
-                giveCnt += map[i][j];
-                getCnt += map[j][i];
+            int receiveCnt = 0;
+            for (int j = 0; j < n; j++) {
+                giveCnt += record[i][j];
+                receiveCnt += record[j][i];
             }
-            giftScore[i] = giveCnt - getCnt;
+            giftScore[i] = giveCnt - receiveCnt;
         }
         
-        // 다음 달에 받을 선물 수 계산
-        int[] result = new int[friends.length];
-        for (int i = 0; i < friends.length; i++) {
-            for (int j = i + 1; j < friends.length; j++) {
-                // i가 j에게 준 선물 수
-                int cnt1 = map[i][j];
-                // j가 i에게 준 선물 수
-                int cnt2 = map[j][i];
-                
-                if (cnt1 > cnt2) {
+        // 다음 달 계산
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (record[i][j] == record[j][i]) {
+                    if (giftScore[i] > giftScore[j]) {
+                        result[i]++;
+                    } else if (giftScore[i] < giftScore[j]) {
+                        result[j]++;
+                    }
+                } else if (record[i][j] > record[j][i]) {
                     result[i]++;
-                } else if (cnt1 < cnt2) {
+                } else if (record[i][j] < record[j][i]) {
                     result[j]++;
                 } 
-                // 주고받은 수가 같거나, 주고받지 않아서 둘 다 0인 경우
-                else {
-                    if (giftScore[i] == giftScore[j]) continue;
-                    if (giftScore[i] > giftScore[j]) {
-                       result[i]++;
-                    } else {
-                       result[j]++;
-                    }
-                }
             }
         }
         
-        // 선물을 가장 많이 받는 선물의 수 찾기
         int answer = 0;
-        for (int cnt : result) {
-            answer = Math.max(answer, cnt);
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, result[i]);
         }
         return answer;
-    }
-    
-    public int getIdx (String name) {
-        for (int i = 0; i < friends.length; i++) {
-            if (friends[i].equals(name)) return i;
-        }
-        return -1;
     }
 }
