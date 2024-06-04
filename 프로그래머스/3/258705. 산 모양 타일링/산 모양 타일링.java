ed3ab9119c66f8b3d3be06ccf52059c38167c595
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-04 12:49:03 +0900

    [level 3] Title: 산 모양 타일링, Time: 8.26 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,19 +1,21 @@
 import java.util.*;
 
 class Solution {
+    
+    static final int mod = 10007;
+    
     public int solution(int n, int[] tops) {
-        
         int[][] dp = new int[n][2];
-        dp[0][0] = tops[0] == 1 ? 3 : 2;
-        dp[0][1] = 1;
+        dp[0][0] = 1;
+        dp[0][1] = tops[0] == 1 ? 3 : 2;
         
         for (int i = 1; i < n; i++) {
-            int num1 = tops[i] == 1 ? 3 : 2;
-            int num2 = tops[i] == 1 ? 2 : 1;
-            dp[i][0] = (dp[i - 1][0] * num1 + dp[i - 1][1] * num2) % 10007; 
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][1]) % 10007;
+            int n1 = tops[i] == 1 ? 2 : 1;
+            int n2 = tops[i] == 1 ? 3 : 2;
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+            dp[i][1] = (dp[i - 1][0] * n1 + dp[i - 1][1] * n2) % mod;
         }
         
-        return (dp[n - 1][0] + dp[n - 1][1]) % 10007;
+        return (dp[n - 1][0] + dp[n - 1][1])  % mod;
     }
 }
