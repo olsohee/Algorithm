35d2f92b1ea45dc901000a5285f5c33139156530
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-07 11:22:42 +0900

    [level 3] Title: 정수 삼각형, Time: 7.49 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,34 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
         
-        int y = triangle.length;
-        int x = triangle[triangle.length - 1].length;
+        if (n == 1) return triangle[0][0];
         
-        int[][] arr = new int[y + 1][x + 1];
-        int[][] dp = new int[y + 1][x + 1];
+        dp[0][0] = triangle[0][0];
         
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < triangle[i].length; j++) {
-                arr[i + 1][j + 1] = triangle[i][j];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
             }
         }
         
-        dp[1][1] = arr[1][1];
-        for (int i = 2; i < y + 1; i++) {
-            for (int j = 1; j < x + 1; j++) {
-                // (위에 dp OR 왼쪽 위 dp) + 현재 자리 arr
-                int max = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + arr[i][j];
-                dp[i][j] = Math.max(dp[i][j], max);
-            }
-        }
-        
-        // 마지막 줄에서 최댓값 찾기
         int answer = 0;
-        for (int i = 1; i < x; i++) {
-            answer = Math.max(answer, dp[y][i]);
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dp[n - 1][i]);
         }
-        
         return answer;
     }
 }
