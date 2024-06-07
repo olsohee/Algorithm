1a60a86cc09e10c2c9f850e72b4c4845c3194fe2
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-07 12:27:25 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.11 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,57 +1,58 @@
-import java.util.*;
-
+import java.util.*; 
 
 class Solution {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        Map<Integer, List<Integer>> reverseMap = new HashMap<>();
-
-        public int[] solution(int[][] edges) {
-            int max = 0;
-            for (int i = 0; i < edges.length; i++) {
-                max = Math.max(Math.max(max, edges[i][0]), edges[i][1]);
-
-                List<Integer> list = map.getOrDefault(edges[i][0], new ArrayList<>());
-                list.add(edges[i][1]);
-                map.put(edges[i][0], list);
-
-                List<Integer> reverseList = reverseMap.getOrDefault(edges[i][1], new ArrayList<>());
-                reverseList.add(edges[i][0]);
-                reverseMap.put(edges[i][1], reverseList);
-            }
-
-            for (int i = 1; i <= max; i++) {
-                if (!map.containsKey(i)) {
-                    map.put(i, new ArrayList<>());
-                }
-
-                if (!reverseMap.containsKey(i)) {
-                    reverseMap.put(i, new ArrayList<>());
-                }
-            }
-
-            // 나가는게 2개 이상인 정점
-            int addPoint = 0;
-            int totalCnt = 0;
-            int eightCnt = 0;
-            for (Integer key : map.keySet()) {
-                if (map.get(key).size() > 1) {
-                    if (reverseMap.get(key).size() == 0) {
-                        addPoint = key;
-                        totalCnt = map.get(key).size();
-                    } else {
-                        eightCnt++;
-                    }
-                }
-            }
-
-            int barCnt = 0;
-            for (Integer key : map.keySet()) {
-                if (map.get(key).size() == 0) {
-                    barCnt++;
-                }
-            }
-
-            int donutCnt = totalCnt - eightCnt - barCnt;
-            return new int[]{addPoint, donutCnt, barCnt, eightCnt};
+    public int[] solution(int[][] edges) {
+        Map<Integer, List<Integer>> out = new HashMap<>();
+        Map<Integer, List<Integer>> in = new HashMap<>();
+        int max = 0;
+        for (int[] edge : edges) {
+            max = Math.max(max, Math.max(edge[0], edge[1]));
         }
+        
+        for (int i = 1; i <= max; i++) {
+            out.put(i, new ArrayList<>());
+            in.put(i, new ArrayList<>());
+        }
+        
+        for (int[] edge : edges) {
+            List<Integer> list1 = out.get(edge[0]);
+            list1.add(edge[1]);
+            out.put(edge[0], list1);
+            
+            List<Integer> list2 = in.get(edge[1]);
+            list2.add(edge[0]);
+            in.put(edge[1], list2);
+        }
+        
+        // 정점: 나가는 선이 2개 이상, 들어오는 선 없음
+        int node = 0;
+        for (int i = 1; i <= max; i++) {
+            if (out.get(i).size() >= 2 && in.get(i).size() == 0) {
+                node = i;
+                break;
+            }
+        }
+        int totalCnt = out.get(node).size();
+        
+        // 8자: 나가는 선 2개
+        int eight = 0;
+        for (int i = 1; i <= max; i++) {
+            if (i == node) continue;
+            if (out.get(i).size() == 2) {
+                eight++;
+            }
+        }
+        
+        // 막대: 나가는 선 없음
+        int bar = 0;
+        for (int i = 1; i <= max; i++) {
+            if (i == node) continue;
+            if (out.get(i).size() == 0 && in.get(i).size() != 0) {
+                bar++;
+            }
+        }
+        
+        // 생성한 정점 번호, 도넛 수, 막대 수, 8자 수 반환
+        return new int[]{node, totalCnt - (bar + eight), bar, eight};
+    }
 }
