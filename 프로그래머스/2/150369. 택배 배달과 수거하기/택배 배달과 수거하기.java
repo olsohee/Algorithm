67b4e6be881cd3acb8ab4b329c5127e58f26cdf0
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-09 02:04:35 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 4.28 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,37 +1,34 @@
-
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-
         long answer = 0;
-        int delivery = cap;
-        int pickup = cap;
-
-        for(int i = n - 1; i >= 0; i--) {
-            if(deliveries[i] != 0 || pickups[i] != 0) {
+        
+        // 시작점
+        for (int i = n - 1; i >= 0; i--) {
+            if (deliveries[i] != 0 || pickups[i] != 0) {
                 answer += (i + 1) * 2;
                 break;
             }
         }
-
-        for(int i = n - 1; i >= 0; i--) {
-            if(deliveries[i] == 0 && pickups[i] == 0) {
-                continue;
+        
+        int d = cap;
+        int p = cap;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            // System.out.println();
+            // System.out.println("i = " + i);
+            // System.out.println("d = " + d);
+            // System.out.println("p = " + p);
+            // System.out.println("answer = " + answer);
+            d -= deliveries[i];
+            p -= pickups[i];
+            
+            while (d < 0 || p < 0) {
+                d += cap;
+                p += cap;
+                answer += (i + 1) * 2;
             }
-//            System.out.println("===========");
-//            System.out.println("i = " + i);
-            delivery -= deliveries[i];
-            pickup -= pickups[i];
-
-            while (delivery < 0 || pickup < 0) {
-                answer += (i+1) * 2;
-                delivery += cap;
-                pickup += cap;
-            }
-//            System.out.println("delivery = " + delivery);
-//            System.out.println("pickup = " + pickup);
-//            System.out.println("answer = " + answer);
         }
-
+        
         return answer;
     }
 }
