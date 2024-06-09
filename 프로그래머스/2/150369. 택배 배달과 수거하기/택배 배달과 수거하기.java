33eefffa35a11713bd72debfeae16fa6b9768a2a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-09 15:53:53 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 5.65 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,32 +1,25 @@
+import java.util.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        
         long answer = 0;
-        
-        // 시작점
-        for (int i = n - 1; i >= 0; i--) {
-            if (deliveries[i] != 0 || pickups[i] != 0) {
-                answer += (i + 1) * 2;
-                break;
-            }
-        }
-        
-        int d = cap;
-        int p = cap;
+        int d = 0;
+        int p = 0;
         
         for (int i = n - 1; i >= 0; i--) {
-            // System.out.println();
-            // System.out.println("i = " + i);
-            // System.out.println("d = " + d);
-            // System.out.println("p = " + p);
-            // System.out.println("answer = " + answer);
             d -= deliveries[i];
             p -= pickups[i];
             
+            // d와 p 둘 중 하나라도 음수이면 물류창고 다녀와야 함
+            int cnt = 0;
             while (d < 0 || p < 0) {
                 d += cap;
                 p += cap;
-                answer += (i + 1) * 2;
+                cnt++;
             }
+            
+            answer += (i + 1) * cnt * 2;
         }
         
         return answer;
