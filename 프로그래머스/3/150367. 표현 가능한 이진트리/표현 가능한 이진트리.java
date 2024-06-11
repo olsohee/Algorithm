e8d1350c4f7ed53d985bef56a2389e0872103bb0
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-11 12:01:33 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 24.57 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -7,21 +7,21 @@ class Solution {
         
         for (int i = 0; i < numbers.length; i++) {
             String str = Long.toBinaryString(numbers[i]);
-            while (!isCollectLength(str)) {
+            while (!collectLength(str)) {
                 str = "0" + str;
             }
             
             boolean result = dfs(str);
             answer[i] = result ? 1 : 0;
         }
-          
+        
         return answer;
     }
     
-    public boolean isCollectLength(String str) {
-        int len = str.length() + 1; //len이 2의 제곱이어야 가능
+    public boolean collectLength(String str) {
+        int len = str.length() + 1; // len이 2의 제곱이어야 함
         int n = 1;
-        while (n < len) {
+        while(n < len) {
             n *= 2;
         }
         return len == n;
@@ -33,15 +33,16 @@ class Solution {
         }
         
         int mid = str.length() / 2;
-        String left = str.substring(0, mid);
-        String right = str.substring(mid + 1);
+        String leftTree = str.substring(0, mid);
+        String rightTree = str.substring(mid + 1);
         
+        // 루트 노드가 0이면 왼쪽, 오른쪽 모든 자식 노드도 0이어야 함
         if (str.charAt(mid) == '0') {
-            if (left.contains("1") || right.contains("1")) {
+            if (leftTree.contains("1") || rightTree.contains("1")) {
                 return false;
             }
         }
         
-        return dfs(left) && dfs(right);
+        return dfs(leftTree) && dfs(rightTree);
     }
 }
