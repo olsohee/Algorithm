2469234a6b1bb2b5d3ff2d7d3edb26950333d4c5
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-19 11:22:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 72.13 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,43 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        
+        int n = queue1.length * 2;
         Queue<Integer> que1 = new LinkedList<>();
         Queue<Integer> que2 = new LinkedList<>();
-        
+        long total = 0;
         long sum1 = 0;
         long sum2 = 0;
-        for (int num : queue1) {
-            sum1 += num;
-            que1.add(num);
+        
+        for (int i = 0; i < n / 2; i++) {
+            que1.add(queue1[i]);
+            sum1 += queue1[i];
+            
+            que2.add(queue2[i]);
+            sum2 += queue2[i];
+            
+            total += queue1[i] + queue2[i];
         }
         
-        for (int num : queue2) {
-            sum2 += num;
-            que2.add(num);
-        }
-        
-        // 합이 큰 큐에서 poll하기
-        long goal = (sum1 + sum2) / 2;
-        int count = 0;
+        int answer = 0;
         while (sum1 != sum2) {
-            if (count >= queue1.length * 4) {
-                return -1;
+            if (answer > n * 2) {
+                answer = -1;
+                break;
             }
-            count++;
+            answer++;
             if (sum1 > sum2) {
-                int poll = que1.poll();
-                que2.add(poll);
-                sum1 -= poll;
-                sum2 += poll;
+                int num = que1.poll();
+                sum1 -= num;
+                sum2 += num;
+                que2.add(num);
             } else {
-                int poll = que2.poll();
-                que1.add(poll);
-                sum2 -= poll;
-                sum1 += poll;
+                int num = que2.poll();
+                sum2 -= num;
+                sum1 += num;
+                que1.add(num);
             }
         }
-        
-        return count;
+        // 최소 횟수 반환 (원소 합을 같게 만들 수 없으면 -1 반환)
+        return answer;
     }
 }
