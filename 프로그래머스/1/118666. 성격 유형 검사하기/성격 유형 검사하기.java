05d3aeb308da9a46abacdfbff86c64abd963df68
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-19 10:58:33 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.83 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,47 +1,46 @@
-
 import java.util.*;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-
         Map<Character, Integer> map = new HashMap<>();
-        map.put('R', 0); map.put('T', 0);
-        map.put('C', 0); map.put('F', 0);
-        map.put('J', 0); map.put('M', 0);
-        map.put('A', 0); map.put('N', 0);
-
-        for (int i = 0; i < choices.length; i++) {
-            char c1 = survey[i].charAt(0);
-            char c2 = survey[i].charAt(1);
-            int answer = choices[i];
-            switch (answer) {
+        map.put('R', 0);
+        map.put('T', 0);
+        map.put('C', 0);
+        map.put('F', 0);
+        map.put('J', 0);
+        map.put('M', 0);
+        map.put('A', 0);
+        map.put('N', 0);
+        
+        for (int i = 0; i < survey.length; i++) {
+            switch (choices[i]) {
                 case 1:
-                    map.put(c1, map.get(c1) + 3);
+                    map.put(survey[i].charAt(0), map.get(survey[i].charAt(0)) + 3);
                     break;
                 case 2:
-                    map.put(c1, map.get(c1) + 2);
+                    map.put(survey[i].charAt(0), map.get(survey[i].charAt(0)) + 2);
                     break;
                 case 3:
-                    map.put(c1, map.get(c1) + 1);
+                    map.put(survey[i].charAt(0), map.get(survey[i].charAt(0)) + 1);
                     break;
                 case 5:
-                    map.put(c2, map.get(c2) + 1);
+                    map.put(survey[i].charAt(1), map.get(survey[i].charAt(1)) + 1);
                     break;
                 case 6:
-                    map.put(c2, map.get(c2) + 2);
+                    map.put(survey[i].charAt(1), map.get(survey[i].charAt(1)) + 2);
                     break;
                 case 7:
-                    map.put(c2, map.get(c2) + 3);
+                    map.put(survey[i].charAt(1), map.get(survey[i].charAt(1)) + 3);
                     break;
+                    
             }
         }
-
+        
         String answer = "";
-
-        answer += (map.get('R') >= map.get('T')) ? 'R' : 'T';
-        answer += (map.get('C') >= map.get('F')) ? 'C' : 'F';
-        answer += (map.get('J') >= map.get('M')) ? 'J' : 'M';
-        answer += (map.get('A') >= map.get('N')) ? 'A' : 'N';
+        answer += map.get('R') >= map.get('T') ? "R" : "T";
+        answer += map.get('C') >= map.get('F') ? "C" : "F";
+        answer += map.get('J') >= map.get('M') ? "J" : "M";
+        answer += map.get('A') >= map.get('N') ? "A" : "N";
         
         return answer;
     }
