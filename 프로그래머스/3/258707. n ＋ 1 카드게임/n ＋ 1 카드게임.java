d1c49bf6bb14704c496f4f20ad281cf35516f2cd
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-20 11:46:28 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.96 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,68 +1,75 @@
 import java.util.*;
 
 class Solution {
+    
+    int coin;
+    int[] cards;
+    int n;
+    Set<Integer> myCardSet = new HashSet<>();
+    Set<Integer> additionalCardSet = new HashSet<>();
+    
     public int solution(int coin, int[] cards) {
-        int answer = 0;
-        int idx = cards.length / 3;
-        int target = cards.length + 1;
-        Set<Integer> myCards = new HashSet<>();
-        Set<Integer> additionalCards = new HashSet<>();
-        
-        for (int i = 0; i < idx; i++) {
-            myCards.add(cards[i]);
+        this.coin = coin;
+        this.cards = cards;
+        n = cards.length;
+        for (int i = 0; i < n / 3; i++) {
+            myCardSet.add(cards[i]);
         }
+        
+        int answer = 0;
+        int idx = n / 3;
         
         while (true) {
             answer++;
             
-            // 추가로 뽑을 카드가 없으면 끝내기
-            if (idx >= cards.length) {
+            // 뽑을 카드가 없는 경우 -> 끝내기
+            if (idx >= n) {
                 break;
             }
             
-            additionalCards.add(cards[idx]);
-            additionalCards.add(cards[idx + 1]);
-            idx += 2;
+            // 2개 뽑기
+            additionalCardSet.add(cards[idx++]);
+            additionalCardSet.add(cards[idx++]);
             
+            // 이미 가진 카드에서 낼 수 있으면 내기
             boolean flag = false;
-            
-            // 1. myCards 에서만 2개 뽑을 수 있나 확인
-            for (int num : myCards) {
-                if (myCards.contains(target - num)) {
-                    myCards.remove(num);
-                    myCards.remove(target - num);
+            for (int card : myCardSet) {
+                if (myCardSet.contains(n + 1 - card)) {
+                    myCardSet.remove((Object) card);
+                    myCardSet.remove((Object) (n + 1 - card));
                     flag = true;
                     break;
                 }
             }
             
-            // 2. myCards 에서 1개, additionalCards 에서 1개 뽑을 수 있나 확인
+            // 그렇지 않으면 additional과 섞어서 내기 (coin 감소)
             if (!flag && coin >= 1) {
-                for (int num : myCards) {
-                    if (additionalCards.contains(target - num)) {
-                        myCards.remove(num);
-                        additionalCards.remove(target - num);
-                        flag = true;
+                // myCardSet에서 1개, additionalCardSet에서 1개
+                for (int card : myCardSet) {
+                    if (additionalCardSet.contains(n + 1 - card)) {
+                        myCardSet.remove((Object) card);
+                        additionalCardSet.remove((Object) (n + 1 - card));
                         coin--;
-                        break;
-                    }
-                }
-            }
-
-            // 3. additionalCards 에서 2개 뽑을 수 있나 확인
-            if (!flag && coin >= 2) {
-                for (int num : additionalCards) {
-                    if (additionalCards.contains(target - num)) {
-                        additionalCards.remove(num);
-                        additionalCards.remove(target - num);
                         flag = true;
-                        coin -= 2;
                         break;
                     }
                 }
             }
             
-            // 2개를 뽑을 수 없으면 끝내기
+            if (!flag && coin >= 2) {
+                // additionalCardSet에서 2개
+                for (int card : additionalCardSet) {
+                    if (additionalCardSet.contains(n + 1 - card)) {
+                        additionalCardSet.remove((Object) card);
+                        additionalCardSet.remove((Object) (n + 1 - card));
+                        coin -= 2;
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            
+            // n + 1에 맞춰서 카드를 낼 수 없는 경우 -> 끝내기
             if (!flag) {
                 break;
             }
