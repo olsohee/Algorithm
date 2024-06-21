6b75d923f4ae6a3ae365f12ef1588ff90667c139
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-21 14:26:45 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 6.01 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,56 +1,67 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int alp, int cop, int[][] problems) {
-
         int goalAlp = 0;
         int goalCop = 0;
-        
         for (int[] problem : problems) {
             goalAlp = Math.max(goalAlp, problem[0]);
             goalCop = Math.max(goalCop, problem[1]);
         }
-        
         goalAlp = Math.max(goalAlp, alp);
         goalCop = Math.max(goalCop, cop);
         
-        int[][] dp = new int[goalAlp + 1][goalCop + 1];
-        
-        for (int[] arr : dp) {
+        // 해당 알고력과 코딩력을 갖는데 걸리는 최단 시간
+        int[][] time = new int[goalAlp + 1][goalCop + 1];
+        for (int[] arr : time) {
             Arrays.fill(arr, Integer.MAX_VALUE);
         }
-        dp[alp][cop] = 0;
         
-        for (int i = alp; i <= goalAlp; i++) {
-            for (int j = cop; j <= goalCop; j++) {
-                // 알고리즘 공부
-                if (i + 1 > goalAlp) {
-                    dp[goalAlp][j] = Math.min(dp[goalAlp][j], dp[i][j] + 1);
-                } else {
-                    dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + 1);
-                }
-                
-                // 코딩 공부
-                if (j + 1 > goalCop) {
-                    dp[i][goalCop] = Math.min(dp[i][goalCop], dp[i][j] + 1);
-                } else {
-                    dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + 1);
-                }
-                
-                // 문제 풀기
-                for (int[] p : problems) {
-                    if (i >= p[0] && j >= p[1]) {
-                        int newAlp = i + p[2];
-                        int newCop = j + p[3];
-                        if (newAlp > goalAlp) newAlp = goalAlp;
-                        if (newCop > goalCop) newCop = goalCop;
-                        dp[newAlp][newCop] = Math.min(dp[newAlp][newCop], dp[i][j] + p[4]);
+        Queue<Node> que = new LinkedList<>();
+        que.add(new Node(alp, cop));
+        time[alp][cop] = 0;
+        
+        while (!que.isEmpty()) {
+            Node now = que.poll();
+            // System.out.println(now.alp + ", " + now.cop);
+            // 알고리즘 공부
+            int newAlp = (now.alp + 1 > goalAlp) ? goalAlp : now.alp + 1;
+            if (time[newAlp][now.cop] > time[now.alp][now.cop] + 1) {
+                time[newAlp][now.cop] = time[now.alp][now.cop] + 1;
+                que.add(new Node(newAlp, now.cop));
+            }
+            
+            // 코딩 공부
+            int newCop = (now.cop + 1 > goalCop) ? goalCop : now.cop + 1;
+            if (time[now.alp][newCop] > time[now.alp][now.cop] + 1) {
+                time[now.alp][newCop] = time[now.alp][now.cop] + 1;
+                que.add(new Node(now.alp, newCop));
+            }
+            
+            // 문제 풀기
+            for (int[] problem : problems) {
+                if ((now.alp >= problem[0]) && (now.cop >= problem[1])) {
+                    newAlp = (now.alp + problem[2] > goalAlp) ? goalAlp : now.alp + problem[2];
+                    newCop = (now.cop + problem[3] > goalCop) ? goalCop : now.cop + problem[3];
+                    if (time[newAlp][newCop] > time[now.alp][now.cop] + problem[4]) {
+                        time[newAlp][newCop] = time[now.alp][now.cop] + problem[4];
+                        que.add(new Node(newAlp, newCop));
                     }
                 }
             }
         }
         
-        return dp[goalAlp][goalCop];
+        return time[goalAlp][goalCop];
+    }
+    
+    private static class Node {
+        
+        int alp;
+        int cop;
+        
+        public Node(int alp, int cop) {
+            this.alp = alp;
+            this.cop = cop;
+        }
     }
 }
