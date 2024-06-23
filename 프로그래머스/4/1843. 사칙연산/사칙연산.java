f55e6c8d728bfe1b98b96613d1f11c90378a916b
olsohee <108605017+olsohee@users.noreply.github.com>
2024-06-24 00:15:42 +0900

    [level 4] Title: 사칙연산, Time: 16.92 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,77 +1,80 @@
+import java.util.*;
+
 class Solution {
     
     int n;
-    int[] nums;
-    char[] opers;
     int[][][] dp;
+    int[] nums;
+    char[] operations;
     
     public int solution(String arr[]) {
-        // 초기화
+        
         n = arr.length / 2;
+        dp = new int[2][n + 1][n + 1];
         nums = new int[n + 1];
-        opers = new char[n];
+        operations = new char[n];
         
         for (int i = 0; i < arr.length; i++) {
             if (i % 2 == 0) {
                 nums[i / 2] = Integer.parseInt(arr[i]);
             } else {
-                opers[i / 2] = arr[i].charAt(0);
+                operations[i / 2] = arr[i].charAt(0);
             }
         }
         
-        dp = new int[2][n + 1][n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
                 dp[0][i][j] = Integer.MIN_VALUE;
                 dp[1][i][j] = Integer.MAX_VALUE;
             }
         }
         
-        return solve(0, 0, n); // nums의 인덱스 0 ~ n까지 숫자들의 연산 최댓값
+        return solve(0, 0, n);
     }
     
-    public int solve (int flag, int start, int end) {
+    public int solve(int flag, int start, int end) {
+        
         if (start == end) {
-            dp[flag][start][end] = nums[start];
+            return dp[flag][start][end] = nums[start];
         }
         
-        if (flag == 0 && dp[flag][start][end] != Integer.MIN_VALUE) {
-            return dp[flag][start][end];
-        }
-        if (flag == 1 && dp[flag][start][end] != Integer.MAX_VALUE) {
-            return dp[flag][start][end];
+        if (flag == 0 && dp[0][start][end] != Integer.MIN_VALUE) {
+            return dp[0][start][end];
         }
         
-        int result = flag == 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        if (flag == 1 && dp[1][start][end] != Integer.MAX_VALUE) {
+            return dp[1][start][end];
+        }
         
-        // 최댓값을 구해야 하는 경우
+        int result = (flag == 0) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        // 최댓값 구하기
         if (flag == 0) {
-            for (int i = start; i < end; i++) {
-                // 최대 - 최소
-                if (opers[i] == '-') {
-                     result = Math.max(result, solve(0, start, i) - solve(1, i + 1, end));
+            for (int mid = start; mid < end; mid++) {
+                // 최대 - 최소 
+                if (operations[mid] == '-') {
+                    result = Math.max(result, solve(0, start, mid) - solve(1, mid + 1, end));
                 } 
                 // 최대 + 최대
-                else {
-                    result = Math.max(result, solve(0, start, i) + solve(0, i + 1, end));
-                }
-            }
-        } 
-        
-        // 최솟값을 구해야 하는 경우
-        else {
-            for (int i = start; i < end; i++) {
-                // 최소 - 최대
-                if (opers[i] == '-') {
-                     result = Math.min(result, solve(1, start, i) - solve(0, i + 1, end));
-                } 
-                // 최소 + 최소
-                else {
-                    result = Math.min(result, solve(1, start, i) + solve(1, i + 1, end));
+                if (operations[mid] == '+') {
+                    result = Math.max(result, solve(0, start, mid) + solve(0, mid + 1, end));
                 }
             }
         }
-            
+        
+        // 최솟값 구하기
+        if (flag == 1) {
+            for (int mid = start; mid < end; mid++) {
+                // 최소 - 최대 
+                if (operations[mid] == '-') {
+                    result = Math.min(result, solve(1, start, mid) - solve(0, mid + 1, end));
+                } 
+                // 최소 + 최소
+                if (operations[mid] == '+') {
+                    result = Math.min(result, solve(1, start, mid) + solve(1, mid + 1, end));
+                }
+            }
+        }
+        
         return dp[flag][start][end] = result;
     }
 }
