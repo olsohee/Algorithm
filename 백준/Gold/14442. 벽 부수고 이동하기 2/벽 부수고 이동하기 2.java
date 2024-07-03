b938e5d989aeb168f8b0bd449cbc4573f30d185b
olsohee <108605017+olsohee@users.noreply.github.com>
2024-07-03 15:42:29 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1648 ms, Memory: 382732 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -7,70 +7,87 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int answer = -1;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+    static int n, m, k;
+    static int[][] map;
+    static int[][][] visited;
+    static int answer = -1;
 
     public static void main(String[] args) throws IOException {
 
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[n][m];
-        int[][][] visited = new int[k + 1][n][m];
+        map = new int[n][m];
+        visited = new int[k + 1][n][m];
 
         for (int i = 0; i < n; i++) {
-            String[] arr = br.readLine().split("");
+            String input = br.readLine();
             for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(arr[j]);
+                map[i][j] = input.charAt(j) - '0';
             }
         }
 
-        // 0,0에서부터 bfs
+        bfs();
+        System.out.println(answer);
+    }
+
+    public static void bfs() {
         Queue<Node> que = new LinkedList<>();
         que.add(new Node(0, 0, 0));
         visited[0][0][0] = 1;
 
-        while (!que.isEmpty()) {
+        while(!que.isEmpty()) {
             Node now = que.poll();
+
             if (now.y == n - 1 && now.x == m - 1) {
-                answer = visited[now.cnt][now.y][now.x];
+                answer = visited[now.breakCnt][now.y][now.x];
                 break;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + now.x;
-                int ny = dy[i] + now.y;
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
 
-                // 길인 경우 이동
-                if (map[ny][nx] == 0 && visited[now.cnt][ny][nx] == 0) {
-                    visited[now.cnt][ny][nx] = visited[now.cnt][now.y][now.x] + 1;
-                    que.add(new Node(ny, nx, now.cnt));
+                if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+                    continue;
                 }
 
-                // 벽인 경우 이동
-                if (now.cnt + 1 <= k) {
-                    if (map[ny][nx] == 1 && visited[now.cnt + 1][ny][nx] == 0) {
-                        visited[now.cnt + 1][ny][nx] = visited[now.cnt][now.y][now.x] + 1;
-                        que.add(new Node(ny, nx, now.cnt + 1));
+                // 길인 경우
+                if (map[ny][nx] == 0) {
+                    if (visited[now.breakCnt][ny][nx] != 0) {
+                        continue;
                     }
+                    visited[now.breakCnt][ny][nx] = visited[now.breakCnt][now.y][now.x] + 1;
+                    que.add(new Node(ny, nx, now.breakCnt));
+                }
+
+                // 벽인 경우
+                if (map[ny][nx] == 1) {
+                    if (now.breakCnt + 1 > k) {
+                        continue;
+                    }
+                    if (visited[now.breakCnt + 1][ny][nx] != 0) {
+                        continue;
+                    }
+                    visited[now.breakCnt + 1][ny][nx] = visited[now.breakCnt][now.y][now.x] + 1;
+                    que.add(new Node(ny, nx, now.breakCnt + 1));
                 }
             }
         }
-
-        System.out.println(answer);
     }
 
-    public static class Node {
+    private static class Node {
         int y, x;
-        int cnt;
-        public Node (int y, int x, int cnt) {
+        int breakCnt;
+
+        public Node(int y, int x, int breakCnt) {
             this.y = y;
             this.x = x;
-            this.cnt = cnt;
+            this.breakCnt = breakCnt;
         }
     }
 }
