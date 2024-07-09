21ebb0f435d63ef76a31e5b53d3eac410b9d791f
olsohee <108605017+olsohee@users.noreply.github.com>
2024-07-09 11:22:25 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,43 +1,37 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
-
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static int[] dp;
-    static int[][] input;
-    static int n;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        dp = new int[n + 2]; // 배열의 범위는 0 ~ (n + 1), 이때 n + 1은 퇴사하는 날
-        input = new int[2][n + 2];
-
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        int[] time = new int[n + 1];
+        int[] money = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            input[0][i] = Integer.parseInt(st.nextToken());
-            input[1][i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            time[i] = Integer.parseInt(st.nextToken());
+            money[i] = Integer.parseInt(st.nextToken());
         }
 
-        int max = 0;
-        for (int i = n; i >= 1; i--) {
-            int time = input[0][i];
-            int revenue = input[1][i];
-            if (i + time <= n + 1) {
-                int availableMaxRevenue = 0; // i번째 날짜에 일을 하고, 그 다음 일들을 할 때 최대 수익
-                for (int j = i + time; j <= n; j++) {
-                    availableMaxRevenue = Math.max(availableMaxRevenue, dp[j]);
-                }
-                dp[i] = revenue + availableMaxRevenue;
-                max = Math.max(max, dp[i]);
+        int maxMoney = 0;
+        for (int i = 1; i <= n; i++) {
+            // 끝나는 날짜가 n을 초과하는 경우
+            int end = time[i] + i - 1;
+
+            if (end <= n) {
+                dp[end] = Math.max(dp[end], money[i] + maxMoney);
             }
+
+            maxMoney = Math.max(maxMoney, dp[i]);
+
         }
 
-        System.out.println(max);
+        System.out.println(maxMoney);
     }
 }
-
