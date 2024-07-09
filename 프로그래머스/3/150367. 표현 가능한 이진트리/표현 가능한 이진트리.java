f2649f624fa98cfa25881fc2ea7dc71e011ddb74
olsohee <108605017+olsohee@users.noreply.github.com>
2024-07-09 13:39:47 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 18.61 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -7,42 +7,59 @@ class Solution {
         
         for (int i = 0; i < numbers.length; i++) {
             String str = Long.toBinaryString(numbers[i]);
-            while (!collectLength(str)) {
-                str = "0" + str;
-            }
-            
-            boolean result = dfs(str);
-            answer[i] = result ? 1 : 0;
+            str = validateLen(str);
+            char mid = str.charAt(str.length() / 2);
+            int result = dfs(str) ? 1 : 0;
+            answer[i] = result;
         }
-        
         return answer;
     }
     
-    public boolean collectLength(String str) {
-        int len = str.length() + 1; // len이 2의 제곱이어야 함
-        int n = 1;
-        while(n < len) {
-            n *= 2;
-        }
-        return len == n;
-    }
-    
-    public boolean dfs(String str) {
+    // 부모 노드가 0 -> 자식 노드도 모두 0
+    // 부모 노드가 1 -> 자식 노드는 0, 1 상관없음
+    private boolean dfs(String str) {
         if (str.length() == 1) {
             return true;
         }
         
         int mid = str.length() / 2;
-        String leftTree = str.substring(0, mid);
-        String rightTree = str.substring(mid + 1);
+        String left = str.substring(0, mid);
+        String right = str.substring(mid + 1, str.length());
         
-        // 루트 노드가 0이면 왼쪽, 오른쪽 모든 자식 노드도 0이어야 함
+        // 가운데 부모 노드가 0이면, 그 아래도 다 0이어야 함
         if (str.charAt(mid) == '0') {
-            if (leftTree.contains("1") || rightTree.contains("1")) {
+            if (left.contains("1") || right.contains("1")) {
                 return false;
+            } else {
+                return true;
             }
+        } 
+        // 가운데 부모 노드가 1이면, 계속 탐색
+        else {
+            return dfs(left) && dfs(right);
+        }
+    }
+    
+    private String validateLen(String str) {
+        // 길이가 2^n - 1가 될 때까지 앞에 0을 붙이기 (str.len + 1 == 2^n이 되게 만들기)
+        while (!validateBinary(str.length() + 1)) {
+            str = "0" + str;
         }
         
-        return dfs(leftTree) && dfs(rightTree);
+        return str;
+    }
+    
+    // num이 2^n의 값인지?
+    private boolean validateBinary(int num) {
+        int result = 1;
+        while (result < num) {
+            result *= 2;
+        }
+        
+        if (result == num) {
+            return true;
+        }
+        
+        return false;
     }
 }
