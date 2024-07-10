8a13fbfb37bd3633292cc386cd23c7943eea16ec
olsohee <108605017+olsohee@users.noreply.github.com>
2024-07-10 15:10:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1980 ms, Memory: 133024 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                // 행마다 구간합 구하기
+                if (j == 1) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                } else {
+                    map[i][j] = map[i][j - 1] + Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+
+            for (int j = x1; j <= x2; j++) {
+                sum += map[j][y2] - map[j][y1 - 1];
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
