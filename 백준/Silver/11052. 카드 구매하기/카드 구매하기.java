2e69ee7d3d4d41c9c666b7c4b8061fa8ebfa0841
olsohee <108605017+olsohee@users.noreply.github.com>
2024-07-19 16:57:36 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -11,20 +11,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        int[] money = new int[n + 1];
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            money[i] = Integer.parseInt(st.nextToken());
-            dp[i] = money[i];
+            dp[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i <= n; i++) {
-            int max = 0;
             for (int j = i - 1; j >= (i - j); j--) {
-                max = Math.max(max, dp[j] + dp[i - j]);
+                dp[i] = Math.max(dp[i], dp[j] + dp[i - j]);
             }
-
-            dp[i] = Math.max(dp[i], max);
         }
 
         System.out.println(dp[n]); // 카드 n개를 구매하는데 지불하는 최대 금액
