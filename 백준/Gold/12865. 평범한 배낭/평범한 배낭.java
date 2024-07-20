84066ea6ceceabcfc12ad7acd241ede1027b9a46
olsohee <108605017+olsohee@users.noreply.github.com>
2024-07-20 22:41:32 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53792 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,34 +6,33 @@ import java.util.*;
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int n;
-    static int k;
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        int[] w = new int[n + 1];
-        int[] v = new int[n + 1];
-        int[][] dp = new int[n + 1][k + 1];
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][k + 1]; //dp[i][j] = i번째 무게까지 고려했을 때 j만큼의 무게를 담았을 때 최대 가치
+        int[][] arr = new int[n + 1][2];
 
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            w[i] = Integer.parseInt(st.nextToken());
-            v[i] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= k; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j - w[i] >= 0) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
+                if (arr[i][0] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], arr[i][1] + dp[i - 1][j - arr[i][0]]);
                 }
             }
         }
 
+        // 최대 k만큼 짐을 싸면서, 가치의 최댓값!
         System.out.println(dp[n][k]);
     }
 }
