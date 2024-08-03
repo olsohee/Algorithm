acf733a571a49147cf2aac58707bf2210055f62a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-03 13:08:59 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -7,52 +7,38 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int[] parent;
+    static boolean[][] map;
+    static boolean[] visited;
+    static int n;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
-        int v = Integer.parseInt(br.readLine());
-        int e = Integer.parseInt(br.readLine());
-        parent = new int[v + 1];
-        for (int i = 0; i <= v; i++) {
-            parent[i] = i;
-        }
-
-        // 노드들을 합치기
-        for (int i = 0; i < e; i++) {
+        n = Integer.parseInt(br.readLine());
+        map = new boolean[n + 1][n + 1];
+        visited = new boolean[n + 1];
+        int cnt = Integer.parseInt(br.readLine());
+        for (int i = 0; i < cnt; i++) {
             st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
-
-            union(n1, n2);
+            map[n1][n2] = true;
+            map[n2][n1] = true;
         }
 
-        // 1번 컴퓨터와 연결된 컴퓨터 수 찾기
-        int answer = 0;
-        int p = find(1);
+        dfs(1);
 
-        for (int i = 2; i <= v; i++) {
-            if (find(i) == p) {
-                answer++;
+        System.out.println(--answer);
+    }
+
+    private static void dfs(int start) {
+        visited[start] = true;
+        answer++;
+
+        for (int i = 1; i <= n; i++) {
+            if (map[start][i] && !visited[i]) {
+                dfs(i);
             }
         }
-        System.out.println(answer);
-    }
-
-    public static void union(int n1, int n2) {
-        // 최상단의 부모 노드 찾기
-        int parent1 = find(n1);
-        int parent2 = find(n2);
-
-        if (parent1 < parent2) {
-            parent[parent2] = parent1;
-        } else {
-            parent[parent1] = parent2;
-        }
-    }
-
-    public static int find(int x) {
-        if (parent[x] == x) return x;
-        else return parent[x] = find(parent[x]);
     }
 }
