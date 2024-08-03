ab6b3d0a80877fe77cdde172370674ba00edb778
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-03 13:20:54 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -7,38 +7,47 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static boolean[][] map;
-    static boolean[] visited;
+    static int[] parent;
     static int n;
     static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
         n = Integer.parseInt(br.readLine());
-        map = new boolean[n + 1][n + 1];
-        visited = new boolean[n + 1];
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
         int cnt = Integer.parseInt(br.readLine());
         for (int i = 0; i < cnt; i++) {
             st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
-            map[n1][n2] = true;
-            map[n2][n1] = true;
+            union(n1, n2);
         }
 
-        dfs(1);
+        int p = find(1);
+        for (int i = 2; i <= n; i++) {
+            if (find(i) == p) answer++;
+        }
 
-        System.out.println(--answer);
+        System.out.println(answer);
     }
 
-    private static void dfs(int start) {
-        visited[start] = true;
-        answer++;
+    private static void union(int n1, int n2) {
+        int p1 = find(n1);
+        int p2 = find(n2);
 
-        for (int i = 1; i <= n; i++) {
-            if (map[start][i] && !visited[i]) {
-                dfs(i);
-            }
+        if (p1 < p2) {
+            parent[p2] = p1;
+        } else {
+            parent[p1] = p2;
         }
+    }
+
+    private static int find(int n) {
+        if (parent[n] == n) return n;
+        return find(parent[n]);
     }
 }
