b4a3f3df6f22784acafc922a5e437c56bdbefa99
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-06 13:23:36 +0900

    [level 3] Title: 양과 늑대, Time: 3.99 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,69 +1,44 @@
 import java.util.*;
 
 class Solution {
-
-    private List<List<Integer>> graph = new ArrayList<>();
-    private int answer = 0;
-    private int[] info;
+    
+    int answer = 0;
+    int[] info;
+    int[][] edges;
     
     public int solution(int[] info, int[][] edges) {
-        
         this.info = info;
+        this.edges = edges;
         
-        for (int i = 0; i < info.length; i++) {
-            graph.add(new ArrayList<>());
-        }
+        // 루트 노드(0번)부터 시작
+        dfs(0, new boolean[info.length], 0, 0);
         
-        for (int[] edge : edges) {
-            graph.get(edge[0]).add(edge[1]);
-        }
-        
-        // 루트노드부터 시작
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        dfs(0, 0, 0, list);
-        
+        // 루트 노드로 돌아왔을 때 양의 최대 갯수
         return answer;
     }
     
-    private void dfs(int node, int sheep, int wolf, List<Integer> nextRoute) {
+    private void dfs(int now, boolean[] visited, int sheep, int wolf) {
+        visited[now] = true;
         
-        if (info[node] == 0) {
-            sheep++;
-        } else {
-            wolf++;
-        }
-
-        if (wolf >= sheep) {
-            return;
-        }
+        if (info[now] == 0) sheep++;
+        else wolf++;
+        
+        if (wolf >= sheep) return;
         
         answer = Math.max(answer, sheep);
         
-        // 다음으로 이동
-        // List<Integer> copyNextRoute = new ArrayList<>();
-        // for (int next : nextRoute) {
-        //     copyNextRoute.add(next);
-        // }
-        
-        // copyNextRoute.remove(Integer.valueOf(node));
-        nextRoute.remove(Integer.valueOf(node));
-        
-        for (int next : graph.get(node)) {
-            // copyNextRoute.add(next);
-            nextRoute.add(next);
-        }
-        
-        for (int next : nextRoute) {
-            List<Integer> list = new ArrayList<>();
-            for (int i : nextRoute) {
-                list.add(i);
+        // 현재 위치에서 더 깊게 이동 OR 지금까지 지나온 길에서 더 깊게 이동
+        for (int[] edge : edges) {
+            // 부모 노드 방문했고 자식 노드 방문 안했으면, 그 자식 노드를 방문하기 
+            // 이때 부모 노드는 이미 지나온 길들
+            if (visited[edge[0]] && !visited[edge[1]]) {
+                boolean[] newVisited = visited.clone();
+                // boolean[] newVisited = new boolean[info.length];
+                // for (int i = 0; i < info.length; i++) {
+                //     newVisited[i] = visited[i];
+                // }
+                dfs(edge[1], newVisited, sheep, wolf);
             }
-            dfs(next, sheep, wolf, list);
         }
-        
-        // for (int next : copyNextRoute) {
-        //     dfs(next, sheep, wolf, copyNextRoute);
-        // }
     }
 }
