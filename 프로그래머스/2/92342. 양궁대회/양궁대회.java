dfa7e82d3f6e4ef5658662c495f985d781926637
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-06 12:13:30 +0900

    [level 2] Title: 양궁대회, Time: 74.28 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,105 +1,86 @@
 import java.util.*;
 
-/*
-주의
-- 라이언 점수 == 어피치 점수 -> 어피치가 k점 획득
-- 라이언 점수 == 어피치 점수 == 0 -> 둘 다 0점
-- 최종 점수가 라이언 == 어피치 -> 어피치 승리
-- 라이언이 지거나 비기면 -1 반환
-- 우승 방법이 여러개이면, 가장 낮은 점수를 많이 획득한 방법으로
- */
-
-/*
-1. n개의 화살을 0 ~ 10점으로 나누는 조합 만들기
-    - 어피치보다 2개 이상 맞추는 경우는 탐색할 필요 없음
-2. 조합별로, 어피치-라이언 점수 계산해서 diff 만들기
-3. diff가 가장 큰 조합이 답
-4. diff가 0이거나 음수이면 -1 반환
- */
-
 class Solution {
-    int n;
-    int[] info;
-    List<int[]> list = new ArrayList<>();
-
-    public int[] solution(int n, int[] info) {
-        this.n = n;
-        this.info = info;
-
-        // 1. n개의 화살을 0 ~ 10점으로 나누는 조합 만들기
-        dfs(0, new int[11], n);
-
-        // 2. 조합별로, 어피치-라이언 점수 계산해서 diff 만들기
+    
+        int n;
+        int[] info;
         int maxDiff = 0;
-        int[] answerArr = new int[11];
-        for(int i = 0; i < list.size(); i++) {
-            int[] arr = list.get(i);
+        List<int[]> list = new ArrayList<>();
 
-            int diff = getScoreDiff(arr);
-            if(maxDiff == diff) {
-                // 점수차가 같은 경우가 여러개 있으면, 더 낮은 점수를 많이 맞춘 경우를 답으로
-                for(int j = 10; j >= 0; j--) {
-                    if(arr[j] == answerArr[j]) {
-                        continue;
-                    }
-                    if(arr[j] > answerArr[j]) {
-                        answerArr = arr;
-                        break;
-                    } else {
-                        break;
+        public int[] solution(int n, int[] info) {
+            this.n = n;
+            this.info = info;
+
+            // n발을 10~0점에 나누기
+            dfs(0, n, new int[11]);
+
+            int[] answer = new int[11];
+            for (int[] result : list) {
+                int diff = getResult(result);
+
+                if (maxDiff < diff) {
+                    maxDiff = diff;
+                    answer = result;
+                }
+
+                if (maxDiff == diff) {
+                    for (int i = 10; i >= 0; i--) {
+                        if (answer[i] > result[i]) {
+                            break;
+                        } else if (answer[i] < result[i]) {
+                            answer = result;
+                            break;
+                        }
                     }
                 }
-            } else if(maxDiff < diff) {
-                maxDiff = diff;
-                answerArr = arr;
+            }
+
+            System.out.println("maxDiff = " + maxDiff);
+
+            if (maxDiff <= 0) {
+                return new int[]{-1}; // 라이언이 무조건 지거나 비기면 -1 반환
+            }
+            
+            return answer;
+        }
+
+        private void dfs(int idx, int remain, int[] result) {
+            if (idx == 11) {
+                if (remain == 0) {
+                    list.add(result);
+                }
+                return;
+            }
+
+            for (int i = 0; i <= remain; i++) {
+                int[] cloneArr = result.clone();
+                cloneArr[idx] = i;
+                dfs(idx + 1, remain - i, cloneArr);
             }
         }
 
-        // 3. diff가 가장 큰 조합이 답
-        // 4. diff가 0이거나 음수이면 -1 반환
-        if(maxDiff <= 0) {
-            return new int[] {-1};
-        } else {
-            return answerArr;
-        }
-    }
+        private int getResult(int[] result) {
 
-    private int getScoreDiff(int[] arr) {
-        int lionScore = 0;
-        int apeachScore = 0;
+            int apeach = 0;
+            int lion = 0;
+            // 똑같은 횟수로 맞혔으면 어피치가 점수 획득
+            // 둘 다 0점이면 둘 다 획득 불가
+            for (int i = 0; i <= 10; i++) {
+                int score = 10 - i;
 
-        for(int i = 0; i <= 10; i++) {
-            if(arr[i] == 0 && info[i] == 0) {
-                continue;
+                if (info[i] >= result[i]) {
+                    if (info[i] != 0) {
+                        apeach += score;
+                    }
+                } else {
+                    lion += score;
+                }
             }
-            if(arr[i] > info[i]) {
-                lionScore += 10 - i;
-            } else {
-                apeachScore += 10 - i;
-            }
-        }
 
-        return lionScore - apeachScore;
-    }
+            // System.out.println(apeach);
+            // System.out.println(lion);
 
-    // index 0부터 10까지 탐색하며, 하나씩 값 채우기
-    // index가 11이 되었을 때, 총 합이 n이면 리스트에 담고 끝내기
-    private void dfs(int index, int[] arr, int remain) {
-        if(index == 11) {
-            if(remain == 0) {
-                list.add(arr);
-            }
-            return;
-        }
-
-        // cnt 인덱스에 숫자 몇을 넣을지
-        for(int i = 0; i <= remain; i++) {
-            int[] cloneArr = arr.clone();
-            cloneArr[index] = i;
-            dfs(index + 1, cloneArr, remain - i);
-            if(cloneArr[index] > info[index]) {
-                break;
-            }
+            return lion - apeach;
         }
     }
-}
+
