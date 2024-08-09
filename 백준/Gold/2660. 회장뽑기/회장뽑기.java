9e60f6608a681f1f71062eb2c649c43df00c97e5
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-09 11:17:43 +0900

    [Gold V] Title: 회장뽑기, Time: 124 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.java
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.java
@@ -7,70 +7,66 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static int n;
+    static int[][] map;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
-
-        // 플로이드 (O(N^3) = 125,000)
-        int n = Integer.parseInt(br.readLine());
-        int[][] map = new int[n + 1][n + 1];
+        n = Integer.parseInt(br.readLine());
+        map = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i != j) {
-                    map[i][j] = Integer.MAX_VALUE;
-                }
+                if (i == j) continue;
+                map[i][j] = Integer.MAX_VALUE;
             }
         }
 
-        // 입력
         while (true) {
             st = new StringTokenizer(br.readLine());
             int num1 = Integer.parseInt(st.nextToken());
             int num2 = Integer.parseInt(st.nextToken());
-            if (num1 == -1 && num2 == -1) {
-                break;
-            }
+
+            if (num1 == -1 && num2 == -1) break;
             map[num1][num2] = map[num2][num1] = 1;
         }
 
-        // 플로이드
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                for (int k = 1; k <= n; k++) {
-                    // j -> k (i경유)
-                    if (map[j][i] != Integer.MAX_VALUE && map[i][k] != Integer.MAX_VALUE) {
-                        map[j][k] = Math.min(map[j][k], map[j][i] + map[i][k]);
+        // 각 노드에서 각 노드로의 최단 거리 구하기(플로이드)
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (map[i][k] != Integer.MAX_VALUE && map[k][j] != Integer.MAX_VALUE) {
+                        map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
                     }
                 }
             }
         }
 
-        int[] score = new int[n + 1];
-        int min = Integer.MAX_VALUE;
+        int[] result = new int[n + 1];
+        int totalMin = Integer.MAX_VALUE;
+
         for (int i = 1; i <= n; i++) {
             int max = 0;
             for (int j = 1; j <= n; j++) {
-                if (map[i][j] > max) {
-                    max = map[i][j];
-                }
+                max = Math.max(max, map[i][j]);
             }
-            score[i] = max;
-            min = Math.min(min, max);
+            result[i] = max;
+            totalMin = Math.min(totalMin, result[i]);
         }
 
         int cnt = 0;
         List<Integer> list = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-            if (score[i] == min) {
+            if (result[i] == totalMin) {
                 cnt++;
                 list.add(i);
             }
         }
 
-        System.out.println(min + " " + cnt);
-        for (int i : list) {
-            System.out.print(i + " ");
+        System.out.println(totalMin + " " + list.size());
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
         }
-
     }
 }
