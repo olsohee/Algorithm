8a9265179e1e9debab4e74140e907fdb001f46f2
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-13 18:16:32 +0900

    [level 3] Title: 산 모양 타일링, Time: 19.82 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,21 +1,29 @@
 import java.util.*;
 
 class Solution {
-    
-    static final int mod = 10007;
-    
     public int solution(int n, int[] tops) {
-        int[][] dp = new int[n][2];
-        dp[0][0] = 1;
-        dp[0][1] = tops[0] == 1 ? 3 : 2;
         
-        for (int i = 1; i < n; i++) {
-            int n1 = tops[i] == 1 ? 2 : 1;
-            int n2 = tops[i] == 1 ? 3 : 2;
-            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
-            dp[i][1] = (dp[i - 1][0] * n1 + dp[i - 1][1] * n2) % mod;
+        int mod = 10007;
+        int[][] dp = new int[n + 1][2];
+        
+        if (tops[0] == 1) {
+            dp[1][0] = 3;
+            dp[1][1] = 1;
+            
+        } else {
+            dp[1][0] = 2;
+            dp[1][1] = 1;
         }
         
-        return (dp[n - 1][0] + dp[n - 1][1])  % mod;
+        for (int i = 2; i <= n; i++) {
+            if (tops[i - 1] == 1) {
+                dp[i][0] = ((dp[i - 1][0] + dp[i - 1][1]) * 2 + dp[i - 1][0]) % mod;
+            } else {
+                dp[i][0] = ((dp[i - 1][0] + dp[i - 1][1]) + dp[i - 1][0]) % mod;
+            }
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+        }
+        
+        return (dp[n][0] + dp[n][1]) % mod;
     }
 }
