bcbdb636c8e48cc33ae5d8a6a31909cb1ba25de2
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-13 13:04:50 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 24.72 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,67 +1,57 @@
 import java.util.*;
-
 class Solution {
     public int solution(int alp, int cop, int[][] problems) {
-        int goalAlp = 0;
-        int goalCop = 0;
+
+        int goalAlp = alp;
+        int goalCop = cop;
         for (int[] problem : problems) {
             goalAlp = Math.max(goalAlp, problem[0]);
             goalCop = Math.max(goalCop, problem[1]);
         }
-        goalAlp = Math.max(goalAlp, alp);
-        goalCop = Math.max(goalCop, cop);
-        
-        // 해당 알고력과 코딩력을 갖는데 걸리는 최단 시간
+
         int[][] time = new int[goalAlp + 1][goalCop + 1];
         for (int[] arr : time) {
             Arrays.fill(arr, Integer.MAX_VALUE);
         }
-        
-        Queue<Node> que = new LinkedList<>();
-        que.add(new Node(alp, cop));
         time[alp][cop] = 0;
-        
-        while (!que.isEmpty()) {
-            Node now = que.poll();
-            // System.out.println(now.alp + ", " + now.cop);
-            // 알고리즘 공부
-            int newAlp = (now.alp + 1 > goalAlp) ? goalAlp : now.alp + 1;
-            if (time[newAlp][now.cop] > time[now.alp][now.cop] + 1) {
-                time[newAlp][now.cop] = time[now.alp][now.cop] + 1;
-                que.add(new Node(newAlp, now.cop));
-            }
-            
-            // 코딩 공부
-            int newCop = (now.cop + 1 > goalCop) ? goalCop : now.cop + 1;
-            if (time[now.alp][newCop] > time[now.alp][now.cop] + 1) {
-                time[now.alp][newCop] = time[now.alp][now.cop] + 1;
-                que.add(new Node(now.alp, newCop));
-            }
-            
-            // 문제 풀기
-            for (int[] problem : problems) {
-                if ((now.alp >= problem[0]) && (now.cop >= problem[1])) {
-                    newAlp = (now.alp + problem[2] > goalAlp) ? goalAlp : now.alp + problem[2];
-                    newCop = (now.cop + problem[3] > goalCop) ? goalCop : now.cop + problem[3];
-                    if (time[newAlp][newCop] > time[now.alp][now.cop] + problem[4]) {
-                        time[newAlp][newCop] = time[now.alp][now.cop] + problem[4];
-                        que.add(new Node(newAlp, newCop));
+
+        for (int i = alp; i <= goalAlp; i++) {
+            for (int j = cop; j <= goalCop; j++) {
+                // 알고, 코딩력이 i, j일 때 할 수 있는 것들
+
+                // 알고리즘 공부
+                if (i < goalAlp) {
+                    time[i + 1][j] = Math.min(time[i + 1][j], time[i][j] + 1);
+                } else {
+                    time[goalAlp][j] = Math.min(time[goalAlp][j], time[i][j] + 1);
+                }
+
+                // 코딩 공부
+                if (j < goalCop) {
+                    time[i][j + 1] = Math.min(time[i][j + 1], time[i][j] + 1);
+                } else {
+                    time[i][goalCop] = Math.min(time[i][goalCop], time[i][j] + 1);
+                }
+
+                // 문제 풀기
+                for (int[] problem : problems) {
+                    if (i >= problem[0] && j >= problem[1]) {
+                        int newAlp = i + problem[2];
+                        int newCop = j + problem[3];
+                        if (newAlp >= goalAlp) newAlp = goalAlp;
+                        if (newCop >= goalCop) newCop = goalCop;
+                        time[newAlp][newCop] = Math.min(time[newAlp][newCop], time[i][j] + problem[4]);
                     }
                 }
             }
         }
-        
         return time[goalAlp][goalCop];
-    }
-    
-    private static class Node {
-        
-        int alp;
-        int cop;
-        
-        public Node(int alp, int cop) {
-            this.alp = alp;
-            this.cop = cop;
-        }
+//        int min = Integer.MAX_VALUE;
+//        for (int i = goalAlp; i < goalAlp + 30; i++) {
+//            for (int j = goalCop; j < goalCop + 30; j++) {
+//                min = Math.min(min, time[i][j]);
+//            }
+//        }
+//        return min;
     }
 }
