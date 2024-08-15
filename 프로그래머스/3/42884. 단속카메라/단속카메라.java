acd2153ef2e7f9fd0d05ea15d336c1577f7a1543
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-15 10:30:38 +0900

    [level 3] Title: 단속카메라, Time: 8.59 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,30 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         
-        // 끝나는 시간이 이른 순으로 정렬 
-        Arrays.sort(routes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
-            }
-        });
-            
-      
-        int last = routes[0][1];
-        System.out.println(last);
+        Arrays.sort(routes, ((o1, o2) -> {
+            return o1[1] - o2[1];
+        }));
+
         int answer = 1;
+        int lastLocation = routes[0][1];
         for (int i = 1; i < routes.length; i++) {
-            if (routes[i][0] <= last && routes[i][1] >= last) {
-                continue;
-            } 
-            last = routes[i][1];
-            answer++;
-            System.out.println(last);
-            
+            if (lastLocation < routes[i][0]) {
+                answer++;
+                lastLocation = routes[i][1];
+            }
         }
-    
-        
-        // 설치해야 하는 최소 카메라 수 반환
+        // 최소 몇 대의 카메라 설치?
         return answer;
     }
 }
