3e705a4e0e24f60d8807c932c81408c1ec327c49
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-16 13:36:36 +0900

    [Gold IV] Title: 타일 채우기 3, Time: 172 ms, Memory: 49528 KB -BaekjoonHub

--- 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.java
+++ 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.java
@@ -12,14 +12,30 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         long[][] dp = new long[1000001][2];
-        dp[0][0] = 1; dp[0][1] = 1;
-        dp[1][0] = 2; dp[1][1] = 3;
-        dp[2][0] = 7; dp[2][1] = 10;
+        dp[0][0] = 1;
+        dp[1][0] = 2;
+        dp[2][0] = 7;
 
         for (int i = 3; i <= n; i++) {
-            dp[i][0] = (dp[i - 1][0] * 2 + dp[i - 2][0] * 3 + dp[i - 3][1] * 2) % 1000000007;
-            dp[i][1] = (dp[i][0] + dp[i - 1][1]) % 1000000007;
+            dp[i][1] = (dp[i - 3][0] * 2 + dp[i - 1][1]) % 1000000007;
+            dp[i][0] = (dp[i - 1][0] * 2 + dp[i - 2][0] * 3 + dp[i][1]) % 1000000007;
         }
         System.out.println(dp[n][0]);
     }
+
+//    public static void main(String[] args) throws IOException {
+//
+//        int n = Integer.parseInt(br.readLine());
+//        long[][] d = new long[1000001][2];
+//        d[0][0] = 1;
+//        d[1][0] = 2;
+//        d[2][0] = 7;
+//        d[2][1] = 0;
+//
+//        for (int i = 3; i <= n; i++) {
+//            d[i][1] = (d[i - 3][0] + d[i - 1][1]) % 1000000007;
+//            d[i][0] = (2 * d[i - 1][0] + 3 * d[i - 2][0] + 2 * d[i][1]) % 1000000007;
+//        }
+//        System.out.println(d[n][0]);
+//    }
 }
