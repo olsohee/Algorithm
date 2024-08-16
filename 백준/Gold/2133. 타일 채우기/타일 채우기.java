6c80f84e316470fcd831504721fc8139b05e67ab
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-16 12:22:04 +0900

    [Gold IV] Title: 타일 채우기, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -13,13 +13,13 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n + 1];
         dp[0] = 1;
-        
         for (int i = 2; i <= n; i += 2) {
             dp[i] = dp[i - 2] * 3;
-            for (int j = i - 4; j >= 0; j -= 2) {
-                dp[i] += dp[j] * 2;
+            for (int j = i; j >= 4; j -= 2) {
+                dp[i] += dp[j - 4] * 2;
             }
         }
+
         System.out.println(dp[n]);
     }
 }
