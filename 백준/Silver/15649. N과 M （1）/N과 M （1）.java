bbc18c6fdb9f5380295ac58dc4badbc175c792a6
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-19 10:57:42 +0900

    [Silver III] Title: N과 M (1), Time: 1636 ms, Memory: 68616 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,60 +5,37 @@ import java.util.*;
 
 public class Main {
 
-    /**
-     * 1. 아이디어
-     * - 백트래킹
-     *
-     * 2. 시간 복잡도
-     * - 중복 불가: N!
-     *
-     * 3. 자료구조
-     * - 결과값 저장 int[]
-     * - 방문 여부 체크 boolean[]
-     */
-
-    static int N;
-    static int M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static BufferedReader br;
-    static int[] arr;
+    static int n, m;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
 
-        // 입력
-        br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-        visited = new boolean[N+1];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n + 1];
 
-        // 백트래킹
-        func(0);
+        dfs(new ArrayList<>());
     }
 
-    public static void func(int k) {
-
-        // M개 골랐으면 출력후 종료
-        if(k == M) {
-            for(int i = 0; i < M; i++) {
-                System.out.print(arr[i] + " ");
+    private static void dfs(List<Integer> list) {
+        if (list.size() == m) {
+            for (Integer num : list) {
+                System.out.print(num + " ");
             }
             System.out.println();
             return;
         }
 
-        // 재귀 호출
-        for(int i = 1; i <= N; i++) {
-            if(!visited[i]) {
-                arr[k] = i;
-                visited[i] = true;
-                func(k + 1);
-
-                // 재귀가 끝난 후에는 방문 처리 false
-                visited[i] = false;
-            }
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            list.add(i);
+            dfs(list);
+            list.remove((Object) i);
+            visited[i] = false;
         }
     }
 }
