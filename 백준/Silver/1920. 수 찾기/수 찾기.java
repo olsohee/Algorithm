7f8bd0b33056889c84e2e79364ae78338a6d2c3f
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-22 12:05:50 +0900

    [Silver IV] Title: 수 찾기, Time: 1064 ms, Memory: 51512 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,51 +1,46 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
-
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
     static int n;
-    static int m;
-    static int[] arr;
-
+    static int[] nums;
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-
-        m = Integer.parseInt(br.readLine());
+        nums = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        int k = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < k; i++) {
             int num = Integer.parseInt(st.nextToken());
-            System.out.println(search(num));
+            System.out.println(canfind(num) ? 1 : 0);
         }
     }
 
-    public static int search(int num) {
-        int min = 0;
-        int max = n - 1;
-        int mid;
+    private static boolean canfind(int num) {
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
 
-        while (min <= max) {
-            mid = (max + min) / 2;
-            if (num > arr[mid]) {
-                min = mid + 1;
-            } else if (num < arr[mid]) {
-                max = mid - 1;
+            if (nums[mid] == num) return true;
+
+            if (nums[mid] > num) {
+                end = mid - 1;
             } else {
-                return 1;
+                start = mid + 1;
             }
         }
-
-        return 0;
+        return false;
     }
 }
