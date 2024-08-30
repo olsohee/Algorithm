2f0c01dd3903538eb5c27aa282144a22c976e59a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-30 12:20:03 +0900

    [level 2] Title: 호텔 대실, Time: 15.11 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,55 +1,55 @@
 import java.util.*;
 
+
 class Solution {
     public int solution(String[][] book_time) {
-        // 최소한의 객실 수
-        int answer = 0; 
-        
-        // 사용 전 방(우선순위 큐: 시작 시간이 빠른 순으로 정렬)
-        Queue<Info> que1 = new PriorityQueue<>(new Comparator<Info>() {
-            @Override
-            public int compare(Info o1, Info o2) {
-                if (o1.start == o2.start) {
-                    return o1.end - o2.end;
-                }
-                return o1.start - o2.start;
-            }
-        });
-        
+
+        List<Time> list = new ArrayList<>();
         for (String[] time : book_time) {
-            String[] start = time[0].split(":");
-            String[] end = time[1].split(":");
-            que1.add(new Info(Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]), 
-                             Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]) + 10));
+            list.add(new Time(convertTime(time[0]), convertTime(time[1]) + 10));
         }
-        
-        // 사용 중인 방(우선순위 큐: 종료 시간이 빠른 순으로 정렬) (끝나는 시간을 저장함)
-        Queue<Integer> que2 = new PriorityQueue<>();
-        while (!que1.isEmpty()) {
-            Info info = que1.poll();
-            // 최초의 방 쓰는 경우
-            if (que2.isEmpty()) {
-                que2.add(info.end);
-                answer++;
+
+        // 끝나는 시간이 이른 순으로 정렬
+//        Collections.sort(list, (o1, o2) -> {
+//            if (o1.end == o2.end) {
+//                return o1.start - o2.start;
+//            }
+//            return o1.end - o2.end;
+//        });
+
+        Collections.sort(list, (o1, o2) -> {
+            return o1.start - o2.start;
+        });
+
+        // 각 방의 끝나는 시간을 담는 큐(끝나는 시간이 이른 순으로)
+        Queue<Integer> que = new PriorityQueue<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (que.isEmpty()) {
+                que.add(list.get(i).end);
+                continue;
             }
-            // 새로운 방 쓰는 경우
-            else if (!que2.isEmpty() && que2.peek() > info.start) {
-                answer++;
-                que2.add(info.end);
-            } 
-            // 기존 방 쓰는 경우
-            else {
-                que2.poll();
-                que2.add(info.end);
-            }  
+
+            if (que.peek() <= list.get(i).start) {
+                que.poll();
+                que.add(list.get(i).end);
+            } else {
+                que.add(list.get(i).end);
+            }
         }
-        
-        return answer;
+
+        return que.size();
     }
-    
-    class Info {
-        int start, end;
-        public Info(int start, int end) {
+
+    private int convertTime(String str) {
+        return Integer.parseInt(str.split(":")[0]) * 60 + Integer.parseInt(str.split(":")[1]);
+    }
+
+    private class Time {
+
+        int start;
+        int end;
+
+        public Time(int start, int end) {
             this.start = start;
             this.end = end;
         }
