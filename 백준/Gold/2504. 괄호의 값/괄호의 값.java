ed8aec814f989cfa8ee3d31054e76cc9827fe171
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-30 13:22:15 +0900

    [Gold V] Title: 괄호의 값, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -10,66 +10,50 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String str = br.readLine();
-        char[] arr = str.toCharArray();
+        char[] arr = br.readLine().toCharArray();
         Stack<Character> stack = new Stack<>();
-        int answer = 0;
         int temp = 1;
+        int answer = 0;
+        boolean flag = true;
+
         for (int i = 0; i < arr.length; i++) {
-            // 1. '(', '['이면 스택에 넣기
-            if (arr[i] == '(') {
-                stack.push(arr[i]);
-                temp *= 2;
-            }
-            if (arr[i] == '[') {
-                stack.push(arr[i]);
-                temp *= 3;
+            if (stack.isEmpty() || arr[i] == '(' || arr[i] == '[') {
+                stack.add(arr[i]);
+                temp *= (arr[i] == '(') ? 2 : 3;
+                continue;
             }
 
-            // 2. ')', ']'이면 스택에 있는 값 조회하기
+            // 닫는 괄호인 경우
             if (arr[i] == ')') {
-                if (stack.isEmpty()) {
-                    System.out.println(0);
-                    return;
-                }
-                // peek한 값과 짝이 안맞으면 false
                 if (stack.peek() != '(') {
-                    System.out.println(0);
-                    return;
+                    flag = false;
+                    break;
                 }
-                // peek한 값과 짝이 맞으면 스택의 값 pop, temp의 값 answer에 반영하고 temp 값 초기화
-                stack.pop();
+
                 if (arr[i - 1] == '(') {
                     answer += temp;
-                    temp /= 2;
-                } else {
-                    temp /= 2;
-                }
-            }
-            if (arr[i] == ']') {
-                if (stack.isEmpty()) {
-                    System.out.println(0);
-                    return;
-                }
-                if (stack.peek() != '[') {
-                    System.out.println(0);
-                    return;
                 }
                 stack.pop();
+                temp /= 2;
+            }
+            if (arr[i] == ']') {
+                if (stack.peek() != '[') {
+                    flag = false;
+                    break;
+                }
+
                 if (arr[i - 1] == '[') {
                     answer += temp;
-                    temp /= 3;
-                } else {
-                    temp /= 3;
                 }
+                stack.pop();
+                temp /= 3;
             }
         }
-
-        // 마지막에 스택에 값이 남아있으면 false
-        if (!stack.isEmpty()) {
+        if (!flag || !stack.isEmpty()) {
             System.out.println(0);
-        } else {
-            System.out.println(answer);
+            return;
         }
+
+        System.out.println(answer);
     }
 }
