0dc22c7cb3d01abb447c75a9fedd327d31e364ec
olsohee <108605017+olsohee@users.noreply.github.com>
2024-08-31 18:32:01 +0900

    [level 2] Title: 양궁대회, Time: 28.70 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,86 +1,68 @@
 import java.util.*;
 
 class Solution {
-    
-        int n;
-        int[] info;
-        int maxDiff = 0;
-        List<int[]> list = new ArrayList<>();
 
-        public int[] solution(int n, int[] info) {
-            this.n = n;
-            this.info = info;
+    int n;
+    int[] info;
+    int max = 0;
+    int[] answer;
 
-            // n발을 10~0점에 나누기
-            dfs(0, n, new int[11]);
+    public int[] solution(int n, int[] info) {
+        this.n = n;
+        this.info = info;
 
-            int[] answer = new int[11];
-            for (int[] result : list) {
-                int diff = getResult(result);
+        getCombination(0, n, new int[11]);
 
-                if (maxDiff < diff) {
-                    maxDiff = diff;
-                    answer = result;
-                }
+        // 라이언이 우승할 수 없는 경우
+        if (max == 0) return new int[]{-1};
+        
+        return answer;
+    }
 
-                if (maxDiff == diff) {
-                    for (int i = 10; i >= 0; i--) {
-                        if (answer[i] > result[i]) {
-                            break;
-                        } else if (answer[i] < result[i]) {
-                            answer = result;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            System.out.println("maxDiff = " + maxDiff);
-
-            if (maxDiff <= 0) {
-                return new int[]{-1}; // 라이언이 무조건 지거나 비기면 -1 반환
-            }
-            
-            return answer;
-        }
-
-        private void dfs(int idx, int remain, int[] result) {
-            if (idx == 11) {
-                if (remain == 0) {
-                    list.add(result);
-                }
+    private void getCombination(int idx, int remain, int[] score) {
+        if (remain == 0 || idx == 11) {
+            int result = getResult(score); // 라이언 점수 - 어피치 점수 계산
+            // 어피치가 이기는 경우
+            if (result <= 0) {
                 return;
             }
-
-            for (int i = 0; i <= remain; i++) {
-                int[] cloneArr = result.clone();
-                cloneArr[idx] = i;
-                dfs(idx + 1, remain - i, cloneArr);
-            }
-        }
-
-        private int getResult(int[] result) {
-
-            int apeach = 0;
-            int lion = 0;
-            // 똑같은 횟수로 맞혔으면 어피치가 점수 획득
-            // 둘 다 0점이면 둘 다 획득 불가
-            for (int i = 0; i <= 10; i++) {
-                int score = 10 - i;
-
-                if (info[i] >= result[i]) {
-                    if (info[i] != 0) {
-                        apeach += score;
+            // 라이언이 이기는 경우
+            if (result == max) {
+                for (int i = 10; i >= 0; i--) {
+                    if (answer[i] == score[i]) continue;
+                    else if (answer[i] > score[i]) break;
+                    else {
+                        answer = score.clone();
+                        break;
                     }
-                } else {
-                    lion += score;
                 }
             }
-
-            // System.out.println(apeach);
-            // System.out.println(lion);
-
-            return lion - apeach;
+            if (result > max) {
+                max = result;
+                answer = score.clone();
+            }
+            return;
+        }
+        
+        for (int i = 0; i <= remain; i++) {
+            score[idx] = i;
+            getCombination(idx + 1, remain - i, score);
+            score[idx] = 0;
         }
     }
 
+    private int getResult(int[] score) {
+        int totalScore1 = 0; // 어피치 총점
+        int totalScore2 = 0; // 라이언 총점
+        for (int i = 0; i <= 10; i++) {
+            if (info[i] == 0 && score[i] == 0) continue;
+            if (info[i] >= score[i]) {
+                totalScore1 += (10 - i);
+            } else {
+                totalScore2 += (10 - i);
+            }
+        }
+
+        return totalScore2 - totalScore1;
+    }
+}
