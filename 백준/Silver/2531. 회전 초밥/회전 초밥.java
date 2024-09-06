1033bee2906884a3810ccfbba8d91cc595d0ef0c
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-06 12:11:23 +0900

    [Silver I] Title: 회전 초밥, Time: 164 ms, Memory: 18584 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,49 +17,38 @@ public class Main {
         int k = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
         int[] eat = new int[d + 1];
-        int typeCnt = 0;
+
+        int[] sushi = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if (i <= k - 1) {
-                eat[arr[i]]++;
-                if (eat[arr[i]] == 1) typeCnt++;
-            }
+            sushi[i] = Integer.parseInt(br.readLine());
         }
 
-        // 슬라이딩 윈도우
-        int answer = 0;
         int start = 0;
         int end = k - 1;
-
-        while (true) {
-            // 쿠폰 고려해서 답 갱신
-            if (eat[c] > 0) {
-                answer = Math.max(answer, typeCnt);
-            } else {
-                answer = Math.max(answer, typeCnt + 1);
-            }
-
-            // 이동
-            if (start + 1 == n) {
-                break;
-            }
-
-            eat[arr[start]]--;
-
-            if (eat[arr[start]] == 0) typeCnt--;
-            start++;
-            
-            if (end + 1 == n) {
-                end = 0;
-            } else {
-                end++;
-            }
-            eat[arr[end]]++;
-            if (eat[arr[end]] == 1) typeCnt++;
+        int typeCnt = 0;
+        for(int i = start; i <= end; i++) {
+            if (eat[sushi[i]] == 0) typeCnt++;
+            eat[sushi[i]]++; // 먹은 초밥 기록
         }
 
-        System.out.println(answer);
+        int maxTypeCnt = 0;
+
+        while (true) {
+            maxTypeCnt = Math.max(maxTypeCnt, eat[c] > 0 ? typeCnt : typeCnt + 1);
+            // start++
+            eat[sushi[start]]--;
+            if (eat[sushi[start]] == 0) typeCnt--;
+            start = (start == n - 1) ? 0 : start + 1;
+
+            // end++
+            end = (end == n - 1) ? 0 : end + 1;
+            eat[sushi[end]]++;
+            if (eat[sushi[end]] == 1) typeCnt++;
+
+            if (start == 0 && end == k - 1) break;
+        }
+
+        System.out.println(maxTypeCnt);
     }
 }
