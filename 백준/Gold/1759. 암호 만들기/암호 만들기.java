c93df69512ad38df5e23ae5cf89e94db4487d01a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-09 16:11:28 +0900

    [Gold V] Title: 암호 만들기, Time: 156 ms, Memory: 17960 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -4,62 +4,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 시간 복잡도: O(N!)
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
     static int l;
     static int c;
-    static char[] input;
-    static char[] result;
+    static char[] arr;
 
     public static void main(String[] args) throws IOException {
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         l = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        input = new char[c];
-        result = new char[l];
+
+        arr = new char[c];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < c; i++) {
-            input[i] = st.nextToken().charAt(0);
+            arr[i] = st.nextToken().charAt(0);
         }
-        Arrays.sort(input);
+        Arrays.sort(arr);
 
-        dfs(0, 0);
+        dfs(0, "");
     }
 
-    private static void dfs(int depth, int start) {
-        // l개를 다 고른 경우
-        if (depth == l) {
-            if (checkCondition(result)) {
-                for (char c : result) {
-                    System.out.print(c);
-                }
-                System.out.println();
+    private static void dfs(int start, String str) {
+        if (str.length() == l) {
+            if (canAnswer(str)) {
+                System.out.println(str);
             }
             return;
         }
 
-        for (int i = start; i < c; i++) {
-            // 포함 O
-            result[depth] = input[i];
-            dfs(depth + 1, i + 1);
+        for (int i = start; i < arr.length; i++) {
+            dfs(i + 1, str + arr[i]);
         }
     }
 
-    private static boolean checkCondition(char[] result) {
-        // 최소 2개의 자음, 1개의 모음
-        int consonant = 0; // 자음
-        int vowel = 0; // 모음
-        for (int i = 0; i < result.length; i++) {
-            char c = result[i];
+    private static boolean canAnswer(String str) {
+        int num1 = 0; // 모음 개수
+        int num2 = 0; // 자음 개수
+        for (char c : str.toCharArray()) {
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                vowel++;
+                num1++;
             } else {
-                consonant++;
+                num2++;
             }
         }
-        return consonant >= 2 && vowel >= 1;
+
+        return num1 >= 1 && num2 >= 2;
     }
 }
