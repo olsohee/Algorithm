e00fbda215a7d5f2307a08c8effef38efd3c3652
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-12 01:08:25 +0900

    [level 3] Title: 인사고과, Time: 166.92 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -1,43 +1,50 @@
 import java.util.*;
 
+
 class Solution {
     public int solution(int[][] scores) {
-        int n = scores[0][0];
-        int m = scores[0][1];
+        int score1 = scores[0][0];
+        int score2 = scores[0][1];
 
-        // 제외할 사람 제외하기
         Arrays.sort(scores, (o1, o2) -> {
             if (o2[0] == o1[0]) {
-                return o1[1] - o2[1];
+                return o1[1] - o2[1]; // 태도 점수가 같은 경우 오름차순
             }
-            return o2[0] - o1[0];
+            return o2[0] - o1[0]; // 태도 점수 내림차순
         });
 
-        // [0]에 대해서는 내림차순인 상태
-        // [1]에 대해서만 비교하기
-        int last = scores[0][1];
+        int maxScore = scores[0][1]; // 최대 동료 평가 점수
+        List<Score> list = new ArrayList<>();
+        list.add(new Score(scores[0][0], scores[0][1]));
+        
         for (int i = 1; i < scores.length; i++) {
-            if (scores[i][1] < last) {
-                if (scores[i][0] == n && scores[i][1] == m) return -1;
-                scores[i][0] = -1;
-                scores[i][1] = -1;
-            } else {
-                last = scores[i][1];
+            if (scores[i][1] < maxScore) {
+                if (scores[i][0] == score1 && scores[i][1] == score2) return -1;
+                continue;
             }
+            list.add(new Score(scores[i][0], scores[i][1]));
+            maxScore = Math.max(maxScore, scores[i][1]);
         }
-
-        // 등수 계산하기
-        Arrays.sort(scores, (o1, o2) -> (o2[0] + o2[1]) - (o1[0] + o1[1]));
-
+        
         int answer = 0;
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i][0] + scores[i][1] > n + m) {
-                answer++;
-            } else {
+        Collections.sort(list, (o1, o2) -> o2.sum - o1.sum);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).score1 == score1 && list.get(i).score2 == score2) {
+                answer = i + 1;
                 break;
             }
         }
 
-        return answer + 1;
+        return answer;
+    }
+    
+    private class Score {
+        int score1, score2, sum;
+
+        public Score(int score1, int score2) {
+            this.score1 = score1;
+            this.score2 = score2;
+            this.sum = score1 + score2;
+        }
     }
 }
