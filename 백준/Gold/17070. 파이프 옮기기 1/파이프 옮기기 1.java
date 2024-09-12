5129ef47bf13d2a01c13523bbed6b15244516b81
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-12 12:37:52 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,11 +10,9 @@ public class Main {
     static StringTokenizer st;
     static int n;
     static int[][] map;
-    static int[][][] dp;
     static int answer = 0;
 
     public static void main(String[] args) throws IOException {
-
         n = Integer.parseInt(br.readLine());
         map = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
@@ -23,27 +22,27 @@ public class Main {
             }
         }
 
-        dp = new int[3][n + 1][n + 1]; // dp[k][i][j] : k 방향으로 (i, j)에 파이프를 놓는 방법의 수
-
-        //dp
-        // dir: 0(가로), 1(세로), 2(대각선)
+        int[][][] dp = new int[3][n + 1][n + 1]; // 0가로, 1세로, 2대각선
         dp[0][1][2] = 1;
-
         for (int i = 1; i <= n; i++) {
             for (int j = 2; j <= n; j++) {
                 // 가로
                 if (map[i][j] == 0) {
-                    dp[0][i][j] += dp[0][i][j - 1] + dp[2][i][j - 1];
+                    dp[0][i][j] += dp[0][i][j - 1];
+                    dp[0][i][j] += dp[2][i][j - 1];
                 }
 
                 // 세로
                 if (map[i][j] == 0) {
-                    dp[1][i][j] += dp[1][i - 1][j] + dp[2][i - 1][j];
+                    dp[1][i][j] += dp[1][i - 1][j];
+                    dp[1][i][j] += dp[2][i - 1][j];
                 }
 
                 // 대각선
                 if (map[i][j] == 0 && map[i - 1][j] == 0 && map[i][j - 1] == 0) {
-                    dp[2][i][j] += dp[0][i - 1][j - 1] + dp[1][i - 1][j - 1] + dp[2][i - 1][j - 1];
+                    dp[2][i][j] += dp[0][i - 1][j - 1];
+                    dp[2][i][j] += dp[1][i - 1][j - 1];
+                    dp[2][i][j] += dp[2][i - 1][j - 1];
                 }
             }
         }
@@ -51,3 +50,4 @@ public class Main {
         System.out.println(dp[0][n][n] + dp[1][n][n] + dp[2][n][n]);
     }
 }
+
