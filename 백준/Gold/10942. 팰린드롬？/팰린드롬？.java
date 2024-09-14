b7e5e35e7967a52aa9335e3282df152e9882f20c
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-14 12:36:19 +0900

    [Gold IV] Title: 팰린드롬?, Time: 672 ms, Memory: 218176 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,43 +8,41 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static int n, m;
+    static boolean[][] dp;
+    static int[] input;
 
     public static void main(String[] args) throws IOException {
-
-        int n = Integer.parseInt(br.readLine());
-        boolean[][] dp = new boolean[n + 1][n + 1];
-        int[] input = new int[n + 1];
+        n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
+        input = new int[n + 1];
+        dp = new boolean[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             input[i] = Integer.parseInt(st.nextToken());
         }
 
-        // dp 배열 초기화
-        for (int i = 1; i <= n; i++) {
-            dp[i][i] = true;
-        }
-        for (int i = 1; i <= n - 1; i++) {
-            if (input[i] == input[i + 1]) {
-                dp[i][i + 1] = true;
-            }
-        }
-
         // dp
-        for (int i = n - 2; i >= 1; i--) {
-            for (int j = i + 2; j <= n; j++) {
-                if (dp[i + 1][j - 1] && input[i] == input[j]) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j <= n; j++) {
+                if (i == j) {
                     dp[i][j] = true;
+                    continue;
+                }
+
+                if (input[i] == input[j]) {
+                    if (i + 1 == j) dp[i][j] = true;
+                    else if (dp[i + 1][j - 1]) dp[i][j] = true;
                 }
             }
         }
 
-        // 답 출력
         StringBuilder sb = new StringBuilder();
-        int m = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int answer = dp[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] ? 1 : 0;
-            sb.append(answer).append('\n');
+            boolean result = dp[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())];
+            sb.append(result ? 1 : 0)
+                    .append('\n');
         }
         System.out.println(sb);
     }
