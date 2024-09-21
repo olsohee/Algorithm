62472dec71f4d1d78899ec344e56eed9791d18bf
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-21 15:32:02 +0900

    [Gold IV] Title: 최단경로, Time: 856 ms, Memory: 109892 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -4,44 +4,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 시간 복잡도: 
 public class Main {
 
-    static int v;
-    static int e;
-    static int start;
-    static List<Node>[] list;
-    static int[] dist;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        v = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(br.readLine());
-        list = new ArrayList[v + 1];
-        dist = new int[v + 1];
-
-        // 초기 셋팅
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        for (int i = 1; i <= v; i++) {
-            list[i] = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(br.readLine());
+        List<List<Node>> list = new ArrayList<>();
+        for (int i = 0; i <= v; i++) {
+            list.add(new ArrayList<>());
         }
 
-        // 값 초기화
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            list[start].add(new Node(end, weight));
+            int cost = Integer.parseInt(st.nextToken());
+            list.get(start).add(new Node(end, cost));
         }
 
-        // 다익스트라
-        dijkstra(start);
+        // 다익스트라(k에서 시작)
+        int[] dist = new int[v + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        boolean[] visited = new boolean[v + 1];
+//        Queue<Node> que = new LinkedList<>();
 
-        // 출력
+        Queue<Node> que = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        que.add(new Node(k, 0));
+        dist[k] = 0;
+
+        while (!que.isEmpty()) {
+            Node now = que.poll();
+            if (visited[now.end]) continue;
+            visited[now.end] = true;
+
+            for (Node node : list.get(now.end)) {
+                if (dist[node.end] > dist[now.end] + node.cost) {
+                    dist[node.end] = dist[now.end] + node.cost;
+                    que.add(new Node(node.end, dist[node.end]));
+                }
+            }
+        }
+
         for (int i = 1; i <= v; i++) {
             if (dist[i] == Integer.MAX_VALUE) {
                 System.out.println("INF");
@@ -49,43 +57,15 @@ public class Main {
                 System.out.println(dist[i]);
             }
         }
+
     }
 
-    private static void dijkstra(int start) {
-        PriorityQueue<Node> que = new PriorityQueue<>();
-        boolean[] visited = new boolean[v + 1];
-        que.add(new Node(start, 0)); // start에서 start까지 가는데 거리는 0
-        dist[start] = 0;
+    private static class Node {
+        int end, cost;
 
-        while (!que.isEmpty()) {
-            Node node = que.poll();
-
-            // 이미 해당 위치를 시작점으로 방문했으면 패스
-            if (visited[node.e]) {
-                continue;
-            }
-            visited[node.e] = true;
-
-            for (Node n : list[node.e]) {
-                if (dist[n.e] > dist[node.e] + n.w) {
-                     dist[n.e] = dist[node.e] + n.w;
-                     que.add(new Node(n.e, dist[n.e])); // 갱신할 경우 큐에 넣기
-                }
-            }
-        }
-    }
-
-    static class Node implements Comparable<Node> {
-        int e, w;
-
-        public Node(int e, int w) {
-            this.e = e;
-            this.w = w;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.w - o.w;
+        public Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
         }
     }
 }
