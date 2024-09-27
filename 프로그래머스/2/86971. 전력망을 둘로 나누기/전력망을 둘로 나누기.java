4a93324dc81037b153246cb5341296aa10d7b361
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-27 16:28:08 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 9.51 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,26 +3,31 @@ import java.util.*;
 class Solution {
     
     int n;
-    int[][] wires;
-    boolean[] visited;
     boolean[][] map;
+    boolean[] visited;
     int answer = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
         this.n = n;
-        this.wires = wires;
         
-        for (int i = 0; i < n - 1; i++) {
-            visited = new boolean[n + 1];
+        for (int i = 0; i < wires.length; i++) {
             map = new boolean[n + 1][n + 1];
+            visited = new boolean[n + 1];
             
-            for (int j = 0; j < n - 1; j++) {
+            // 1개의 선 끊은 map 채우기
+            for (int j = 0; j < wires.length; j++) {
                 if (i == j) continue;
-                map[wires[j][0]][wires[j][1]] = map[wires[j][1]][wires[j][0]] = true;
+                int n1 = wires[j][0];
+                int n2 = wires[j][1];
+                map[n1][n2] = map[n2][n1] = true;
             }
             
-            int result = dfs(1);
-            int diff = Math.abs(result - (n - result));
+            // 1번 노드를 기준으로 dfs
+            visited[1] = true;
+            int cnt = dfs(1);
+            
+            // 차이
+            int diff = Math.abs(cnt - (n - cnt));
             answer = Math.min(answer, diff);
         }
         
@@ -30,16 +35,16 @@ class Solution {
     }
     
     private int dfs(int start) {
-        
         int sum = 1;
-        visited[start] = true;
         
         for (int i = 1; i <= n; i++) {
-            if (map[start][i] && !visited[i]) {
+            if (map[start][i]) {
+                if (visited[i]) continue;
+                visited[i] = true;
                 sum += dfs(i);
+                visited[i] = false;
             }
         }
-        
         return sum;
     }
 }
