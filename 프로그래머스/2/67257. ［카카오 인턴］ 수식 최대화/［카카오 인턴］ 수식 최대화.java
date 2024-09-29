4d3c30e1e1f53bb3411e7ccff8ff041abf66ca34
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-29 23:37:56 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 2.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,86 +1,81 @@
 import java.util.*;
 
 class Solution {
-    
-    boolean[] visited;
-    ArrayList<String> operators = new ArrayList();
-    ArrayList<Long> numbers = new ArrayList();
+
+    ArrayList<Long> numList = new ArrayList<>();
+    ArrayList<Character> operationList = new ArrayList<>();
+    char[] operations = new char[]{'+', '-', '*'};
+    boolean[] visited = new boolean[3];
     long answer = 0;
-    ArrayList<String> origin = new ArrayList();
-    
+
     public long solution(String expression) {
-        
-        // 연산자와 숫자 구분
+
+        // 숫자, 연산자 분리
         String[] arr = expression.split("((?<=\\d)(?=\\D))|((?<=\\D)(?=\\d))");
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].equals("+") || arr[i].equals("-") || arr[i].equals("*")) {
-                operators.add(arr[i]);
+                operationList.add(arr[i].charAt(0));
             } else {
-                numbers.add(Long.parseLong(arr[i]));
+                numList.add(Long.parseLong(arr[i]));
             }
         }
-        
-        // 우선순위 정하고 계산
-        origin.add("+");
-        origin.add("-");
-        origin.add("*");
-        visited = new boolean[3];
-        dfs(0, new String[3]);
-        
+
+        // 연산자 우선순위 정하기 (dfs로 조합 만들기)
+        dfs(0, new ArrayList<>());
+
         return answer;
     }
-    
-    public void dfs(int depth, String[] priority) {
-        if (depth == 3) {
-            // 우선순위대로 계산하기
-            answer = Math.max(answer, calculate(priority, (ArrayList<String>) operators.clone(), (ArrayList<Long>)numbers.clone()));
-            return;
-        }
-        
-        for (int i = 0; i < origin.size(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                priority[depth] = origin.get(i);
-                
-                dfs(depth + 1, priority);
-                
-                visited[i] = false;
-            }
-        }
-    }
-    
-    public long calculate(String[] priority, ArrayList<String> operators, ArrayList<Long> numbers) {
-        for (int i = 0; i < priority.length; i++) {
-            System.out.print(priority[i] + " ");
-        }
-        System.out.println();
-    
-        for (int i = 0; i < priority.length; i++) {
-            System.out.println(priority[i]);
-            for (int j = 0; j < operators.size(); j++) {
-                if (operators.get(j).equals(priority[i])) {
-                    long result;
-                    System.out.println(numbers.get(j)  +  priority[i]  +numbers.get(j + 1));
-                    if (priority[i].equals("+")) {
-                        result = numbers.get(j) + numbers.get(j + 1);
-                    } else if (priority[i].equals("-")) {
-                        result = numbers.get(j) - numbers.get(j + 1);
-                    } else {
-                        result = numbers.get(j) * numbers.get(j + 1);
+
+    private long getResult(List<Character> operationsOrder) {
+
+        List<Character> operationList = (List<Character>)this.operationList.clone();
+        List<Long> numList = (List<Long>)this.numList.clone();
+
+        for (int i = 0; i < operationsOrder.size(); i++) {
+            char nowOrderOperation = operationsOrder.get(i);
+            for (int j = 0; j < operationList.size(); j++) {
+                if (operationList.get(j) == nowOrderOperation) {
+                    long result = 0;
+                    if (nowOrderOperation == '+') {
+                        result = numList.get(j) + numList.get(j + 1);
                     }
-                    
-                    numbers.remove(j + 1);
-                    numbers.remove(j);
-                    numbers.add(j, result);
-                    
-                    operators.remove(j--);
-                    
-                    System.out.println("result = " + result);
+                    if (nowOrderOperation == '-') {
+                        result = numList.get(j) - numList.get(j + 1);
+                    }
+                    if (nowOrderOperation == '*') {
+                        result = numList.get(j) * numList.get(j + 1);
+                    }
+
+                    numList.remove(j + 1);
+                    numList.remove(j);
+                    numList.add(j, result);
+
+                    operationList.remove(j);
+                    j--;
                 }
             }
         }
-        System.out.println(numbers.get(0));
-        
-        return Math.abs(numbers.get(0));
+
+        return Math.abs(numList.get(0));
+    }
+
+    private void dfs(int depth, List<Character> operationList) {
+        if (depth == 3) {
+            long result = getResult(operationList);
+            answer = Math.max(answer, result);
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (visited[i]) continue;
+
+            operationList.add(operations[i]);
+            visited[i] = true;
+
+            dfs(depth + 1, operationList);
+
+            visited[i] = false;
+            operationList.remove((Object)operations[i]);
+        }
     }
 }
