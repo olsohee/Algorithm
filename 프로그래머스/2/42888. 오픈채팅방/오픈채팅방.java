aac9ad4f0cb84070dae60a6550a1d4cba3397c94
olsohee <108605017+olsohee@users.noreply.github.com>
2024-09-30 12:24:48 +0900

    [level 2] Title: 오픈채팅방, Time: 224.06 ms, Memory: 208 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,43 +3,31 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         Map<String, String> map = new HashMap<>(); // 유저 아이디, 닉네임
-        Queue<String[]> que = new LinkedList<>();
         
         for (String str : record) {
-            String[] arr = str.split(" ");
-            String command = arr[0];
-            String userId = arr[1];
+            if (str.split(" ")[0].equals("Leave")) continue;
             
-            if (command.equals("Enter")) {
-                String nickname = arr[2];
-                que.add(new String[]{"Enter", userId});
-                map.put(userId, nickname);
+            String userId = str.split(" ")[1];
+            String nickname = str.split(" ")[2];
+            map.put(userId, nickname);
+        }
+        
+        List<String> answerList = new ArrayList<>();
+        for (String str : record) {
+            if (str.split(" ")[0].equals("Enter")) {
+                String userId = str.split(" ")[1];
+                answerList.add(map.get(userId) + "님이 들어왔습니다.");
             }
-            
-            if (command.equals("Leave")) {
-                que.add(new String[]{"Leave", userId});
-            }
-            
-            if (command.equals("Change")) {
-                String nickname = arr[2];
-                map.put(userId, nickname);
+            if (str.split(" ")[0].equals("Leave")) {
+                String userId = str.split(" ")[1];
+                answerList.add(map.get(userId) + "님이 나갔습니다.");
             }
         }
         
-        String[] answer = new String[que.size()];
-        int idx = 0;
-        while (!que.isEmpty()) {
-            String[] str = que.poll();
-            if (str[0].equals("Enter")) {
-                answer[idx] = map.get(str[1]) + "님이 들어왔습니다.";
-            }
-            if (str[0].equals("Leave")) {
-                answer[idx] = map.get(str[1]) + "님이 나갔습니다.";
-            }
-            idx++;
+        String[] answer = new String[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
         }
-        
-        
         
         return answer;
     }
