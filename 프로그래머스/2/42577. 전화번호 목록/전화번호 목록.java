864cb8524d56ea532d25692066df137c19300257
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-02 12:31:09 +0900

    [level 2] Title: 전화번호 목록, Time: 274.48 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,25 +1,19 @@
 import java.util.*;
 
 class Solution {
-        public boolean solution(String[] phone_book) {
-
-            // set에 저장
-            Set<String> set = new HashSet<>();
-            for (String s : phone_book) {
-                set.add(s);
-            }
-
-            // 접두사가 있는지 확인 (O(N * 20))
-            for (String s : phone_book) {
-                for (int i = 1; i < s.length(); i++) {
-                    String preStr = s.substring(0, i);
-                    if (set.contains(preStr)) {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
+    public boolean solution(String[] phone_book) {
+        Set<String> numbers = new HashSet<>();
+        for (String number : phone_book) {
+            numbers.add(number);
         }
+        
+        for (String number : phone_book) {
+            for (int i = 1; i < number.length(); i++) {
+                String str = number.substring(0, i);
+                if (numbers.contains(str)) return false;
+            }
+        }
+        
+        return true;
+    }
 }
-
