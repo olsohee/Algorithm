b713dde110ee87656dcf663149056989f302fe56
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-03 02:09:31 +0900

    [level 2] Title: 귤 고르기, Time: 82.53 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,44 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        // map에 담기 (key: 귤 넘버, value: 갯수)
+        // Map에 넣기
         Map<Integer, Integer> map = new HashMap<>();
-        for (Integer num : tangerine) {
+        for (int num : tangerine) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        // map2에 갯수별로 귤 분류하기 (key: 갯수, value: 귤 넘버)
-        Map<Integer, List<Integer>> map2 = new HashMap<>();
-        for (Integer num : map.keySet()) {
-            int cnt = map.get(num);
-            List<Integer> list = map2.getOrDefault(cnt, new ArrayList<>());
-            list.add(num);
-            map2.put(cnt, list);
-        }
-        
-        List<Integer> cntList = new ArrayList<>();
-        for (int cnt : map2.keySet()) {
-            cntList.add(cnt);
-        }
-        Collections.sort(cntList);
     
-        // 갯수 많은 것부터 채우기
-        int sum = 0;
-        int answer = 0;
-        for (int i = cntList.size() - 1; i >= 0; i--) {
-            int cnt = cntList.get(i);
-            
-            for (int num : map2.get(cnt)) {
-                if (sum < k) {
-                    sum += cnt;
-                    answer++;
-                }
-            }
-            if (sum >= k) {
-                break;
-            }
+        
+        // 사이즈별 객체로 만들기
+        List<Size> list = new ArrayList<>();
+        for (int size : map.keySet()) {
+            list.add(new Size(size, map.get(size)));
+        }
+        
+        // 객체 정렬
+        Collections.sort(list, (o1, o2) -> o2.cnt - o1.cnt);
+        
+        // 갯수가 많은 것 우선으로 고르기 (순차 탐색)
+        int answer = 0; // 종류 수
+        for (int i = 0; i < list.size(); i++) {
+            if (k <= 0) break;
+            k -= list.get(i).cnt;
+            answer++;
         }
         
         return answer;
+    }
+    
+    private class Size {
+        
+        int size;
+        int cnt;
+        
+        public Size(int size, int cnt) {
+            this.size = size;
+            this.cnt = cnt;
+        }
     }
 }
