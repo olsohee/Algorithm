d0462daf05c9af3cfcb3e1de62800fbdd1fe1f86
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-04 18:58:40 +0900

    [Gold II] Title: 레이스, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/1508. 레이스/레이스.java
+++ 백준/Gold/1508. 레이스/레이스.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,63 +8,60 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int[] arr;
-    static int n, m, k;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        arr = new int[k];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken()); // k개의 위치에 m명 위치시키기
 
         st = new StringTokenizer(br.readLine());
+        int[] locations = new int[k];
         for (int i = 0; i < k; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            locations[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 이분탐색: 간격 구하기 (최대)
-        int min = 1;
-        int max = arr[arr.length - 1] - arr[0];
-        while (min <= max) {
-            int mid = (min + max) / 2;
+        // 이분탐색 (심판 간 최대 거리 찾기)
+        int start = 1;
+        int end = n;
 
-            int cnt = 1;
-            int before = arr[0];
-            for (int i = 1; i < arr.length; i++) {
-                if (arr[i] - before >= mid) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // 심판 간 최대 거리가 mid일 때, 위치시키는 심판 명수
+            int cnt = 1; // locations[0]에 무조건 위치시킴
+            int preLocation = locations[0];
+            for (int i = 1; i < locations.length; i++) {
+                if (locations[i] - preLocation >= mid) {
                     cnt++;
-                    before = arr[i];
+                    preLocation = locations[i];
                 }
             }
 
-            // 간격 늘리기
             if (cnt >= m) {
-                min = mid + 1;
-            }
-            // 간격 줄이기
-            else {
-                max = mid - 1;
+                start = mid + 1; // m명보다 많이 위치시킬 수 있으면 -> 간격 더 넒게 조정
+            } else {
+                end = mid - 1;
             }
         }
 
-        // max가 간격이 됨
-        String answer = "1";
-        int before = arr[0];
-        int cnt = 1;
-        for (int i = 1; i < arr.length; i++) {
-            if (cnt == m) {
-                answer += "0";
-                continue;
-            }
-            if (arr[i] - before >= max) {
-                answer += "1";
-                before = arr[i];
-                cnt++;
-            } else {
-                answer += "0";
+        // end = 심판 간 최대 거리
+        // 사전순으로 늦은 것을 출력 = 맨 앞부터 심판 위치시키기
+        int[] answer = new int[locations.length];
+        answer[0] = 1;
+        int locateCnt = 1;
+        int preLocation = locations[0];
+        for (int i = 1; i < locations.length; i++) {
+            if (locateCnt == m) break;
+            if (locations[i] - preLocation >= end) {
+                preLocation = locations[i];
+                answer[i] = 1;
+                locateCnt++;
             }
         }
-        System.out.println(answer);
+
+        for (int i : answer) {
+            System.out.print(i);
+        }
     }
 }
