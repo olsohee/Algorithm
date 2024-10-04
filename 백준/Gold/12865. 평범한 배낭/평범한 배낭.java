1ec412e7196154e34be879a70ea31062ffad6927
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-04 18:35:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 53784 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -9,30 +9,39 @@ public class Main {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-
         st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[n + 1][k + 1]; //dp[i][j] = i번째 무게까지 고려했을 때 j만큼의 무게를 담았을 때 최대 가치
-        int[][] arr = new int[n + 1][2];
+        int[][] value = new int[n + 1][2]; // value[i] = i번 물건의 무게, 가치
 
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            value[i][0] = Integer.parseInt(st.nextToken());
+            value[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        // dp
+        int[][] dp = new int[n + 1][k + 1];
+        // dp[i][j] = i번 물건까지 고려했을 때, j 무게를 채우는데 얻는 최대 가치
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
-                if (arr[i][0] > j) {
-                    dp[i][j] = dp[i - 1][j];
+            int w = value[i][0]; // i번 물건의 무게
+            int v = value[i][1]; // i번 물건의 가치
+            for (int j = 0; j <= k; j++) {
+                if (j >= w) {
+                    dp[i][j] = Math.max(v + dp[i - 1][j - w], dp[i - 1][j]);
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], arr[i][1] + dp[i - 1][j - arr[i][0]]);
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        // 최대 k만큼 짐을 싸면서, 가치의 최댓값!
+//        for (int i = 0; i <= n; i++) {
+//            for (int j = 0; j <= k; j++) {
+//                System.out.print(dp[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
         System.out.println(dp[n][k]);
     }
 }
