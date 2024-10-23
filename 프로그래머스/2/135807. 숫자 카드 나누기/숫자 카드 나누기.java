64b742bac4719b7d65e1d29a6712010bd4869835
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-24 01:47:51 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 46.06 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,42 +1,47 @@
+import java.util.*;
+
 class Solution {
-    
     public int solution(int[] arrayA, int[] arrayB) {
+        /*
+        arrayA의 최대 공약수 -> arrayB는 나눌 수 없음
+        arrayB의 최대 공약수 -> arrayA는 나눌 수 없음
+        
+        최대 공약수 구하기: 재귀
+        재귀로 나온 최대 공약수를 다른 array와 비교하기 
+        */
         int answer = 0;
         
-        // 각 집합의 최대 공약수 찾은 후, 상대 집합을 나눌 수 없는지 확인
+        Arrays.sort(arrayA);
         int gcdA = arrayA[0];
-        int gcdB = arrayB[0];
-        for (int i = 0; i < arrayA.length; i++) {
-            gcdA = findGcd(gcdA, arrayA[i]);
-            gcdB = findGcd(gcdB, arrayB[i]);
+        for (int i = 1; i < arrayA.length; i++) {
+            gcdA = func(gcdA, arrayA[i]);
         }
-        
-        if (notDivisible(gcdA, arrayB)) {
+         
+        if (canResult(gcdA, arrayB)) {
             answer = Math.max(answer, gcdA);
         }
         
-        if (notDivisible(gcdB, arrayA)) {
+        Arrays.sort(arrayB);
+        int gcdB = arrayB[0];
+        for (int i = 1; i < arrayB.length; i++) {
+            gcdB = func(gcdB, arrayB[i]);
+        }
+         
+        if (canResult(gcdB, arrayA)) {
             answer = Math.max(answer, gcdB);
         }
         
         return answer;
     }
     
-    private int findGcd(int n1, int n2) {
-        // n1 <= n2로 만들기
-        if (n2 < n1) {
-            int temp = n1;
-            n1 = n2;
-            n2 = temp;
-        }
-        
+    private int func(int n1, int n2) {
         if (n2 % n1 == 0) return n1;
-        return findGcd(n1, n2 % n1);  
+        return func(n2 % n1, n1);
     }
     
-    private boolean notDivisible(int n, int[] arr) {
+    private boolean canResult(int num, int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] % n == 0) return false;
+            if (arr[i] % num == 0) return false;
         }
         return true;
     }
