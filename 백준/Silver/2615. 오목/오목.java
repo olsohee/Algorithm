a136730588c7430644ed84dcce936f4180b52f95
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-24 14:37:31 +0900

    [Silver I] Title: 오목, Time: 128 ms, Memory: 15988 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -8,6 +8,8 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int[][] map = new int[19][19];
+    static int[] dy = {0, 1, -1, 1};
+    static int[] dx = {1, 0, 1, 1};
     static int answer;
     static int[] answerNode = new int[2];
 
@@ -21,97 +23,81 @@ public class Main {
 
         for (int i = 0; i < 19; i++) {
             for (int j = 0; j < 19; j++) {
-                if (map[i][j] == 1 || map[i][j] == 2) {
-                    if (j == 0 || (j - 1 >= 0 && map[i][j - 1] != map[i][j])) {
-                        dfs(i, j, i, j, 0, 1); // dir = 0 (오른쪽)
-                    }
-                    if (i == 0 || (i - 1 >= 0 && map[i - 1][j] != map[i][j])) {
-                        dfs(i, j, i, j, 1, 1); // dir = 1 (아래)
-                    }
-                    if ((i == 0 || j == 0) || (j - 1 >= 0 && i - 1 >= 0 && map[i - 1][j - 1] != map[i][j])) {
-                        dfs(i, j, i, j, 2, 1); // dir = 0 (오른쪽 아래 대각선)
-                    }
-                    if ((i == 18 || j == 0) || (j + 1 < 19 && i - 1 >= 0 && map[i + 1][j - 1] != map[i][j])) {
-                        dfs(i, j, i, j, 3, 1); // dir = 0 (오른쪽 위 대각선)
-                    }
+                if (map[i][j] == 0) continue;
+                // 오른쪽 이동
+                if (j == 0 || (j - 1 >= 0 && map[i][j - 1] != map[i][j])) {
+                    dfs(i, j, i, j, 0, 1);
+                }
+                // 아래 이동
+                if (i == 0 || (i - 1 >= 0 && map[i - 1][j] != map[i][j])) {
+                    dfs(i, j, i, j, 1, 1);
+                }
+                // 오른쪽 위 대각선 이동
+                if ((i == 18 || j == 0) || (i + 1 < 19  && j - 1 >= 0 && map[i + 1][j - 1] != map[i][j])) {
+                    dfs(i, j, i, j, 2, 1);
+                }
+                // 오른쪽 아래 대각선 이동
+                if ((i == 0 || j == 0) || (i - 1 >= 0  && j - 1 >= 0 && map[i - 1][j - 1] != map[i][j])) {
+                    dfs(i, j, i, j, 3, 1);
                 }
             }
         }
 
+        /*
+        시작점에서 오른쪽, 위, 대각선 위, 대각선 아래로 이동
+
+        if (시작점이 끝 or 반대쪽과 다른 색) -> dfs시작
+
+        if (같은 색 5개 -> 현재가 끝 or 다음번이 다른 색) -> 답
+         */
         System.out.println(answer);
         if (answer != 0) {
             System.out.print(answerNode[0] + " " + answerNode[1]);
         }
+
     }
 
-    private static void dfs(int startY, int startX, int y, int x, int dir, int cnt) { // cnt = map[y][x] 포함한 갯수
+    private static void dfs(int startY, int startX, int y, int x, int dir, int cnt) {
         if (cnt == 5) {
-            boolean canAnswer = false;
-            // 여섯개가 아닌지 확인 후 답 반영
-            if (dir == 0) {
-                if (x + 1 < 19) {
-                    if (map[y][x] != map[y][x + 1]) {
-                        canAnswer = true;
+            // 6목이 아니어야 답 가능
+            boolean isAnswer = false;
+            switch (dir) {
+                case 0:
+                    if (x == 18 || (x + 1 < 19 && map[y][x + 1] != map[y][x])) {
+                        isAnswer = true;
                     }
-                } else {
-                    canAnswer = true;
-                }
-            }
-            if (dir == 1) {
-                if (y + 1 < 19) {
-                    if (map[y][x] != map[y + 1][x]) {
-                        canAnswer = true;
+                    break;
+                case 1:
+                    if (y == 18 || (y + 1 < 19 && map[y + 1][x] != map[y][x])) {
+                        isAnswer = true;
                     }
-                } else {
-                    canAnswer = true;
-                }
-            }
-            if (dir == 2) {
-                if (x + 1 < 19 && y + 1 < 19) {
-                    if (map[y][x] != map[y + 1][x + 1]) {
-                        canAnswer = true;
+                    break;
+                case 2:
+                    if ((y == 0 || x == 18) || (y - 1 >= 0 && x + 1 < 19 && map[y - 1][x + 1] != map[y][x])) {
+                        isAnswer = true;
                     }
-                } else {
-                    canAnswer = true;
-                }
-            }
-            if (dir == 3) {
-                if (x + 1 < 19 && y - 1 >= 0) {
-                    if (map[y][x] != map[y - 1][x + 1]) {
-                        canAnswer = true;
+                    break;
+                case 3:
+                    if ((y == 18 || x == 18) || (y + 1 < 19 && x + 1 < 19 && map[y + 1][x + 1] != map[y][x])) {
+                        isAnswer = true;
                     }
-                } else {
-                    canAnswer = true;
-                }
             }
 
-            if (canAnswer) {
+            if (isAnswer) {
                 answer = map[y][x];
                 answerNode[0] = startY + 1;
                 answerNode[1] = startX + 1;
             }
-        }
 
-        switch (dir) {
-            case 0:
-                if (x + 1 < 19 && map[y][x + 1] == map[y][x]) {
-                    dfs(startY, startX, y, x + 1, dir, cnt + 1);
-                }
-                break;
-            case 1:
-                if (y + 1 < 19 && map[y + 1][x] == map[y][x]) {
-                    dfs(startY, startX, y + 1, x, dir, cnt + 1);
-                }
-                break;
-            case 2:
-                if (x + 1 < 19 && y + 1 < 19 && map[y + 1][x + 1] == map[y][x]) {
-                    dfs(startY, startX, y + 1, x + 1, dir, cnt + 1);
-                }
-                break;
-            case 3:
-                if (x + 1 < 19 && y - 1 >= 0 && map[y - 1][x + 1] == map[y][x]) {
-                    dfs(startY, startX, y - 1, x + 1, dir, cnt + 1);
-                }
+            return;
+        }
+        
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if (ny >= 0 && ny < 19 && nx >= 0 && nx < 19) {
+            if (map[y][x] == map[ny][nx]) {
+                dfs(startY, startX, ny, nx, dir, cnt + 1);
+            }
         }
     }
 }
