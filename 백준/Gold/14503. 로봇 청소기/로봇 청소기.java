5f37b26cd3b500dc7aca634c51b7579085adb471
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-26 18:11:11 +0900

    [Gold V] Title: 로봇 청소기, Time: 104 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,8 +12,9 @@ public class Main {
     static int[][] map;
     static int dir;
     static int nowY, nowX;
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
@@ -33,105 +35,47 @@ public class Main {
             }
         }
 
-        // map[i][j] = -1(청소O), 0(청소X), 1(벽)
-        while (true) {
+        start();
 
-            // 1. 현재 칸 청소
-            if (map[nowY][nowX] == 0) {
-                map[nowY][nowX] = -1;
-            }
+        System.out.println(answer);
+    }
 
-            // 2. 주변 네 칸 청소유무 확인
-            boolean allClean = true;
-            for (int i = 0; i < 4; i++) {
-                int ny = nowY + dy[i];
-                int nx = nowX + dx[i];
-                if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-                // 주변 네 칸 중 하나라도 청소되어 있지 않으면 체크하고 break
-                if (map[ny][nx] == 0) {
-                    allClean = false;
-                    break;
-                }
-            }
+    private static void start() {
+        // 1. 현재 칸 청소
+        if (map[nowY][nowX] == 0) {
+            map[nowY][nowX] = -1;
+            answer++;
+        }
 
-            // 3. 후진 OR 주변 네 칸 중 청소되지 않은 칸으로 이동
-            if (allClean) {
-                if (!back()) {
-                    break;
-                }
-            } else {
+        // 2. 주변 네 칸 중 청소되지 않은 경우
+        for (int i = 0; i < 4; i++) {
+            int ny = nowY + dy[i];
+            int nx = nowX + dx[i];
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+            if (map[ny][nx] == 0) {
                 while (true) {
-                    dir--;
-                    if (dir == -1) dir = 3;
+                    dir = (dir + 3) % 4;
 
-                    if (go()) {
-                        break;
+                    // 청소 X인 경우 전진
+                    if (map[nowY + dy[dir]][nowX + dx[dir]] == 0) {
+                        nowY = nowY + dy[dir];
+                        nowX = nowX + dx[dir];
+                        start();
+                        return;
                     }
                 }
             }
         }
 
-        // 청소된 칸 계산
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == -1) answer++;
-            }
+        // 3. 주변 네 칸이 모두 청소된 경우
+        int ny = nowY + dy[(dir + 2) % 4];
+        int nx = nowX + dx[(dir + 2) % 4];
+        // 후진 가능하면 후진, 불가하면 끝내기
+        if (ny >= 0 && ny < n && nx >= 0 && nx < m && map[ny][nx] != 1) {
+            nowY = ny;
+            nowX = nx;
+            start();
         }
-        System.out.println(answer);
-    }
-
-    private static boolean go() {
-        // 앞쪽이 청소되지 않은 빈 칸(0)이면 전진 후 return true
-        // 앞쪽이 벽이거나 청소된 칸이면 return false
-        switch (dir) {
-            case 0: // 위로
-                if (nowY - 1 >= 0 && map[nowY - 1][nowX] == 0) {
-                    nowY--;
-                    return true;
-                }
-                break;
-            case 1: // 오른쪽으로
-                if (nowX + 1 < m && map[nowY][nowX + 1] == 0) {
-                    nowX++;
-                    return true;
-                }
-                break;
-            case 2: // 아래로
-                if (nowY + 1 < n && map[nowY + 1][nowX] == 0) {
-                    nowY++;
-                    return true;
-                }
-                break;
-            case 3: // 왼쪽으로
-                if (nowX - 1 >= 0 && map[nowY][nowX - 1] == 0) {
-                    nowX--;
-                    return true;
-                }
-        }
-        return false;
-    }
-
-    private static boolean back() {
-        // 후진할 수 있으면 후진 후 return true
-        // 벽(1)이면 return false
-        switch (dir) {
-            case 0: // 아래로
-                if (nowY + 1 >= n || map[nowY + 1][nowX] == 1) return false;
-                nowY++;
-                break;
-            case 1: // 왼쪽으로
-                if (nowX - 1 < 0 || map[nowY][nowX - 1] == 1) return false;
-                nowX--;
-                break;
-            case 2: // 위로
-                if (nowY - 1 < 0 || map[nowY - 1][nowX] == 1) return false;
-                nowY--;
-                break;
-            case 3: // 오른쪽으로
-                if (nowX + 1 >= m || map[nowY][nowX + 1] == 1) return false;
-                nowX++;
-        }
-        return true;
     }
 }
