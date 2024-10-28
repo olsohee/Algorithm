538cd2ca198c8539423e9011a5869dee0a5489f4
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-29 00:33:08 +0900

    [Gold IV] Title: 돌 그룹, Time: 192 ms, Memory: 45944 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.java
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.java
@@ -8,52 +8,70 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static boolean[][] visited = new boolean[1001][1001];
-    static int answer = 0;
+    static int sum;
 
     public static void main(String[] args) throws IOException {
 
         st = new StringTokenizer(br.readLine());
-        int[] stones = new int[3];
-        for (int i = 0; i < 3; i++) {
-            stones[i] = Integer.parseInt(st.nextToken());
-        }
-        if (stones[0] == stones[1] && stones[1] == stones[2]) {
-            System.out.println(1);
-            return;
-        }
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        sum = a + b + c;
 
-        // dfs
-        visited[stones[0]][stones[1]] = true;
-        dfs(stones.clone(), 0, 1);
-        dfs(stones.clone(), 0, 2);
-        dfs(stones.clone(), 1, 2);
+        Queue<Stone> que = new LinkedList<>();
+        que.add(new Stone(a, b, c));
+        visited[a][b] = true;
+        int answer = 0;
+        while (!que.isEmpty()) {
+            Stone stone = que.poll();
+            if (stone.a == stone.b && stone.b == stone.c) {
+                answer = 1;
+                break;
+            }
+
+            // a, b 재조정
+            if (stone.a != stone.b) {
+                int newA = stone.a > stone.b ? stone.a - stone.b : stone.a * 2;
+                int newB = stone.a > stone.b ? stone.b * 2 : stone.b - stone.a;
+                if (newA <= 1000 && newB <= 1000 && !visited[newA][newB]) {
+                    visited[newA][newB] = true;
+                    que.add(new Stone(newA, newB, stone.c));
+                }
+            }
+
+            // a, c 재조정
+            if (stone.a != stone.c) {
+                int newA = stone.a > stone.c ? stone.a - stone.c : stone.a * 2;
+                int newC = stone.a > stone.c ? stone.c * 2 : stone.c - stone.a;
+                if (newA <= 1000 && !visited[newA][stone.b]) {
+                    visited[newA][stone.b] = true;
+                    que.add(new Stone(newA, stone.b,newC));
+                }
+            }
+
+            // b, c 재조정
+            if (stone.b != stone.b) {
+                int newB = stone.b > stone.c ? stone.b - stone.c : stone.b * 2;
+                int newC = stone.b > stone.c ? stone.c * 2 : stone.c - stone.b;
+                if (newB <= 1000 && !visited[stone.a][newB]) {
+                    visited[stone.a][newB] = true;
+                    que.add(new Stone(stone.a, newB, newC));
+                }
+            }
+        }
 
         System.out.println(answer);
     }
 
-    private static void dfs(int[] stones, int idx1, int idx2) {
-        // idx1번 돌과 idx2번 돌의 개수 재조정
-        if (stones[idx1] < stones[idx2] && stones[idx1] * 2 <= 1000) {
-            stones[idx2] -= stones[idx1];
-            stones[idx1] *= 2;
-        } else if (stones[idx2] < stones[idx1] && stones[idx2] * 2 <= 1000) {
-            stones[idx1] -= stones[idx2];
-            stones[idx2] *= 2;
-        } else {
-            return;
-        }
+    static class Stone {
+        int a; // 첫번째 그룹의 개수
+        int b; // 두번째 그룹의 개수
+        int c; // 세번째 그룹의 개수
 
-        if (stones[0] == stones[1] && stones[1] == stones[2]) {
-            answer = 1;
-            return;
-        }
-
-        // next dfs
-        if (!visited[stones[0]][stones[1]]) {
-            visited[stones[0]][stones[1]] = true;
-            dfs(stones.clone(), 0, 1);
-            dfs(stones.clone(), 0, 2);
-            dfs(stones.clone(), 1, 2);
+        public Stone(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
         }
     }
 }
