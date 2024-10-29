b6bcaea000881fd8c92146da3f6f6cd9ff391095
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-29 11:43:39 +0900

    [Gold III] Title: 스티커 붙이기, Time: 156 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.java
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.java
@@ -1,118 +1,95 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
-
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
     static int n; // 세로
     static int m; // 가로
     static int k; // 스티커 개수
-    static int[][] map;
-    static int answer = 0;
+    static int[][] notebook;
+    static int answer;
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-
-        for (int i = 0; i < k; i++) {
-            // 스티커 입력받기
+        notebook = new int[n][m];
+        while (k-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int r = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int[][] sticker = new int[r][c];
-            for (int j = 0; j < r; j++) {
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int[][] sticker = new int[y][x];
+            for (int i = 0; i < y; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < c; k++) {
-                    sticker[j][k] = Integer.parseInt(st.nextToken());
+                for (int j = 0; j < x; j++) {
+                    sticker[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            // 스티커 붙이기
-            putSticker(sticker);
+            putOnNotebook(sticker);
         }
-
-        // 스티커가 붙은 칸 수 계산
-        getStickerArea();
         System.out.println(answer);
     }
 
-    private static void putSticker(int[][] sticker) {
+    private static void putOnNotebook(int[][] sticker) {
+        int turnCnt = 0;
+        while (turnCnt < 4) {
+            // 붙이기 시도
+            int y = sticker.length;
+            int x = sticker[0].length;
+            if (isSuccess(sticker, y, x)) {
+              break;
+            }
 
-        for (int i = 0; i < 4; i++) {
-            // 지금 방향에서 붙일 수 있는지 확인
-            int x = sticker[0].length; // 가로
-            int y = sticker.length; // 세로
+            // 못붙였으면 회전
+            turnCnt++;
+            int[][] turnSticker = new int[x][y];
+            for (int i = 0; i < y; i++) {
+                for (int j = 0; j < x; j++) {
+                    turnSticker[j][y - 1 - i] = sticker[i][j];
+                }
+            }
+            sticker = turnSticker;
+        }
+    }
 
-            // map 범위 내에 있다면 붙이기 시도
-            for (int j = 0; j <= (n - y); j++) {
-                for (int k = 0; k <= (m - x); k++) {
-                    // map 범위를 벗어나지 않는지 확인
-                    if (j + y > n || k + x > m) {
-                        continue;
+    private static boolean isSuccess(int[][] sticker, int y, int x) {
+        if (y > n || x > m) return false;
+        for (int i = 0; i <= n - y; i++) {
+            for (int j = 0; j <= m - x; j++) {
+                // (i, j)를 시작점으로 붙이기
+                boolean canPut = true;
+
+                outer: for (int a = 0; a < y; a++) {
+                    for (int b = 0; b < x; b++) {
+                        if (sticker[a][b] == 1) { // 스티커 자리가 노트북에 비워져있는지 확인(스티커가 1, 노트북은 0)
+                            if (notebook[a + i][b + j] != 0) {
+                                canPut = false;
+                                break outer;
+                            }
+                        }
                     }
-                    // 붙이기
-                    if (canPut(sticker, j, k)) {
-                        return;
+                }
+
+                // 붙이기
+                if (canPut) {
+                    for (int a = 0; a < y; a++) {
+                        for (int b = 0; b < x; b++) {
+                            if (sticker[a][b] == 1) {
+                                notebook[a + i][b + j] = -1;
+                                answer++;
+                            }
+                        }
                     }
-                }
-            }
-            // 90도로 스티커 회전
-            sticker = rotation(sticker);
-        }
-    }
-
-    private static boolean canPut(int[][] sticker, int startY, int startX) {
-        int x = sticker[0].length; // 가로
-        int y = sticker.length; // 세로
-
-        // 붙일 수 있는지 확인 (스티커가 1인데 map도 1이면 못붙임)
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                if (sticker[i][j] == 1 && map[i + startY][j + startX] == 1) {
-                    return false;
+                    return true;
                 }
             }
         }
-
-        // 붙이기
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                if (sticker[i][j] == 1) {
-                    map[i + startY][j + startX] = 1;
-                }
-            }
-        }
-        return true;
-    }
-
-
-    private static int[][] rotation(int[][] sticker) {
-        int newX = sticker.length;
-        int newY = sticker[0].length;
-        int[][] newSticker = new int[newY][newX];
-
-        for (int i = 0; i < newX; i++)
-            for (int j = 0; j < newY; j++)
-                newSticker[j][newX - i - 1] = sticker[i][j];
-
-        return newSticker;
-    }
-
-    private static void getStickerArea() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 1) {
-                    answer++;
-                }
-            }
-        }
+        return false;
     }
 }
