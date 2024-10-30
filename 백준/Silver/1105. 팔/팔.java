d892b8871a952720c23a4972083888a418df1609
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-30 11:13:37 +0900

    [Silver I] Title: 팔, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        String start = st.nextToken();
+        String end = st.nextToken();
+
+        int cnt = 0;
+        if (start.length() == end.length()) {
+            for (int i = 0; i < start.length(); i++) {
+                if (start.charAt(i) == end.charAt(i)) {
+                    if (start.charAt(i) == '8') cnt++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
