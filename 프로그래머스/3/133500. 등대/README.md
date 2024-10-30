c384544d9e060ccee0022ed3b0b8ac66b5f27165
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-30 17:37:10 +0900

    [level 3] Title: 등대, Time: 70.97 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/README.md
+++ 프로그래머스/3/133500. 등대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등대 - 133500 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133500?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133500) 
 
 ### 성능 요약
 
-메모리: 126 MB, 시간: 81.19 ms
+메모리: 110 MB, 시간: 70.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 12:13:13
+2024년 10월 30일 17:37:05
 
 ### 문제 설명
 
--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -2,59 +2,52 @@ import java.util.*;
 
 class Solution {
     
-    List<Integer>[] route;
-    int answer = 0;
+    List<Integer>[] list;
+    int n;
+    boolean[] visited;
+    int answer;
     
     public int solution(int n, int[][] lighthouse) {
-        route = new List[n + 1];
+        this.n = n;
+        list = new List[n + 1];
+        visited = new boolean[n + 1];
+        
         for (int i = 0; i <= n; i++) {
-            route[i] = new ArrayList<>();
+            list[i] = new ArrayList<>();
         }
         
-        // route 반영
         for (int[] light : lighthouse) {
-            route[light[0]].add(light[1]);
-            route[light[1]].add(light[0]);
+            list[light[0]].add(light[1]);
+            list[light[1]].add(light[0]);
         }
         
-        // dfs(1번 노드를 루트 노드로)
-        dfs(1, 0);        
-        
+        // 1번 노드는 루트 노드라고 가정하고, dfs 시작
+        dfs(1);
         
         return answer;
     }
     
-    /*
-    리프 노드 -> 켜지 X (1 반환)
-    현재 노드의 자식 노드들의 dfs 합이 0 
-        = 자식 노드들이 모두 켠 것 
-        -> 현재 노드는 켜지 X (1 반환)
-    현재 노드의 자식 노도들의 dfs 합이 0 초과
-        = 자식 노드 중 켜지 않은 노드 존재
-        -> 현재 노드 켜기 (0 반환)
+    private int dfs(int now) {
         
-    켤 때마다(0 반환할 때마다) answer++
-    */
-    private int dfs(int now, int pre) {
-        // 리프 노드인 경우 켜지 X (1 반환)
-        if (route[now].size() == 1 && route[now].get(0) == pre) {
-            return 1; 
-        }
+        visited[now] = true;
         
-        // 자식 노드 dfs
-        int childSum = 0;
-        for (int next : route[now]) {
-            if (next == pre) continue;
-            childSum += dfs(next, now);
-        }
-        
-        // 자식 노드들의 합이 0 = 모두 켠 것 = 현재 노드 켜지 X
-        // 자식 노드들의 합이 1이상 = 하나라도 켜지 않은 것 = 현재 노드 킴 O
-        if (childSum == 0) {
+        // 리프노드이면 끄기
+        if (list[now].size() == 1 && visited[list[now].get(0)]) {
             return 1;
-        } else {
-            answer++;
-            return 0;
         }
+        
+        int sum = 0;
+        
+        for (int next : list[now]) {
+            if (!visited[next]) {
+                sum += dfs(next);
+            }
+        }
+        
+        if (sum > 0) {
+            answer++;
+            return 0; // 켜기
+        }
+        else return 1; // 끄기
     }
 }
