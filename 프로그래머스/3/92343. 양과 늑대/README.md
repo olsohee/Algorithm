10cd83b831fb32e2aa69f3458788608b4ea617bb
olsohee <108605017+olsohee@users.noreply.github.com>
2024-10-31 14:52:59 +0900

    [level 3] Title: 양과 늑대, Time: 11.57 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343#) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 3.99 ms
+메모리: 95 MB, 시간: 11.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 13:23:33
+2024년 10월 31일 14:52:54
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -2,42 +2,56 @@ import java.util.*;
 
 class Solution {
     
-    int answer = 0;
+    int n;
     int[] info;
     int[][] edges;
+    List<List<Integer>> routeList = new ArrayList<>();
+    int answer;
+    boolean[] visited;
     
     public int solution(int[] info, int[][] edges) {
+        n = info.length;
         this.info = info;
         this.edges = edges;
         
-        // 루트 노드(0번)부터 시작
-        dfs(0, new boolean[info.length], 0, 0);
+        for (int i = 0; i < n; i++) {
+            routeList.add(new ArrayList<>());
+        }
         
-        // 루트 노드로 돌아왔을 때 양의 최대 갯수
-        return answer;
+        for (int[] edge : edges) {
+            routeList.get(edge[0]).add(edge[1]); // 위에서 아래로 단방향
+        }
+        
+        visited = new boolean[n];
+        
+        visited[0] = true;
+        
+        dfs(0, 0, 0); // 0번 노드부터 dfs 시작
+        
+        return answer;        
     }
     
-    private void dfs(int now, boolean[] visited, int sheep, int wolf) {
-        visited[now] = true;
-        
+    private void dfs(int now, int sheep, int wolf) {
         if (info[now] == 0) sheep++;
-        else wolf++;
+        if (info[now] == 1) wolf++;
         
-        if (wolf >= sheep) return;
-        
+        if (sheep <= wolf) return;
+    
         answer = Math.max(answer, sheep);
         
-        // 현재 위치에서 더 깊게 이동 OR 지금까지 지나온 길에서 더 깊게 이동
-        for (int[] edge : edges) {
-            // 부모 노드 방문했고 자식 노드 방문 안했으면, 그 자식 노드를 방문하기 
-            // 이때 부모 노드는 이미 지나온 길들
-            if (visited[edge[0]] && !visited[edge[1]]) {
-                boolean[] newVisited = visited.clone();
-                // boolean[] newVisited = new boolean[info.length];
-                // for (int i = 0; i < info.length; i++) {
-                //     newVisited[i] = visited[i];
-                // }
-                dfs(edge[1], newVisited, sheep, wolf);
+        // 현재 노드(now)에서 이동할 수 있는 곳
+        // 1. 현재 노드의 자식 노드로
+        // 2. 되돌아 간 뒤, 되돌아 간 노드의 자식 노드로
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                for (int next : routeList.get(i)) {
+                    if (!visited[next]) {
+                        visited[next] = true;
+                        dfs(next, sheep, wolf);
+                        visited[next] = false;
+                    }
+                    
+                }
             }
         }
     }
