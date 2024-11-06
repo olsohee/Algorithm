4a498790c60d0109be432eecbb60394fcb1c71c3
olsohee <108605017+olsohee@users.noreply.github.com>
2024-11-06 17:54:32 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.09 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/README.md
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/README.md
@@ -1,10 +1,10 @@
 # [level 2] 혼자서 하는 틱택토 - 160585 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585#) 
 
 ### 성능 요약
 
-메모리: 84.9 MB, 시간: 30.10 ms
+메모리: 74.9 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 06일 17:38:45
+2024년 11월 06일 17:54:28
 
 ### 문제 설명
 
--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,6 +1,87 @@
+import java.util.*;
+
 class Solution {
+    
+    char[][] map;
+    int xCnt;        
+    int oCnt;
+    int xWinCnt;
+    int oWinCnt;
+    
     public int solution(String[] board) {
-        int answer = -1;
-        return answer;
+        map = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            char[] arr = board[i].toCharArray();
+            for (int j = 0; j < 3; j++) {
+                map[i][j] = arr[j];
+            }
+        }
+        
+        // 1. 개수
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (map[i][j] == 'X') xCnt++;
+                if (map[i][j] == 'O') oCnt++;
+            }
+        }
+        
+        if (xCnt > oCnt || Math.abs(oCnt-xCnt) >= 2) return 0;
+        
+        // 2. 이기는 횟수
+        // 가로/세로
+        for (int i = 0; i < 3; i++) {
+            func(i, true);
+            func(i, false);
+        }
+        // 대각선
+        if (map[0][0] == map[1][1] && map[1][1] == map[2][2]) {
+            if (map[0][0] == 'O') oWinCnt++;
+            if (map[0][0] == 'X') xWinCnt++;
+        }
+        
+        if (map[0][2] == map[1][1] && map[1][1] == map[2][0]) {
+            if (map[0][2] == 'O') oWinCnt++;
+            if (map[0][2] == 'X') xWinCnt++;
+        }
+        
+        if (oWinCnt >= 1 && xWinCnt >= 1) return 0;
+        if (oWinCnt >= 1) {
+            if (oCnt != xCnt + 1) return 0;
+        }
+        if (xWinCnt >= 1) {
+            if (oCnt != xCnt) return 0;
+        }
+        
+        return 1;
+    }
+    
+    private void func(int idx, boolean isVertical) {
+        if (isVertical) {
+            char flag = map[0][idx];
+            boolean isWin = true;
+            for (int i = 1; i < 3; i++) {
+                if (flag != map[i][idx]) {
+                    isWin = false;
+                    break;
+                }
+            }
+            if (isWin) {
+                if (flag == 'O') oWinCnt++;
+                if (flag == 'X') xWinCnt++;
+            }
+        } else {
+            char flag = map[idx][0];
+            boolean isWin = true;
+            for (int i = 1; i < 3; i++) {
+                if (flag != map[idx][i]) {
+                    isWin = false;
+                    break;
+                }
+            }
+            if (isWin) {
+                if (flag == 'O') oWinCnt++;
+                if (flag == 'X') xWinCnt++;
+            }
+        }
     }
 }
