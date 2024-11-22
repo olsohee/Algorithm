49372302b5e4fdf562b4eb9f25c970c89a5d4c4a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-11-22 12:19:38 +0900

    [Gold V] Title: 감소하는 수, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,28 +10,24 @@ public class Main {
     static List<Long> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        /*
-        재귀
-        - 시작: 0~9
-        - 현재 숫자보다 작은 수가 이어서 나오게 재귀 진행
-         */
-        
         int n = Integer.parseInt(br.readLine());
-
-        if (n < 10) {
+        if (n <= 10) {
             System.out.println(n);
         } else {
             for (int i = 0; i <= 9; i++) {
                 func(i);
             }
-            Collections.sort(list);
-            System.out.println((n >= list.size()) ? -1 : list.get(n));
+            if (n >= list.size()) {
+                System.out.println(-1);
+            } else {
+                Collections.sort(list);
+                System.out.println(list.get(n));
+            }
         }
     }
 
     private static void func(long num) {
         list.add(num);
-
         for (int i = 0; i < num % 10; i++) {
             func(num * 10 + i);
         }
