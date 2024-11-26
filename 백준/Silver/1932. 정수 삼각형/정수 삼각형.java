9a07b0cd10ac909c917b179021c79ee57e568c27
olsohee <108605017+olsohee@users.noreply.github.com>
2024-11-26 18:12:17 +0900

    [Silver I] Title: 정수 삼각형, Time: 244 ms, Memory: 26816 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,39 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static int[][] input;
-    static int[][] dp;
-    static int n;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        input = new int[n + 1][n + 1];
-        dp = new int[n + 1][n + 1];
 
-        for(int i = 1; i <= n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= i; j++) {
-                input[i][j] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[][] map = new int[n][n];
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= i; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + input[i][j];
+        // dp
+        dp[0][0] = map[0][0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + map[i][j];
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + map[i][j];
             }
         }
 
-        int result = 0;
-        for(int i = 1; i <= n; i++) {
-            result = Math.max(dp[n][i], result);
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dp[n - 1][i]);
         }
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
