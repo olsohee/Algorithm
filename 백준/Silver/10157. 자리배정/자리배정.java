2858db765a5a657b53c33c4d52374374d6e09afb
olsohee <108605017+olsohee@users.noreply.github.com>
2024-12-05 17:33:41 +0900

    [Silver III] Title: 자리배정, Time: 140 ms, Memory: 16912 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.java
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,7 +11,7 @@ public class Main {
     static int c;
     static int r;
     static int k;
-    static int[][] map;
+    static boolean[][] visited;
     static int[] dy = {-1, 0, 1, 0}; // 위, 오른쪽, 아래, 왼쪽
     static int[] dx = {0, 1, 0, -1};
     static int answerY = 0;
@@ -20,7 +21,7 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         c = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
-        map = new int[r][c];
+        visited = new boolean[r][c];
         k = Integer.parseInt(br.readLine());
 
         if (c * r < k) {
@@ -28,28 +29,31 @@ public class Main {
             return;
         }
 
-        dfs(1, r - 1, 0, 0);
-        System.out.println((answerX + 1) + " " + (r - answerY));
-    }
+        int num = 1;
+        int y = r - 1;
+        int x = 0;
+        int dir = 0;
 
-    private static void dfs(int num, int y, int x, int dir) {
-        if (num == k) {
-            answerY = y;
-            answerX = x;
-            return;
+        while (num < k) {
+            // 1. 현재 위치 방문
+            visited[y][x] = true;
+
+            // 2. 다음 위치찾기
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+
+            // 방향 전환이 필요한 경우
+            if (ny < 0 || ny >= r || nx < 0 || nx >= c || visited[ny][nx]) {
+                dir = (dir + 1) % 4;
+                ny = y + dy[dir];
+                nx = x + dx[dir];
+            }
+
+            y = ny;
+            x = nx;
+            num++;
         }
-        map[y][x] = num;
 
-        int ny = y + dy[dir];
-        int nx = x + dx[dir];
-        
-        // 방향 전환이 필요한 경우
-        if (ny < 0 || ny >= r || nx < 0 || nx >= c || map[ny][nx] != 0) {
-            dir = (dir + 1) % 4;
-            ny = y + dy[dir];
-            nx = x + dx[dir];
-        }
-
-        dfs(num + 1, ny, nx, dir);
+        System.out.println((x + 1) + " " + (r - y));
     }
 }
