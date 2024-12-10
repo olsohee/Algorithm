68950ccd5636da6d6d0da06a527b260f2c49bd9a
olsohee <108605017+olsohee@users.noreply.github.com>
2024-12-10 13:31:35 +0900

    [Silver I] Title: 영역 구하기, Time: 120 ms, Memory: 14848 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,78 +1,88 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    static int M; // 세로
-    static int N; // 가로
-    static int K;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int m, n;
     static int[][] map;
     static boolean[][] visited;
-    static int answer = 0;
-    static int cnt = 0;
-    static List<Integer> list = new ArrayList<>();
+    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        map = new int[M][N];
-        visited = new boolean[M][N];
-
-        for(int i = 0; i < K; i++) {
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        map = new int[m][n];
+        visited = new boolean[m][n];
+        for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
+            fill(x1, m - y1, x2, m - y2);
+        }
 
-            // 좌표 채우기
-            int width = x2 - x1;
-            int heigth = y2 - y1;
-            for(int j = (M - y2); j < heigth + (M - y2); j++) {
-                for(int k = x1; k < width + x1; k++) {
-                    map[j][k] = 1;
+        List<Integer> answerList = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    answerList.add(bfs(i, j));
                 }
             }
         }
-
-        for(int i = 0; i < M; i++) {
-            for(int j = 0; j < N; j++) {
-                if(map[i][j] == 0 && !visited[i][j]) {
-                    answer++;
-                    cnt = 0;
-                    dfs(i, j);
-                    list.add(cnt);
-                }
-            }
-        }
-        Collections.sort(list);
-
-        System.out.println(answer);
-        for (Integer i : list) {
-            System.out.print(i + " ");
+        Collections.sort(answerList);
+        System.out.println(answerList.size());
+        for (Integer cnt : answerList) {
+            System.out.print(cnt + " ");
         }
     }
 
-    static void dfs(int y, int x) {
+    private static void fill(int x1, int y1, int x2, int y2) {
+        for (int i = y2; i < y1; i++) {
+            for (int j = x1; j < x2; j++) {
+                map[i][j] = 1;
+            }
+        }
+    }
 
-        cnt++;
+    private static int bfs(int y, int x) {
         visited[y][x] = true;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        for(int i = 0; i < 4; i++) {
-            int nx = dx[i] + x;
-            int ny = dy[i] + y;
-            if(nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                continue;
+        Queue<Node> que = new LinkedList<>();
+        que.add(new Node(y, x));
+        int cnt = 1;
+
+        while (!que.isEmpty()) {
+            Node now = que.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = now.y + dy[i];
+                int nx = now.x + dx[i];
+                if (ny < 0 || ny >= m || nx < 0 || nx >= n || visited[ny][nx]) {
+                    continue;
+                }
+
+                if (map[ny][nx] == 0) {
+                    visited[ny][nx] = true;
+                    que.add(new Node(ny, nx));
+                    cnt++;
+                }
             }
-            if(map[ny][nx] == 0 && !visited[ny][nx]) {
-                dfs(ny, nx);
-            }
+        }
+        return cnt;
+    }
+
+    static class Node {
+        int y, x;
+
+        public Node(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
     }
 }
