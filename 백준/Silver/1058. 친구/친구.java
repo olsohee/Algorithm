d77e7abe5837fd3954f302a5257d7a5a935cb95f
olsohee <108605017+olsohee@users.noreply.github.com>
2025-01-10 14:44:46 +0900

    [Silver II] Title: 친구, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.java
+++ 백준/Silver/1058. 친구/친구.java
@@ -6,47 +6,52 @@ import java.util.*;
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int n;
-    static int[][] map;
 
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            char[] arr = br.readLine().toCharArray();
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == 'Y') {
-                    map[i][j] = 1;
-                } else {
-                    map[i][j] = Integer.MAX_VALUE;
-                }
-            }
-        }
+        int n = Integer.parseInt(br.readLine());
+        char[][] map = new char[n][n];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (map[i][k] != Integer.MAX_VALUE && map[k][j] != Integer.MAX_VALUE) {
-                        map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+            map[i] = br.readLine().toCharArray();
+        }
+        
+        int answer = 0;
+
+        // BFS
+        for (int i = 0; i < n; i++) {
+            boolean[] visited = new boolean[n];
+            Queue<Node> que = new LinkedList<>();
+            que.add(new Node(i, 0));
+            visited[i] = true;
+            int friendCnt = 0;
+
+            while (!que.isEmpty()) {
+                Node now = que.poll();
+                friendCnt++;
+
+                if (now.cnt < 2) {
+                    for (int j = 0; j < n; j++) {
+                        if (map[now.node][j] == 'Y' && !visited[j]) {
+                            visited[j] = true;
+                            que.add(new Node(j, now.cnt + 1));
+                        }
                     }
                 }
             }
-        }
 
-        // n명 각각의 2-친구 수 구하기
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            int friendCnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                if (map[i][j] == 1 || map[i][j] == 2) {
-                    friendCnt++;
-                }
-            }
-            answer = Math.max(answer, friendCnt);
+            answer = Math.max(answer, friendCnt - 1); // -1을 하는 이유 = 자기 자신을 poll 한 경우는 빼기
         }
 
         System.out.println(answer);
+    }
+
+    static class Node {
+        int node;
+        int cnt;
+
+        public Node(int node, int cnt) {
+            this.node = node;
+            this.cnt = cnt;
+        }
     }
 }
