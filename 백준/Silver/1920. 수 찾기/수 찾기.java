9ce8fb276e45aca8d688c0df1bc36311c671ec93
olsohee <108605017+olsohee@users.noreply.github.com>
2025-01-23 12:21:32 +0900

    [Silver IV] Title: 수 찾기, Time: 1060 ms, Memory: 50684 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -7,35 +7,34 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int n;
     static int[] nums;
-    public static void main(String[] args) throws IOException {
 
-        n = Integer.parseInt(br.readLine());
-        nums = new int[n];
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
+        nums = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(nums);
 
-        int k = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < k; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            System.out.println(canfind(num) ? 1 : 0);
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            System.out.println(isContain(target) ? 1 : 0);
         }
     }
 
-    private static boolean canfind(int num) {
+    private static boolean isContain(int target) {
         int start = 0;
-        int end = n - 1;
+        int end = nums.length - 1;
+
         while (start <= end) {
             int mid = (start + end) / 2;
-
-            if (nums[mid] == num) return true;
-
-            if (nums[mid] > num) {
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] > target) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
