3937344bd668e077bb8271eae87f4ab2b5598717
olsohee <108605017+olsohee@users.noreply.github.com>
2025-02-11 13:46:34 +0900

    [Silver I] Title: 트럭, Time: 124 ms, Memory: 15284 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.java
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -1,49 +1,49 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-// 시간 복잡도:
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int n;
-    static int w;
-    static int l;
-    static Queue<Integer> truck = new LinkedList<>();
-    static Queue<Integer> bridge = new LinkedList<>();
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        w = Integer.parseInt(st.nextToken());
-        l = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] trucks = new int[n];
         for (int i = 0; i < n; i++) {
-            truck.add(Integer.parseInt(st.nextToken()));
+            trucks[i] = Integer.parseInt(st.nextToken());
         }
 
+        Queue<Integer> que = new LinkedList<>();
         for (int i = 0; i < w; i++) {
-            bridge.add(0);
+            que.add(0);
         }
-
         int sum = 0;
+        int truckIdx = 0;
         int time = 0;
 
-        while (!bridge.isEmpty()) {
-            time++;
-            sum -= bridge.poll(); // 다리에서 트럭 내리기
-            if (!truck.isEmpty()) {
-                if (truck.peek() + sum <= l) {
-                    sum += truck.peek();
-                    bridge.add(truck.poll()); // 다리로 트럭 올리
-                } else {
-                    bridge.add(0);
-                }
+        while (truckIdx < n) {
+            Integer pollTruck = que.poll();
+            sum -= pollTruck;
+            if (sum + trucks[truckIdx] <= l) {
+                que.add(trucks[truckIdx]);
+                sum += trucks[truckIdx];
+                truckIdx++;
+            } else {
+                que.add(0);
             }
+
+            time++;
         }
+
+        // 마지막 트럭이 다리 위에 올라간 순간부터 내려가는데까지 걸리는 시간 계산
+        time += que.size();
 
         System.out.println(time);
     }
