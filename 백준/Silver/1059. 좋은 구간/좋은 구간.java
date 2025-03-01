7ca5ba6a52080ad02fd1ea53fbc2c14382a33db7
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-01 14:30:05 +0900

    [Silver IV] Title: 좋은 구간, Time: 112 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int s;
+    static int[] arr;
+    static int n;
+    static int start;
+    static int end;
+
+    public static void main(String[] args) throws IOException {
+        s = Integer.parseInt(br.readLine());
+        arr = new int[s];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < s; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        n = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+        if (Arrays.stream(arr)
+                .anyMatch(num -> num == n)) {
+            System.out.println(0);
+        } else if (arr[s - 1] <= n) {
+            System.out.println(0);
+        }
+        else {
+            System.out.println(result());
+        }
+    }
+
+    private static int result() {
+        for (int i = 0; i < s - 1; i++) {
+            if (arr[i] < n && arr[i + 1] > n) {
+                start = arr[i];
+                end = arr[i + 1];
+                break;
+            }
+        }
+        if (start == 0 && end == 0) {
+            start = 0;
+            end = arr[0];
+        }
+//        System.out.println(start + "~" + end);
+
+        for (int i = 0; i < s - 1; i++) {
+            if (arr[i] < n && arr[i + 1] > n) {
+                if (end - start < arr[i + 1] - arr[i]) {
+                    start = arr[i];
+                    end = arr[i + 1];
+                }
+            }
+        }
+
+        int cnt = 0;
+
+        for (int i = start + 1; i < end; i++) {
+            if (i > n) {
+                break;
+            }
+            for (int j = i + 1; j < end; j++) {
+                if (j < n) {
+                    continue;
+                }
+                cnt++;
+            }
+        }
+
+
+        return cnt;
+    }
+}
