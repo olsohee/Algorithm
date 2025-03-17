7d0217971b519e3660a44aff59664010e8118f5a
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-17 18:57:41 +0900

    [level 2] Title: 완전범죄, Time: 0.50 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    static final int INF = 121;
+    
+    public int solution(int[][] info, int n, int m) {
+        int cnt = info.length;
+        int[][] dp = new int[cnt + 1][m];
+        for (int i = 0; i <= cnt; i++) {
+            Arrays.fill(dp[i], INF);
+        }
+        dp[0][0] = 0;
+        
+        for (int i = 1; i <= cnt; i++) {
+            int a = info[i - 1][0];
+            int b = info[i - 1][1];
+            for (int j = 0; j < m; j++) {
+                // a가 훔치는 경우
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + a);
+                // b가 훔치는 경우
+                if ((j + b) < m) {
+                    dp[i][j + b] = Math.min(dp[i][j + b], dp[i - 1][j]);   
+                }
+            }
+        }
+        
+        int min = INF;
+        for (int i = 0; i < m; i++) {
+            min = Math.min(min, dp[cnt][i]);
+        }
+        return (min >= n) ? -1 : min;
+    }
+}
