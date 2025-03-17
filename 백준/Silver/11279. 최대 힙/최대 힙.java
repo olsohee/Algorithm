f5b526663a1923a7720923b252f11932d31fb5ea
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-17 15:18:19 +0900

    [Silver II] Title: 최대 힙, Time: 864 ms, Memory: 33100 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> que = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                System.out.println(que.isEmpty() ? 0 : que.poll());
+            } else {
+                que.add(num);
+            }
+        }
+    }
+}
