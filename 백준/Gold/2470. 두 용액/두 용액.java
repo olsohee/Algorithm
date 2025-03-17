4f6142ac50174507eee427c60b876a26041ad990
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-17 15:56:03 +0900

    [Gold V] Title: 두 용액, Time: 348 ms, Memory: 28508 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -9,35 +9,44 @@ public class Main {
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
 
-        int min = Integer.MAX_VALUE;
-        List<Integer> answer = new ArrayList<>();
+        // 1. 정렬
+        Arrays.sort(nums);
 
+        // 2. 투포인터
+        int minDiff = Integer.MAX_VALUE;
         int start = 0;
-        int end = arr.length - 1;
+        int end = n - 1;
+        int[] answer = new int[2];
+
         while (start < end) {
-            int sum = arr[start] + arr[end];
-            if (Math.abs(sum) < min) {
-                min = Math.abs(sum);
-                answer = List.of(arr[start], arr[end]);
+            int sum = nums[start] + nums[end];
+            if (sum == 0) {
+                answer[0] = nums[start];
+                answer[1] = nums[end];
+                break;
             }
-            if (sum == 0) break;
+
+            if (minDiff > Math.abs(sum)) {
+                minDiff = Math.abs(sum);
+                answer[0] = nums[start];
+                answer[1] = nums[end];
+            }
+
             if (sum > 0) {
                 end--;
             } else {
                 start++;
             }
         }
-        for (int i : answer) {
-            System.out.print(i + " ");
-        }
+
+        // 출력: 오름차순으로
+        System.out.print(answer[0] + " " + answer[1]);
     }
 }
