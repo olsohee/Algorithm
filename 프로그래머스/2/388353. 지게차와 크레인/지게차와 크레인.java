d2f1e3e40c862488de6d6963e819ea9dcf86c872
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-18 14:12:07 +0900

    [level 2] Title: 지게차와 크레인, Time: 4078.94 ms, Memory: 871 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -5,104 +5,89 @@ class Solution {
     int y, x;
     char[][] map;
     boolean[][] visited;
-    List<int[]> list = new ArrayList<>(); // 테두리 리스트
-    int[] dy = {0, 0, -1, 1};
-    int[] dx = {1, -1, 0, 0};
+    int[] dy = {1, -1, 0, 0};
+    int[] dx = {0, 0, 1, -1};
+    List<int[]> outlines = new ArrayList<>();
     
     public int solution(String[] storage, String[] requests) {
-        y = storage.length;
-        x = storage[0].length();
+        y = storage.length + 2;
+        x = storage[0].length() + 2;
         map = new char[y][x];
         visited = new boolean[y][x];
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                map[i][j] = storage[i].charAt(j);
+        
+        for (int i = 0; i < storage.length; i++) {
+            for (int j = 0; j < storage[i].length(); j++) {
+                map[i + 1][j + 1] = storage[i].charAt(j);
             }
         }
         
-        int answer = 0;
+        // 컨테이너 꺼내기
         for (String request : requests) {
-            char alp = request.charAt(0);
-            list = new ArrayList<>();
-            // 1. 기본 방법으로 꺼내기: 테두리에서 bfs 시작
+            char container = request.charAt(0);
+            // 1. 지게차로 꺼내기
             if (request.length() == 1) {
+                // 가장자리 분류
+                outlines = new ArrayList<>();
                 for (int i = 0; i < y; i++) {
                     for (int j = 0; j < x; j++) {
-                        // 테두리인 경우, bfs시작
                         if (i == 0 || i == y - 1 || j == 0 || j == x - 1) {
-                            // if (map[i][j] == alp) {
-                            //     visited[i][j] = true;
-                            // }
-                            if (visited[i][j]) {
-                                bfs(i, j, alp);
-                            }
-                        }
+                            bfs(i, j);
+                        } 
                     }
                 }
                 
-                for (int i = 0; i < y; i++) {
-                    for (int j = 0; j < x; j++) {
-                        if (i == 0 || i == y - 1 || j == 0 || j == x - 1) {
-                            if (map[i][j] == alp) {
-                                visited[i][j] = true;
-                            }
-                        }
-                    }
-                }
-                for (int[] outline : list) {
+                // 가장자리에서 접근
+                for (int[] outline : outlines) {
                     for (int i = 0; i < 4; i++) {
                         int ny = dy[i] + outline[0];
                         int nx = dx[i] + outline[1];
                         if (ny < 0 || ny >= y || nx < 0 || nx >= x) continue;
-                        if (map[ny][nx] == alp && !visited[ny][nx]) {
+                        if (map[ny][nx] == container && !visited[ny][nx]) {
                             visited[ny][nx] = true;
                         }
                     }
                 }
             }
             
-            // 2특별 방법으로 꺼내기: 전체 돌면서 꺼내기
+            // 2. 크레인으로 꺼내기
             if (request.length() == 2) {
                 for (int i = 0; i < y; i++) {
                     for (int j = 0; j < x; j++) {
-                        if (!visited[i][j] && map[i][j] == alp) {
+                        if (map[i][j] == container && !visited[i][j]) {
                             visited[i][j] = true;
                         }
                     }
                 }
             }
         }
-    
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                // String str = (visited[i][j]) ? "X" : "O";
-                // System.out.print(str + " ");
+        
+        int answer = 0;
+        for (int i = 1; i < y - 1; i++) {
+            for (int j = 1; j < x - 1; j++) {
                 if (!visited[i][j]) answer++;
             }
-            // System.out.println();
         }
-        return answer; // 남은 컨테이너 수
+        
+        return answer;
     }
     
-    private void bfs(int startY, int startX, char alp) {
+    private void bfs(int nowY, int nowX) {
         Queue<int[]> que = new LinkedList<>();
-        boolean[][] nowVisited = new boolean[y][x];
-        que.add(new int[]{startY, startX});
-        list.add(new int[]{startY, startX});
-        
-        nowVisited[startY][startX] = true;
+        boolean[][] bfsVisited = new boolean[y][x];
+        que.add(new int[]{nowY, nowX});
+        bfsVisited[nowY][nowX] = true;
+        outlines.add(new int[]{nowY, nowX});
         
         while (!que.isEmpty()) {
             int[] now = que.poll();
-        
             for (int i = 0; i < 4; i++) {
                 int ny = dy[i] + now[0];
                 int nx = dx[i] + now[1];
                 if (ny < 0 || ny >= y || nx < 0 || nx >= x) continue;
-                if (!nowVisited[ny][nx] && visited[ny][nx]) {
+                if (visited[ny][nx] && !bfsVisited[ny][nx]) {
+                    bfsVisited[ny][nx] = true;
                     que.add(new int[]{ny, nx});
-                    list.add(new int[]{ny, nx});
-                    nowVisited[ny][nx] = true;
+                    outlines.add(new int[]{ny, nx});
                 }
             }
         }
