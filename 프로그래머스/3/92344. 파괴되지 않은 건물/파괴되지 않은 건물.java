281ee1abe8bad7633afa2bc1c1ddf0ae22788bef
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-19 16:02:59 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 64.41 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,71 +1,76 @@
+import java.util.*;
+
 class Solution {
+    
+    int n, m;
+    int[][] mark;
+    
     public int solution(int[][] board, int[][] skill) {
-        int n = board.length; // 세로
-        int m = board[0].length; // 가로
+        n = board.length; // 세로 길이
+        m = board[0].length; // 가로 길이
+        mark = new int[n][m];
         
-        // 1. 공격을 누적합 준비 배열에 반영
-        int[][] pSum = new int[n][m];
-        for (int i = 0; i < skill.length; i++) {
-            int type = skill[i][0];
-            int y1 = skill[i][1];
-            int x1 = skill[i][2];
-            int y2 = skill[i][3];
-            int x2 = skill[i][4];
-            int degree = skill[i][5];
+        // 1. 스킬을 표시하기
+        for (int[] s : skill) {
+            int type = s[0];
+            int y1 = s[1];
+            int x1 = s[2];
+            int y2 = s[3];
+            int x2 = s[4];
+            int degree = s[5];
             
             // 공격
             if (type == 1) {
-                pSum[y1][x1] -= degree;
-                if (x2 + 1 < m) {
-                    pSum[y1][x2 + 1] += degree;
-                }
-                if (y2 + 1 < n) {
-                    pSum[y2 + 1][x1] += degree;
-                }
-                if (y2 + 1 < n && x2 + 1 < m) {
-                    pSum[y2 + 1][x2 + 1] -= degree;
-                }
+                mark(y1, x1, y2, x2, degree * - 1);
             }
-            
             // 회복
             if (type == 2) {
-                pSum[y1][x1] += degree;
-                if (x2 + 1 < m) {
-                    pSum[y1][x2 + 1] -= degree;
-                }
-                if (y2 + 1 < n) {
-                    pSum[y2 + 1][x1] -= degree;
-                }
-                if (y2 + 1 < n && x2 + 1 < m) {
-                    pSum[y2 + 1][x2 + 1] += degree;
-                }
+                mark(y1, x1, y2, x2, degree);
             }
         }
         
-        // 2. 누적합 준비 배열을 누적합하기
+        // 2. 스킬을 표시한 mark 표를 누적합 하기
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (i == 0 && j == 0) {
                     continue;
-                } else if (i == 0) {
-                    pSum[i][j] += pSum[i][j - 1];
-                } else if (j == 0) {
-                    pSum[i][j] += pSum[i - 1][j];
-                } else {
-                    pSum[i][j] = pSum[i - 1][j] + pSum[i][j - 1] - pSum[i - 1][j - 1] + pSum[i][j];
+                } 
+                if (i == 0) {
+                    mark[i][j] = mark[i][j - 1] + mark[i][j];
+                    continue;
                 }
+                if (j == 0) {
+                    mark[i][j] = mark[i - 1][j] + mark[i][j];
+                    continue;
+                }
+                
+                mark[i][j] = mark[i - 1][j] + mark[i][j - 1] - mark[i - 1][j - 1] + mark[i][j];
             }
         }
         
-        // 3. 누적합 결과를 원본 배열에 반영, 파괴되지 않은 건물 개수 세기
+        // 3. 누적합한 결과를 board에 반영하기
         int answer = 0;
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                board[i][j] += pSum[i][j];
+                board[i][j] += mark[i][j];
                 if (board[i][j] > 0) answer++;
             }
         }
         
-        return answer;
+        return answer; // 최종적으로 파괴되지 않은 건물 수
+    }
+    
+    private void mark(int y1, int x1, int y2, int x2, int degree) {
+        mark[y1][x1] += degree;
+        if (y2 + 1 < n && x2 + 1 < m) {
+            mark[y1][x2 + 1] -= degree;
+            mark[y2 + 1][x1] -= degree;
+            mark[y2 + 1][x2 + 1] += degree;
+        } else if (y2 + 1 < n) {
+            mark[y2 + 1][x1] -= degree;
+        } else if (x2 + 1 < m) {
+            mark[y1][x2 + 1] -= degree;
+        } 
     }
 }
