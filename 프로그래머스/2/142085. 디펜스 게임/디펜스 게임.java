64d76bd990f8b69ce27627f5089965af9c911797
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-20 20:25:51 +0900

    [level 2] Title: 디펜스 게임, Time: 92.39 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,47 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        if (k >= enemy.length) return enemy.length;
+        if (k >= enemy.length) {
+            return enemy.length;
+        }
         
-        // 가능한 라운드의 범위: 0 ~ enemy.length
-        int start = 0;
-        int end = enemy.length;
+        Queue<Integer> que = new PriorityQueue<>((o1, o2) -> o2 - o1);
         
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            long cnt = getEnemyCnt(k, enemy, mid); // 대적하는 적의 수 (k개의 적은 제외하고)
-            if (cnt <= n) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        for (int i = 0; i < enemy.length; i++) {
+            que.add(enemy[i]);
+            n -= enemy[i];
+            
+            if (n < 0) {
+                if (k > 0) {
+                    n += que.poll();
+                    k--;
+                } else {
+                    return i;
+                }
             }
-        }        
-
-        if (end == 1) {
-            if (enemy[0] <= n) return end;
-            else return 0;
-        }
-        return end;
-    }
-    
-    private long getEnemyCnt(int k, int[] enemy, int round) { 
-        // 1~round까지 간다고 가정
-        if (k >= round) {
-            return 0; 
         }
         
-        int[] enemyInRound = new int[round];
-        for(int i = 0; i < round; i++) {
-            enemyInRound[i] = enemy[i];
-        }
-        Arrays.sort(enemyInRound);
-        
-        // 적이 많은 k개 제외하고 대적
-        long enemyCnt = 0;
-        for (int i = 0; i < round - k; i++) {
-            enemyCnt += enemyInRound[i];
-            // System.out.println("더함 : " + enemyInRound[i]);
-        }
-        return enemyCnt;
+        return enemy.length;
     }
 }
