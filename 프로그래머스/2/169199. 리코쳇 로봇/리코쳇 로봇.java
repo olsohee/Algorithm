0f5d6e2248ae03deba68a31471efc3f00d4519ea
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-20 16:10:26 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.19 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,44 +2,43 @@ import java.util.*;
 
 class Solution {
     
+    int y, x;
     char[][] map;
-    boolean[][] visited;
-    int r;
-    int c;
-    int[] dy = {1, -1, 0, 0};
-    int[] dx = {0, 0, 1, -1};
-    int answer = -1;
     
     public int solution(String[] board) {
+        y = board.length;
+        x = board[0].length();
+        map = new char[y][x];
+        for (int i = 0; i < y; i++) {
+            map[i] = board[i].toCharArray();
+        }
         
-        r = board.length;
-        c = board[0].length();
-        map = new char[r][c];
-        visited = new boolean[r][c];
-        for (int i = 0; i < r; i++) {
-            char[] arr = board[i].toCharArray();
-            for (int j = 0; j < c; j++) {
-                map[i][j] = arr[j];
+        int startY = 0;
+        int startX = 0;
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                if (map[i][j] == 'R') {
+                    startY = i;
+                    startX = j;
+                } 
             }
         }
         
         // 시작점에서 bfs 시작
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (map[i][j] == 'R') {
-                    bfs(i, j);
-                }
-            }
-        }
-        
-        return answer;
+        return bfs(startY, startX);
     }
     
-    private void bfs(int y, int x) {
-        
+    private int bfs(int startY, int startX) {
         Queue<Node> que = new LinkedList<>();
-        que.add(new Node(y, x, 0));
-        visited[y][x] = true;
+        que.add(new Node(startY, startX, 0));
+        
+        boolean[][] visited = new boolean[y][x];
+        visited[startY][startX] = true;
+        
+        int answer = -1;
+        
+        int[] dy = {1, -1, 0, 0};
+        int[] dx = {0, 0, 1, -1};
         
         while (!que.isEmpty()) {
             Node now = que.poll();
@@ -47,40 +46,64 @@ class Solution {
                 answer = now.cnt;
                 break;
             }
+            
             for (int i = 0; i < 4; i++) {
+                /* now.dir이
+                0이면, 1로 이동 X
+                1이면, 0으로 이동 X
+                2이면, 3으로 이동 X
+                3이면, 2로 이동 X
+                */
+                // if (lastDir == 0 && i == 1) continue;
+                // if (lastDir == 1 && i == 0) continue;
+                // if (lastDir == 2 && i == 3) continue;
+                // if (lastDir == 3 && i == 2) continue;
+                
+                // i 방향으로 이동
                 int ny = now.y;
                 int nx = now.x;
+                
                 while (true) {
                     ny += dy[i];
                     nx += dx[i];
-                    if (ny < 0 || ny >= r || nx < 0 || nx >= c) {
-                        ny -= dy[i];
-                        nx -= dx[i];
+                    // 테두리이면 끝내기
+                    if (ny < 0 || ny == y || nx < 0 || nx == x) {
                         break;
                     }
+                    // 장애물 부딪히면 끝내기
                     if (map[ny][nx] == 'D') {
-                        ny -= dy[i];
-                        nx -= dx[i];
                         break;
                     }
                 }
                 
-                if (visited[ny][nx]) {
-                    continue;
+                
+                if (i == 0) {
+                    ny--;
+                }
+                if (i == 1) {
+                    ny++;
+                }
+                if (i == 2) {
+                    nx--;
+                }
+                if (i == 3) {
+                    nx++;
                 }
                 
-                que.add(new Node(ny, nx, now.cnt + 1));
-                visited[ny][nx] = true;
+                if (!visited[ny][nx]) {
+                    que.add(new Node(ny, nx, now.cnt + 1));
+                    visited[ny][nx] = true;
+                }
             }
         }
         
-        
+        return answer;
     }
     
-    private static class Node {
-        
+    static class Node {
         int y, x;
-        int cnt;
+        // int dir; // 해당 위치로 올 때 어떤 방향으로 왔는지 (0, 1, 2, 3)
+        int cnt; // 움직인 횟수
         
         public Node(int y, int x, int cnt) {
             this.y = y;
