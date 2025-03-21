1b6df2e44a78eabaaee1fc914a8379258656b224
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-21 17:02:18 +0900

    [level 3] Title: 광고 삽입, Time: 639.95 ms, Memory: 421 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,69 +1,71 @@
 import java.util.*;
 
 class Solution {
-    int[] ad = new int[360000]; // ad[i] = i 시간에 보고 있는 시청자 수
     public String solution(String play_time, String adv_time, String[] logs) {
-
+        int[] time = new int[360000];
         
-            for (String log : logs) {
-                String[] arr = log.split("-");
-                int start = timeToInt(arr[0]);
-                int end = timeToInt(arr[1]);
-                for (int i = start; i < end; i++) {
-                    ad[i]++;
-                }
-            }
-
-            // 슬라이딩 윈도우
-            int start = 0;
-            int end = timeToInt(adv_time);
-
-            long sum = 0;
-            for (int i = start; i < end; i++) {
-                sum += ad[i];
-            }
-            long max = sum;
-            int answer = 0;
-
-            while (end <= timeToInt(play_time)) {
-                if (max < sum) {
-                    max = sum;
-                    answer = start;
-                }
-
-                sum -= ad[start++];
-                sum += ad[end++];
-            }
-
-            // 시작 시간 반환
-            return intToTime(answer);
+        for (String log : logs) {
+            int start = convertToTime(log.split("-")[0]);
+            int end = convertToTime(log.split("-")[1]);
+            time[start]++;
+            time[end]--;
         }
-
-        private int timeToInt(String str) {
-            String[] arr = str.split(":");
-            int time = 0;
-            time += Integer.parseInt(arr[0]) * 3600;
-            time += Integer.parseInt(arr[1]) * 60;
-            time += Integer.parseInt(arr[2]);
-            return time;
+        
+        // 누적합을 통해 각 시간에 보는 시청자 수 구하기
+        for (int i = 1; i < convertToTime(play_time); i++) {
+            time[i] += time[i - 1];
         }
-
-        private String intToTime(int num) {
-            String answer = "";
-
-            int h = num / 3600;
-            if (h < 10) answer += "0";
-            answer += h + ":";
-
-            int m = num % 3600 / 60;
-            if (m < 10) answer += "0";
-            answer += m + ":";
-
-            int s = num % 3600 % 60;
-            if (s < 10) answer += "0";
-            answer += s;
-
-            return answer;
+        
+        // 슬라이딩 윈도우로 time 중 어느 범위가 최대 인원이 되는지 확인
+        long sum = 0;
+        int start = 0;
+        int end = convertToTime(adv_time);
+        for (int i = start; i < end; i++) {
+            sum += time[i];
         }
+        long max = sum;
+        int answer = start; // 시작 시간
+        
+        while (end <= convertToTime(play_time)) {
+            if (max < sum) {
+                max = sum;
+                answer = start;
+            }
+            sum -= time[start++];
+            sum += time[end++];
+        }
+        
+        return convertToString(answer);
     }
-
+    
+    private int convertToTime(String str) {
+        String[] arr = str.split(":");
+        return Integer.parseInt(arr[0]) * 60 * 60
+            + Integer.parseInt(arr[1]) * 60 
+            + Integer.parseInt(arr[2]);
+    }
+    
+    private String convertToString(int time) {
+        System.out.println("시간 = " +time);
+        int hour = time / 3600;
+        int minute = (time % 3600) / 60;
+        int sec = (time % 3600) % 60;
+        String str = "";
+        
+        if (hour < 10) {
+            str += "0";
+        }
+        str += hour + ":";
+        
+        if (minute < 10) {
+            str += "0";
+        }
+        str += minute + ":";
+        
+        if (sec < 10) {
+            str += "0";
+        }
+        str += sec;
+        return str;
+    }
+}
