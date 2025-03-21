3f8b8fe84c796d9d2f0b839e84715eac54ad56d1
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-21 15:51:50 +0900

    [level 2] Title: 디펜스 게임, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
+        if (k >= enemy.length) {
+            return enemy.length;
+        }
         
         Queue<Integer> que = new PriorityQueue<>((o1, o2) -> o2 - o1);
         
@@ -10,7 +13,7 @@ class Solution {
             n -= enemy[i];
             
             if (n < 0) {
-                if (k > 0) { 
+                if (k > 0) {
                     n += que.poll();
                     k--;
                 } else {
