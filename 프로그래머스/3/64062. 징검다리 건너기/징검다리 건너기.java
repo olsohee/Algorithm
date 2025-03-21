58c3facd593381bc750c5a0773bd157b837b07c1
olsohee <108605017+olsohee@users.noreply.github.com>
2025-03-21 15:42:54 +0900

    [level 3] Title: 징검다리 건너기, Time: 40.77 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,13 +1,7 @@
 import java.util.*;
 
 class Solution {
-    
-    int[] stones;
-    int k;
-    
     public int solution(int[] stones, int k) {
-        this.stones = stones;
-        this.k = k;
         
         int start = 0;
         int end = 0;
@@ -15,36 +9,28 @@ class Solution {
             end = Math.max(end, stone);
         }
         
-        int answer =  0;
+        // 이분탐색으로 건널 수 있는 사람의 최대 수 구하기
         while (start <= end) {
-            int mid = (start + end) / 2; // mid : 건너는 인원 수
-            if (isSuccess(mid)) {
-                answer = Math.max(answer, mid); // 성공하면, 답에 반영
-                start = mid + 1; // 성공하면, 인원 늘리기
+            int mid = (start + end) / 2;
+            if (canMove(stones, mid, k)) {
+                start = mid + 1;
             } else {
-                end = mid - 1; // 실패하면, 인원 줄이기
+                end = mid - 1;
             }
         }
-        
-        return answer;
+        return end;
     }
     
-    private boolean isSuccess(int movePeopleCnt) {
-        
-        int keepCnt = 1; // 연속으로 건너뛴 개수
-        
+    private boolean canMove(int[] stones, int people, int k) {
+        int cnt = 0; // 연속으로 건너뛰는 돌의 수
         for (int i = 0; i < stones.length; i++) {
-            if (stones[i] >= movePeopleCnt) {
-                keepCnt = 1;
-            } else { // 건너뛰기
-                if (keepCnt < k) { 
-                    keepCnt++;
-                } else { // 이미 k만큼 다 건너뛴 경우 실패
-                    return false;
-                }
+            if (stones[i] >= people) { // 건너는 경우
+                cnt = 0;
+            } else { // 건너뛰는 경우
+                cnt++;
+                if (cnt == k) return false;
             }
         }
-        
         return true;
     }
 }
