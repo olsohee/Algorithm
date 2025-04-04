553b57a47509786c56e9fd9c947306bcef968bda
olsohee <108605017+olsohee@users.noreply.github.com>
2025-04-04 14:27:20 +0900

    [level 3] Title: 가장 먼 노드, Time: 32.70 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,69 +1,57 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int n, int[][] edge) {
         
-        List<List<Node>> list = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             list.add(new ArrayList<>());
         }
         
         for (int[] e : edge) {
-            int start = e[0];
-            int end = e[1];
-            list.get(start).add(new Node(end, 1));
-            list.get(end).add(new Node(start, 1));
+            list.get(e[0]).add(e[1]);
+            list.get(e[1]).add(e[0]);
         }
         
-        // 1번 노드를 시작으로 다익스트라 
-        boolean[] visited = new boolean[n + 1];
+        // 다익스트라
+        Queue<Node> que = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
         int[] dist = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         
-        Queue<Node> que = new PriorityQueue<>();
-        que.add(new Node(1, 0));
         dist[1] = 0;
+        que.add(new Node(1, 0));
         
         while (!que.isEmpty()) {
             Node now = que.poll();
             
-            if (visited[now.end]) continue;
-            visited[now.end] = true;
-            
-            for (Node node : list.get(now.end)) {
-                if (dist[node.end] > now.cost + node.cost) {
-                    dist[node.end] = now.cost + node.cost;
-                    que.add(new Node(node.end, dist[node.end]));
+            for (int next : list.get(now.end)) {
+                if (dist[next] > now.cost + 1) {
+                    dist[next] = now.cost + 1;
+                    que.add(new Node(next, dist[next]));
                 }
             }
         }
         
-        // dist 배열에서 가장 큰 값의 노드 번호 찾기
-        int max = 0;
-        int maxCnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if (max < dist[i]) {
-                max = dist[i];
-                maxCnt = 1;
-            } else if (max == dist[i]) {
-                maxCnt++;
-            }
-        }
+        int maxCost = 0;
+        int cnt = 0;
         
-        // 1번 노드로부터 가장 멀리 떨어진 노드의 개수
-        return maxCnt;
-    }
+        for (int i = 2; i <= n; i++) {
+            if (maxCost < dist[i]) {
+                maxCost = dist[i];
+                cnt = 1;
+            } else if (maxCost == dist[i]) cnt++;
+        }
+        return cnt;
+    } 
     
-    public class Node implements Comparable<Node> {
-        int end, cost;
-        public Node (int end, int cost) {
+    class Node {
+        
+        int end;
+        int cost;
+        
+        public Node(int end, int cost) {
             this.end = end;
             this.cost = cost;
-        }
-        
-        public int compareTo(Node o) {
-            return this.cost - o.cost;
         }
     }
 }
