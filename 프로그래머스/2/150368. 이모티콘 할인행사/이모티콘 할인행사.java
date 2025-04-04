b8748877c54bbee282c2b8a9c6ad709e0f826dbb
olsohee <108605017+olsohee@users.noreply.github.com>
2025-04-04 13:46:32 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 72.89 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -4,68 +4,73 @@ class Solution {
     
     int[][] users;
     int[] emoticons;
-    List<int[]> saleList = new ArrayList<>();
+    int n, m;
+    List<Result> answerList = new ArrayList<>();
     
     public int[] solution(int[][] users, int[] emoticons) {
+        
         this.users = users;
         this.emoticons = emoticons;
+        this.n = users.length;
+        this.m = emoticons.length;
         
-        // 할인 조합 만들기
-        dfs(0, new int[emoticons.length]);
+        // dfs로 각 이모티콘의 할인율 조합 구하기
+        dfs(0, new int[m]);
         
-//         for (int[] arr : saleList) {
-//             for (int i : arr) {
-//                 System.out.print(i + " ");
-                
-//             }
-//             System.out.println();
-//         }
+        Collections.sort(answerList);
         
-        // 각 할인 조합마다 판단
-        int[] answer = new int[2];
-        for (int[] sale : saleList) {
-            int totalJoinCnt = 0;
-            int totalBuyCost = 0;
-            for (int[] user : users) {
-                // user[0] : 비율, user[1]: 가격
-                int buyCost = 0;
-                for (int i = 0; i < emoticons.length; i++) {
-                    // user[0] 이상의 할인을 하면 구매
-                    if (sale[i] >= user[0]) {
-                        buyCost += emoticons[i] * (100 - sale[i]) / 100;
-                    }
-                }
-                
-                // 총 구매 금액에 user[1] 이상이면 플러스 가입
-                if (buyCost >= user[1]) {
-                    totalJoinCnt++;
-                } else {
-                    totalBuyCost += buyCost;
-                }
-            }
-            
-            // 플러스 가입자 수가 가장 많고, 구매 금액이 가장 많은 것이 답
-            if (totalJoinCnt > answer[0]) {
-                answer[0] = totalJoinCnt;
-                answer[1] = totalBuyCost;
-            } else if (totalJoinCnt == answer[0])  {
-                if (totalBuyCost > answer[1]) {
-                    answer[1] = totalBuyCost;
-                }
-            }
-        }
-        return answer;
+        return new int[]{answerList.get(0).joinCnt, answerList.get(0).salesAmount};
     }
     
-    public void dfs(int depth, int[] arr) {
-        if (depth == emoticons.length) {
-            saleList.add(arr.clone());
+    private void dfs(int idx, int[] discoutPercent) {
+        if (idx == m) {
+            getResult(discoutPercent);
             return;
         }
         
         for (int i = 1; i <= 4; i++) {
-            arr[depth] = i * 10;
-            dfs(depth + 1, arr);
+            discoutPercent[idx] = i * 10;
+            dfs(idx + 1, discoutPercent);
+        }
+    }
+    
+    private void getResult(int[] discoutPercent) {
+        int joinCnt = 0;
+        int salesAmount = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int percent = users[i][0];
+            int joinAmount = users[i][1];
+            for (int j = 0; j < m; j++) {
+                if (discoutPercent[j] >= percent) { // 구매
+                    sum += emoticons[j] * (100 - discoutPercent[j]) / 100;
+                } 
+            }
+            if (sum >= joinAmount) {
+                joinCnt++;
+            } else {
+                salesAmount += sum;
+            }
+        }
+        
+        answerList.add(new Result(joinCnt, salesAmount));
+    }
+    
+    class Result implements Comparable<Result> {
+        int joinCnt;
+        int salesAmount;
+        
+        public Result (int joinCnt, int salesAmount) {
+            this.joinCnt = joinCnt;
+            this.salesAmount = salesAmount;
+        }
+        
+        @Override
+        public int compareTo(Result o) {
+            if (o.joinCnt == this.joinCnt) {
+                return o.salesAmount - this.salesAmount;
+            }
+            return o.joinCnt - this.joinCnt;
         }
     }
 }
