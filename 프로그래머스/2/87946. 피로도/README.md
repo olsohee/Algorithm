f55022cf0d8f06c48146ba8a8107a7e0f0daa357
olsohee <108605017+olsohee@users.noreply.github.com>
2025-04-09 12:19:12 +0900

    [level 2] Title: 피로도, Time: 14.37 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#qna) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 0.11 ms
+메모리: 79.8 MB, 시간: 14.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 10:48:12
+2025년 04월 09일 12:19:07
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,35 +2,51 @@ import java.util.*;
 
 class Solution {
     
+    int n; // 던전 수
     int k;
     int[][] dungeons;
+    int answer;
     boolean[] visited;
-    int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        
+        this.n = dungeons.length;
         this.k = k;
         this.dungeons = dungeons;
-        this.visited = new boolean[dungeons.length];
+        visited = new boolean[n];
         
-        dfs(0);
+        dfs(new ArrayList<>()); // 각 던전들의 순서 조합 만들기
+        
         return answer;
     }
     
-    public void dfs(int cnt) {
-        
-        for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && k >= dungeons[i][0]) {
-                visited[i] = true;
-                k -= dungeons[i][1];
-                
-                dfs(cnt + 1);
-                
-                visited[i] = false;
-                k += dungeons[i][1];
-            }
+    private void dfs(List<Integer> order) {
+        if (order.size() == n) {
+            // 해당 순서에서 피로도 계산하기
+            play(order);
+            return;
         }
         
-        answer = Math.max(answer, cnt);
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            order.add(i);
+            dfs(order);
+            order.remove((Object)i);
+            visited[i] = false;
+        }
+    }
+    
+    private void play(List<Integer> order) {
+        int tiredness = k;
+        int round = 0;
+        
+        for (int i = 0; i < order.size(); i++) {
+            if (tiredness < dungeons[order.get(i)][0]) {
+                break;
+            }
+            round++;
+            tiredness -= dungeons[order.get(i)][1];
+        }
+        answer = Math.max(answer, round);
     }
 }
