6bb9ebd5999c1dbdcec4f1be3afef1de343cc1ac
olsohee <108605017+olsohee@users.noreply.github.com>
2025-04-09 11:59:56 +0900

    [Gold V] Title: LCS, Time: 124 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[str1.length + 1][str2.length + 1];
+
+        for (int i = 1; i <= str1.length; i++) {
+            for (int j = 1; j <= str2.length; j++) {
+                char c1 = str1[i - 1];
+                char c2 = str2[j - 1];
+
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[str1.length ][str2.length]);
+    }
+}
