1dc542392ddda1b9c503204283f0c392fec9927d
olsohee <108605017+olsohee@users.noreply.github.com>
2025-04-16 16:45:14 +0900

    [level 3] Title: 숫자 게임, Time: 64.71 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int idx = 0;
+        for (int i = 0; i < B.length; i++) {
+            if (A[idx] < B[i]) {
+                answer++;
+                idx++;
+            } 
+        }
+        
+        return answer;
+    }
+}
