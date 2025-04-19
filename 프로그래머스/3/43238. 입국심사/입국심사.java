ec0706ea1dda3941428ed5db40c2b1f3d82ecdf5
olsohee <108605017+olsohee@users.noreply.github.com>
2025-04-19 13:09:42 +0900

    [level 3] Title: 입국심사, Time: 71.72 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,26 +2,29 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        // 이분탐색: 모든 사람을 심사하는데 걸리는 최소 시간 찾기
-        long start = 0;
         int maxTime = 0;
         for (int time : times) {
             maxTime = Math.max(maxTime, time);
         }
+        
+        long start = 0;
         long end = (long)maxTime * n;
         
         while (start <= end) {
-            long mid = (start + end) / 2; 
-            long sum = 0; // mid 시간 안에 각 심사대가 심사할 수 있는 사람의 수
+            long mid = (start + end) / 2; // 끝나는 시간
+            
+            long sum = 0; // mid 시간 안에 심사 가능한 사람 수
             for (int time : times) {
-                sum += mid / (long) time;
+                sum += mid / time;
             }
-            if (sum >= n) {
-                end = mid - 1;
-            } else {
+            
+            if (sum < n) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
+        
         return start;
     }
 }
