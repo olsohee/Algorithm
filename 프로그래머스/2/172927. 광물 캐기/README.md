58ee5d196f28d8db8899ffdf541d0dc3fc83c9d6
olsohee <108605017+olsohee@users.noreply.github.com>
2025-05-15 19:08:43 +0900

    [level 2] Title: 광물 캐기, Time: 21.17 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 18.98 ms
+메모리: 80.4 MB, 시간: 21.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 15:42:21
+2025년 05월 15일 19:08:38
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,87 +1,63 @@
 import java.util.*;
 
 class Solution {
+    
+    int answer = Integer.MAX_VALUE;
+    int[] picks;
+    String[] minerals;
+    int[][] value = {
+        {1, 1, 1},
+        {5, 1, 1},
+        {25, 5, 1}
+    };
+    Map<String, Integer> map = new HashMap<>();
+    
     public int solution(int[] picks, String[] minerals) {
+        this.picks = picks;
+        this.minerals = minerals;
+        map.put("diamond", 0);
+        map.put("iron", 1);
+        map.put("stone", 2);
         
-        // 1. 광물들을 5개씩 나눠, 돌로 캤을 때의 피로도 계산
-        List<Group> list = new ArrayList<>();
-        int cnt = 0; // 곡괭이 개수
-        for (int p : picks) {
-            cnt += p;
-        }
-        
-        for (int i = 0; i < minerals.length; i += 5) {
-            
-            if (i != 0 && i / 5 == cnt) {
-                break;
+        for (int i = 0; i < 3; i++) {
+            if (picks[i] > 0) {
+                picks[i]--;
+                // dfs(i, 0, 0, 0);
+                dfs(i, 1, value[i][map.get(minerals[0])], 1);
+                picks[i]++;
             }
-            int diaSum = 0;
-            int ironSum = 0;
-            int stoneSum = 0;
-            
-            for (int j = i; j < i + 5; j++) {
-                if (j == minerals.length) break;
-                if (minerals[j].equals("diamond")) {
-                    diaSum += 1;
-                    ironSum += 5;
-                    stoneSum += 25;
-                }
-                if (minerals[j].equals("iron")) {
-                    diaSum += 1;
-                    ironSum += 1;
-                    stoneSum += 5;
-                }
-                if (minerals[j].equals("stone")) {
-                    diaSum += 1;
-                    ironSum += 1;
-                    stoneSum += 1;
-                }
-            }
-            System.out.println(diaSum + ", " + ironSum + ", " + stoneSum);
-            list.add(new Group(diaSum, ironSum, stoneSum));
         }
-       
-        // 2. 피로도가 가장 높은 집합부터 캐기. 이때 "다이아->철->돌" 순위로 곡괭이 선택
-        Collections.sort(list);
-        int answer = 0;
         
-        for (Group g : list) {
-            System.out.println(g.diaSum + ", " + g.ironSum + ", " + g.stoneSum);
-            if (picks[0] > 0) {
-                answer += g.diaSum;
-                System.out.println("다이아로!");
-                picks[0]--;
-            } else if (picks[1] > 0) {
-                answer += g.ironSum;
-                System.out.println("철로!");
-                
-                picks[1]--;
-            } else if (picks[2] > 0) {
-                answer += g.stoneSum;
-                System.out.println("돌로!");
-                
-                picks[2]--;
-            }  else {
-                System.out.println("못캠!");
-}
-        }
         return answer;
     }
     
-    private class Group implements Comparable<Group> {
-        int diaSum;
-        int ironSum;
-        int stoneSum;
-        
-        public Group(int diaSUm, int ironSum, int stoneSum) {
-            this.diaSum = diaSUm;
-            this.ironSum = ironSum;
-            this.stoneSum = stoneSum;
+    public void dfs(int weapon, int idx, int sum, int cnt) {
+       
+        if (idx == minerals.length) { // 모든 광물을 캔 경우 끝!
+            answer = Math.min(answer, sum);
+            return;
         }
         
-        @Override
-        public int compareTo(Group o) {
-            return o.stoneSum - this.stoneSum; // 내림차순
+        if (cnt == 5) {
+            // 새로운 곡괭이 사용
+            boolean isFinish = true;
+            for (int i = 0; i < 3; i++) {
+                if (picks[i] > 0) {
+                    isFinish = false;
+                    picks[i]--;
+                    String mineral = minerals[idx];
+                    dfs(i, idx + 1, sum + value[i][map.get(mineral)], 1);
+                    picks[i]++;
+                }
+            }
+            if (isFinish) { // 모든 곡괭이를 사용한 경우 끝!
+                answer = Math.min(answer, sum);
+                return;
+            }
+        } else {
+            // 기존 곡괭이 사용
+            String mineral = minerals[idx];
+            dfs(weapon, idx + 1, sum + value[weapon][map.get(mineral)], cnt+1);
         }
     }
 }
