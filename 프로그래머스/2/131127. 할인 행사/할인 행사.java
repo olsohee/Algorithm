6988a914d883ca44abf438b31c3d7b42424089ed
olsohee <108605017+olsohee@users.noreply.github.com>
2025-05-16 12:52:51 +0900

    [level 2] Title: 할인 행사, Time: 44.91 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,46 +1,44 @@
 import java.util.*;
 
 class Solution {
+    
+    int answer = 0;
+    Map<String, Integer> map = new HashMap<>();
+    String[] want;
+    int[] number;
+    
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> wantMap = new HashMap<>();
-        for (int i = 0; i < want.length; i++) {
-            wantMap.put(want[i], number[i]);
-        }
+        this.want = want;
+        this.number = number;
         
-        Map<String, Integer> discountMap = new HashMap<>();
         for (int i = 0; i < 10; i++) {
-            String goods = discount[i];
-            discountMap.put(goods, discountMap.getOrDefault(goods, 0) + 1);
+            map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
         }
         
-        int answer = 0;
-        for (int day = 0; day <= discount.length - 10; day++) { // day: 등록 날
-            // 확인
-            if (canAnswer(wantMap, discountMap)) {
-                answer++;
-            }
+        compare();
+        
+        int start = 1;
+        while (start + 9 < discount.length) {
+            map.put(discount[start - 1], map.get(discount[start - 1]) - 1);
+            map.put(discount[start + 9], map.getOrDefault(discount[start + 9], 0) + 1);
             
-            // 이동
-            String removeGoods = discount[day];
-            discountMap.put(removeGoods, discountMap.get(removeGoods) - 1);
-            if (day + 10 == discount.length) break;
-            String addGoods = discount[day + 10];
-            discountMap.put(addGoods, discountMap.getOrDefault(addGoods, 0) + 1);
+            compare();
+            
+            start++;
         }
         
-        return answer; // 회원 등록 일수 (불가능하면 0 반환)
+        return answer;
     }
     
-    private boolean canAnswer(Map<String, Integer> wantMap, Map<String, Integer> discountMap) {
-        for (String goods : wantMap.keySet()) {
-            int wantCnt = wantMap.get(goods);
-            if (!discountMap.containsKey(goods)) {
-                return false;
+    public void compare() {
+        for (int i = 0; i < want.length; i++) {
+            if (!map.containsKey(want[i])) {
+                return;
             }
-            if (discountMap.get(goods) < wantCnt) {
-                return false;
+            if (map.get(want[i]) < number[i]) {
+                return;
             }
         }
-        return true;
+        answer++;
     }
 }
