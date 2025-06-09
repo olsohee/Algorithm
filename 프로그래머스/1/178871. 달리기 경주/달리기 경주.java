5471e327be6d8edb624a43ff4271a3c28d2e4e9e
olsohee <108605017+olsohee@users.noreply.github.com>
2025-06-09 15:31:10 +0900

    [level 1] Title: 달리기 경주, Time: 514.73 ms, Memory: 386 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -4,23 +4,17 @@ class Solution {
     public String[] solution(String[] players, String[] callings) {
         Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < players.length; i++) {
-            map.put(players[i], i);    
+            map.put(players[i], i);
         }
         
-        for (int i = 0; i < callings.length; i++) {
-            // map에서 해당 선수가 몇등인지 찾기
-            String name1 = callings[i];
-            int rank = map.get(name1);
-            String name2 = players[rank - 1];
-            
-            // 배열에서 위치 변경
-            players[rank - 1] = name1;
-            players[rank] = name2;
-            
-            // 변경된 위치 map에 반영
-            map.put(name1, rank - 1);
-            map.put(name2, rank);
+        for (String callingName : callings) {
+            int rank = map.get(callingName);
+            map.put(players[rank], map.get(players[rank]) - 1);
+            map.put(players[rank - 1], map.get(players[rank - 1]) + 1);
+            players[rank] = players[rank - 1];
+            players[rank - 1] = callingName;
         }
+        
         return players;
     }
 }
