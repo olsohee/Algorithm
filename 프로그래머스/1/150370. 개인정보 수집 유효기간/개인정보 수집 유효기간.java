b1a1203f53c6808403b2ebd504350f936159785f
olsohee <108605017+olsohee@users.noreply.github.com>
2025-06-09 16:53:09 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.31 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,34 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        
         List<Integer> answerList = new ArrayList<>();
-        
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> termMap = new HashMap<>();
         for (String term : terms) {
-            map.put(term.split(" ")[0], Integer.parseInt(term.split(" ")[1]));
+            termMap.put(term.split(" ")[0], Integer.parseInt(term.split(" ")[1]));
         }
         
-        int todayInt =  Integer.parseInt(today.split("\\.")[0]) * 12 * 28
-            + Integer.parseInt(today.split("\\.")[1]) * 28
-            + Integer.parseInt(today.split("\\.")[2]);
-        
+        int todayInt = convertToInt(today);
         for (int i = 0; i < privacies.length; i++) {
-            String privacy = privacies[i].split(" ")[0];
-            int term = map.get(privacies[i].split(" ")[1]) * 28;
-            
-            int start = Integer.parseInt(privacy.split("\\.")[0]) * 12 * 28 
-                + Integer.parseInt(privacy.split("\\.")[1]) * 28 
-                + Integer.parseInt(privacy.split("\\.")[2]);
-            
-            if (todayInt >= start + term) {
+            int start = convertToInt(privacies[i].split(" ")[0]);
+            int end = start + termMap.get(privacies[i].split(" ")[1]) * 28;
+            if (end <= todayInt) {
                 answerList.add(i + 1);
             }
         }
-        
-        // 파기할 번호를 오름차순으로
-        return answerList.stream()
-            .mapToInt(i -> i)
-            .toArray();
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+        return answer;
+    }
+    
+    private int convertToInt(String date) {
+        int year = Integer.parseInt(date.split("\\.")[0]);
+        int month = Integer.parseInt(date.split("\\.")[1]);
+        int day = Integer.parseInt(date.split("\\.")[2]);
+        return year * 12 * 28 + month * 28 + day;
     }
 }
